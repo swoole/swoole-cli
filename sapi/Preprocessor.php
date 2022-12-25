3a091c0011c7261b0edd6f11f592d3c66f44f3a1
jingjingxyk <zonghengbaihe521@qq.com>
2022-12-25 11:23:24 +0800

    下载扩展或者库命令添加重试机制

--- sapi/Preprocessor.php
+++ sapi/Preprocessor.php
@@ -325,7 +325,8 @@ class Preprocessor
         $skip_library_download = getenv('SKIP_LIBRARY_DOWNLOAD');
         if (empty($skip_library_download)) {
             if (!is_file($this->libraryDir . '/' . $lib->file)) {
-                echo `wget {$lib->url} -O {$this->libraryDir}/{$lib->file}`;
+                //echo `wget {$lib->url} -O {$this->libraryDir}/{$lib->file}`;
+                echo `curl --connect-timeout 15 --retry 5 --retry-delay 5  -Lo {$this->libraryDir}/{$lib->file} {$lib->url}`;
                 echo $lib->file;
             } else {
                 echo "[Library] file cached: " . $lib->file . PHP_EOL;
