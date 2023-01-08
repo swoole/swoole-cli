e299125a2d3d823cefd6fb6b1fbc6d30b7aa15c1
zengzhaoman <zengzhaoman@beiyugame.com>
2023-01-08 16:07:50 +0800

    feature 增加apcu扩展配置

---
+++ conf.d/apcu.php
@@ -0,0 +1,12 @@
+<?php
+
+use SwooleCli\Preprocessor;
+use SwooleCli\Extension;
+
+return function (Preprocessor $p) {
+    $p->addExtension((new Extension('apcu'))
+        ->withOptions('--enable-apcu')
+        ->withPeclVersion('5.1.22')
+        ->withHomePage('https://pecl.php.net/package/APCu')
+    );
+};
