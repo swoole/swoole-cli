f667b43801a671845142a38f797dbc07e9553460
jingjingxyk <zonghengbaihe521@qq.com>
2023-02-09 02:12:34 +0800

    save changed

--- conf.d/mongodb.php
+++ conf.d/mongodb.php
@@ -6,5 +6,9 @@ use SwooleCli\Extension;
 return function (Preprocessor $p) {
     $p->addExtension((new Extension('mongodb'))
         ->withOptions('--enable-mongodb')
-        ->withPeclVersion('1.14.2'));
+        ->withPeclVersion('1.14.2')
+        ->withManual('https://www.php.net/mongodb')
+        ->withUrl('https://github.com/mongodb/mongo-php-driver.git')
+        ->withLicense('https://github.com/mongodb/mongo-php-driver/blob/master/LICENSE')
+    );
 };
