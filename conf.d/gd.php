b8be98ee6801f3a0441df35b8f82043690c13dcc
jingjingxyk <zonghengbaihe521@qq.com>
2023-02-10 19:05:57 +0800

    save changed

--- conf.d/gd.php
+++ conf.d/gd.php
@@ -11,8 +11,6 @@ return function (Preprocessor $p) {
     $p->addExtension((new Extension('gd'))
         ->withOptions('--enable-gd \
         --with-jpeg=/usr/libjpeg/ \
-        --with-png-dir=/usr/libpng \
-        --with-zlib-dir=/usr/zlib \
         --with-freetype=/usr/freetype \
         --with-webp=/usr/libwebp')
     );
