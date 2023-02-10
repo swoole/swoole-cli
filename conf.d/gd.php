16a31904485c9d909ef946e10a46ef28afc96688
jingjingxyk <zonghengbaihe521@qq.com>
2023-02-10 18:54:46 +0800

    save changed

--- conf.d/gd.php
+++ conf.d/gd.php
@@ -3,8 +3,19 @@
 use SwooleCli\Preprocessor;
 use SwooleCli\Extension;
 
+/**
+ *  参考文档
+ * https://www.php.net/manual/zh/image.installation.php
+ */
 return function (Preprocessor $p) {
     $p->addExtension((new Extension('gd'))
-        ->withOptions('--enable-gd --with-jpeg=/usr/libjpeg/ --with-freetype=/usr/freetype/')
+        ->withOptions('
+        --enable-gd \
+        --with-jpeg=/usr/libjpeg/ \
+        --with-png-dir=/usr/libpng \
+        --with-zlib-dir=/usr/zlib \
+        --with-freetype=/usr/freetype \
+        --with-webp=/usr/libwebp 
+        ')
     );
 };
