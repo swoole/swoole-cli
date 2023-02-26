d396160ac2f1ec987c1c5b556a131e9cde4d9f12
jingjingxyk <zonghengbaihe521@qq.com>
2023-02-26 13:52:55 +0800

    save changed

--- library_install_builder.php
+++ library_install_builder.php
@@ -1024,7 +1024,6 @@ function install_pgsql(Preprocessor $p)
     $pgsql_prefix= PGSQL_PREFIX ;
 
     $openssl_prefix = OPENSSL_PREFIX;
-    $curl_prefix = CURL_PREFIX;
     $libxml2_prefix = LIBXML2_PREFIX;
     $libxslt_prefix = LIBXSLT_PREFIX;
     $readline_prefix = READLINE_PREFIX;
