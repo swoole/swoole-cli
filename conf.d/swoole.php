eadbdcfae5c5c344d385d0802682a47f838c5290
jingjingxyk <zonghengbaihe521@qq.com>
2023-03-03 10:49:11 +0800

    添加libpq库

--- conf.d/swoole.php
+++ conf.d/swoole.php
@@ -26,7 +26,8 @@ return function (Preprocessor $p) {
             cmake --build . --config Release --target install
 EOF
                 )
-                ->withScriptAfterInstall(<<<EOF
+                ->withScriptAfterInstall(
+                    <<<EOF
             rm -rf {$brotli_prefix}/lib/*.so.*
             rm -rf {$brotli_prefix}/lib/*.so
             rm -rf {$brotli_prefix}/lib/*.dylib
@@ -42,8 +43,70 @@ EOF
         );
         $options .= ' --with-brotli-dir=' . BROTLI_PREFIX;
     }
+    $pgsql_prefix= PGSQL_PREFIX ;
+    $p->addLibrary(
+        (new Library('pgsql'))
+            ->withHomePage('https://www.postgresql.org/')
+            ->withLicense('https://www.postgresql.org/about/licence/', Library::LICENSE_SPEC)
+            ->withUrl('https://ftp.postgresql.org/pub/source/v15.1/postgresql-15.1.tar.gz')
+            ->withPrefix($pgsql_prefix)
+            ->withBuildScript(
+                <<<'EOF'
+            ./configure --help
+            
+            sed -i.backup "s/invokes exit\'; exit 1;/invokes exit\';/"  src/interfaces/libpq/Makefile
+            # 替换指定行内容
+            sed -i.backup "102c all: all-lib" src/interfaces/libpq/Makefile
+            package_names="openssl zlib icu-uc icu-io icu-i18n readline libxml-2.0  libxslt libzstd liblz4"
+            CPPFLAGS="$(pkg-config  --cflags-only-I --static $package_names )" \
+            LDFLAGS="$(pkg-config   --libs-only-L   --static $package_names )" \
+            LIBS="$(pkg-config      --libs-only-l   --static $package_names )" \
+EOF . PHP_EOL . <<<EOF
+            ./configure  \
+            --prefix={$pgsql_prefix} \
+            --enable-coverage=no \
+            --with-ssl=openssl  \
+            --with-readline \
+            --with-icu \
+            --without-ldap \
+            --with-libxml  \
+            --with-libxslt 
+EOF
+                .   <<<'EOF'
+            result_code=$?
+            [[ $result_code -ne 0 ]] && echo "[make FAILURE]" && exit $result_code;
+            make -C src/include install 
 
-    $p->addExtension((new Extension('swoole'))
+            make -C  src/bin/pg_config install
+
+            make -C  src/common -j $cpu_nums all 
+            make -C  src/common install 
+
+            make -C  src/port -j $cpu_nums all 
+            make -C  src/port install 
+   
+            make -C  src/backend/libpq -j $cpu_nums all 
+            make -C  src/backend/libpq install 
+   
+            make -C src/interfaces/ecpg   -j $cpu_nums all-pgtypeslib-recurse all-ecpglib-recurse all-compatlib-recurse all-preproc-recurse
+            make -C src/interfaces/ecpg  install-pgtypeslib-recurse install-ecpglib-recurse install-compatlib-recurse install-preproc-recurse
+ 
+            # 静态编译 src/interfaces/libpq/Makefile  有静态配置  参考： all-static-lib
+            
+            make -C src/interfaces/libpq  -j $cpu_nums # soname=true
+            make -C src/interfaces/libpq  install 
+EOF
+            )
+            ->withPkgName('libpq')
+            ->withScriptAfterInstall(
+                <<<EOF
+             rm -rf {$pgsql_prefix}/lib/*.so.*
+             rm -rf {$pgsql_prefix}/lib/*.so
+EOF
+            )
+    );
+    $p->addExtension(
+        (new Extension('swoole'))
         ->withOptions($options)
         ->withLicense('https://github.com/swoole/swoole-src/blob/master/LICENSE', Extension::LICENSE_APACHE2)
         ->withHomePage('https://github.com/swoole/swoole-src')
