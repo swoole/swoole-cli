4fec0f8c1af70a36d68e9d656441244725f4db2a
jingjingxyk <zonghengbaihe521@qq.com>
2023-03-08 18:35:32 +0800

    save changed

--- conf.d/xlswriter.php
+++ conf.d/xlswriter.php
@@ -5,9 +5,10 @@ use SwooleCli\Extension;
 use SwooleCli\Library;
 
 return function (Preprocessor $p) {
+    # --with-libxlsxwriter=' . LIBXLSXWRITER_PREFIX
     $p->addExtension(
         (new Extension('xlswriter'))
-            ->withOptions('--with-xlswriter  --with-libxlsxwriter=' . LIBXLSXWRITER_PREFIX)
+            ->withOptions('--enable-reader --with-xlswriter '  )
             ->withPeclVersion('1.5.2')
             ->withHomePage('https://github.com/viest/php-ext-xlswriter')
             ->withLicense('https://github.com/viest/php-ext-xlswriter/blob/master/LICENSE', Extension::LICENSE_BSD)
