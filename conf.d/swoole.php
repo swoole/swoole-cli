e7ca37bc8c65a89bd6b2aca78af9b1e8896b8ee1
好吧，你想说啥 <zonghengbaihe521@qq.com>
2023-03-15 11:46:08 +0800

    默认启用brotli 库 (#61)
    * 默认启用brotli 库
    
    * swoole 扩展添加brotli 依赖
    
    * 调整brotli 选项

--- conf.d/swoole.php
+++ conf.d/swoole.php
@@ -6,28 +6,29 @@ use SwooleCli\Extension;
 
 return function (Preprocessor $p) {
     // curl/imagemagick 对 brotli 静态库的支持有点问题，暂时关闭
-    $options = '--enable-swoole --enable-sockets --enable-mysqlnd --enable-swoole-curl --enable-cares';
-    if ($p->getInputOption('with-brotli')) {
-        $brotli_prefix = BROTLI_PREFIX;
-        $p->addLibrary(
-            (new Library('brotli'))
-                ->withUrl('https://github.com/google/brotli/archive/refs/tags/v1.0.9.tar.gz')
-                ->withFile('brotli-1.0.9.tar.gz')
-                ->withPrefix(BROTLI_PREFIX)
-                ->withBuildScript(
-                    <<<EOF
+    $brotli_prefix = BROTLI_PREFIX;
+    $p->addLibrary(
+        (new Library('brotli'))
+            ->withHomePage('https://github.com/google/brotli')
+            ->withLicense('https://github.com/google/brotli/blob/master/LICENSE', Library::LICENSE_MIT)
+            ->withManual('https://github.com/google/brotli')//有多种构建方式，选择cmake 构建
+            ->withUrl('https://github.com/google/brotli/archive/refs/tags/v1.0.9.tar.gz')
+            ->withFile('brotli-1.0.9.tar.gz')
+            ->withPrefix($brotli_prefix)
+            ->withBuildScript(
+                <<<EOF
             cmake . -DCMAKE_BUILD_TYPE=Release \
             -DCMAKE_INSTALL_PREFIX={$brotli_prefix} \
             -DBROTLI_SHARED_LIBS=OFF \
             -DBROTLI_STATIC_LIBS=ON \
-            -DBROTLI_DISABLE_TESTS=ON \
+            -DBROTLI_DISABLE_TESTS=OFF \
             -DBROTLI_BUNDLED_MODE=OFF \
             && \
             cmake --build . --config Release --target install
 EOF
-                )
-                ->withScriptAfterInstall(
-                    <<<EOF
+            )
+            ->withScriptAfterInstall(
+                <<<EOF
             rm -rf {$brotli_prefix}/lib/*.so.*
             rm -rf {$brotli_prefix}/lib/*.so
             rm -rf {$brotli_prefix}/lib/*.dylib
@@ -36,19 +37,17 @@ EOF
             mv     {$brotli_prefix}/lib/libbrotlienc-static.a    {$brotli_prefix}/lib/libbrotlienc.a
             mv     {$brotli_prefix}/lib/libbrotlidec-static.a    {$brotli_prefix}/lib/libbrotlidec.a
 EOF
-                )
-                ->withPkgName('libbrotlicommon libbrotlidec libbrotlienc')
-                ->withLicense('https://github.com/google/brotli/blob/master/LICENSE', Library::LICENSE_MIT)
-                ->withHomePage('https://github.com/google/brotli')
-        );
-        $options .= ' --with-brotli-dir=' . BROTLI_PREFIX;
-    }
+            )
+            ->withPkgName('libbrotlicommon libbrotlidec libbrotlienc')
+            ->withBinPath($brotli_prefix . '/bin/')
+    );
 
+    $options = '--enable-swoole --enable-sockets --enable-mysqlnd --enable-swoole-curl --enable-cares --with-brotli-dir='. $brotli_prefix;
     $p->addExtension(
         (new Extension('swoole'))
         ->withOptions($options)
         ->withLicense('https://github.com/swoole/swoole-src/blob/master/LICENSE', Extension::LICENSE_APACHE2)
         ->withHomePage('https://github.com/swoole/swoole-src')
-        ->depends('curl', 'openssl', 'cares', 'zlib')
+        ->depends('curl', 'openssl', 'cares', 'zlib' , 'brotli')
     );
 };
