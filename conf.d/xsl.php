c2851ce7ba99f210992fb012a73c0a14e63c9ce0
jingjingxyk <zonghengbaihe521@qq.com>
2023-03-15 10:57:47 +0800

    解决libxslt macOS 环境下依赖系统库libgcrypt的问题

--- conf.d/xsl.php
+++ conf.d/xsl.php
@@ -5,13 +5,36 @@ use SwooleCli\Preprocessor;
 use SwooleCli\Extension;
 
 return function (Preprocessor $p) {
+    $libxslt_prefix = LIBXSLT_PREFIX;
+    $libxml2_prefix = LIBXML2_PREFIX;
     $p->addLibrary(
         (new Library('libxslt'))
+            ->withHomePage('https://gitlab.gnome.org/GNOME/libxslt/-/wikis/home')
             ->withUrl('https://gitlab.gnome.org/GNOME/libxslt/-/archive/v1.1.34/libxslt-v1.1.34.tar.gz')
-            ->withPrefix(LIBXSLT_PREFIX)
-            ->withConfigure('./autogen.sh && ./configure --prefix=' . LIBXSLT_PREFIX . ' --enable-static=yes --enable-shared=no')
+            //https://download.gnome.org/sources/libxslt/1.1/
             ->withLicense('http://www.opensource.org/licenses/mit-license.html', Library::LICENSE_MIT)
+            ->withPrefix($libxslt_prefix)
+            ->withConfigure(
+                <<<EOF
+            ./autogen.sh
+            ./configure --help
+            CPPFLAGS="$(pkg-config  --cflags-only-I  --static libxml-2.0  )" \
+            LDFLAGS="$(pkg-config --libs-only-L      --static libxml-2.0  )" \
+            LIBS="$(pkg-config --libs-only-l         --static libxml-2.0  )" \
+            ./configure \
+            --prefix={$libxslt_prefix} \
+            --enable-static=yes \
+            --enable-shared=no \
+            --with-libxml-libs-prefix={$libxml2_prefix} \
+            --without-python \
+            --without-crypto \
+            --without-profiler \
+            --without-plugins \
+            --without-debugger
+EOF
+            )
             ->withPkgName('libexslt libxslt')
+            ->withBinPath($libxslt_prefix . '/bin/')
             ->depends('libxml2', 'libiconv')
     );
     $p->addExtension((new Extension('xsl'))->withOptions('--with-xsl')->depends('libxslt'));
