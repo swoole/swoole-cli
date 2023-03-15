d48d710b93800abc3668f032e721655e3311fa2f
好吧，你想说啥 <zonghengbaihe521@qq.com>
2023-03-15 11:44:08 +0800

    curl 启用libcares库 (#53)
    * curl 启用libcares库
    
    * 调整code 格式

--- conf.d/curl.php
+++ conf.d/curl.php
@@ -14,18 +14,58 @@ return function (Preprocessor $p) {
             ->withLicense('https://c-ares.org/license.html', Library::LICENSE_MIT)
             ->withHomePage('https://c-ares.org/')
     );
+
+    $curl_prefix = CURL_PREFIX;
+    $openssl_prefix = OPENSSL_PREFIX;
+    $zlib_prefix = ZLIB_PREFIX;
+    $cares_prefix = CARES_PREFIX;
     $p->addLibrary(
         (new Library('curl'))
+            ->withHomePage('https://curl.se/')
             ->withUrl('https://curl.se/download/curl-7.88.0.tar.gz')
-            ->withPrefix(CURL_PREFIX)
+            ->withManual('https://curl.se/docs/install.html')
+            ->withLicense('https://github.com/curl/curl/blob/master/COPYING', Library::LICENSE_SPEC)
+            ->withPrefix($curl_prefix)
             ->withConfigure(
-                'autoreconf -fi && ./configure --prefix=' . CURL_PREFIX .
-                ' --enable-static --disable-shared --with-openssl=' . OPENSSL_PREFIX . ' ' .
-                '--without-librtmp --without-brotli --without-libidn2 --disable-ldap --disable-rtsp --without-zstd --without-nghttp2 --without-nghttp3'
+                <<<EOF
+            ./configure --help
+
+            CPPFLAGS="$(pkg-config  --cflags-only-I  --static zlib openssl libcares)" \
+            LDFLAGS="$(pkg-config   --libs-only-L    --static zlib openssl libcares)" \
+            LIBS="$(pkg-config      --libs-only-l    --static zlib openssl libcares)" \
+            ./configure --prefix={$curl_prefix}  \
+            --enable-static \
+            --disable-shared \
+            --without-librtmp \
+            --disable-ldap \
+            --disable-rtsp \
+            --enable-http \
+            --enable-alt-svc \
+            --enable-hsts \
+            --enable-http-auth \
+            --enable-mime \
+            --enable-cookies \
+            --enable-doh \
+            --enable-threaded-resolver \
+            --enable-ipv6 \
+            --enable-proxy  \
+            --enable-websockets \
+            --enable-get-easy-options \
+            --enable-file \
+            --enable-mqtt \
+            --enable-unix-sockets  \
+            --enable-progress-meter \
+            --enable-optimize \
+            --with-zlib={$zlib_prefix} \
+            --with-openssl={$openssl_prefix} \
+            --enable-ares={$cares_prefix} \
+            --with-default-ssl-backend=openssl \
+            --without-nghttp2 \
+            --without-ngtcp2 \
+            --without-nghttp3
+EOF
             )
             ->withPkgName('libcurl')
-            ->withLicense('https://github.com/curl/curl/blob/master/COPYING', Library::LICENSE_SPEC)
-            ->withHomePage('https://curl.se/')
             ->depends('openssl', 'cares', 'zlib')
     );
     $p->addExtension((new Extension('curl'))->withOptions('--with-curl')->depends('curl'));
