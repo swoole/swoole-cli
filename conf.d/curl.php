4d1a483a3c86c357fa4dd5c86cda92b0f9ccf5db
jingjingxyk <zonghengbaihe521@qq.com>
2023-03-16 17:29:03 +0800

    移动 brotli 库 到 curl

--- conf.d/curl.php
+++ conf.d/curl.php
@@ -5,6 +5,42 @@ use SwooleCli\Preprocessor;
 use SwooleCli\Extension;
 
 return function (Preprocessor $p) {
+    $brotli_prefix = BROTLI_PREFIX;
+    $p->addLibrary(
+        (new Library('brotli'))
+            ->withHomePage('https://github.com/google/brotli')
+            ->withLicense('https://github.com/google/brotli/blob/master/LICENSE', Library::LICENSE_MIT)
+            ->withManual('https://github.com/google/brotli')//有多种构建方式，选择cmake 构建
+            ->withUrl('https://github.com/google/brotli/archive/refs/tags/v1.0.9.tar.gz')
+            ->withFile('brotli-1.0.9.tar.gz')
+            ->withPrefix($brotli_prefix)
+            ->withBuildScript(
+                <<<EOF
+            cmake . -DCMAKE_BUILD_TYPE=Release \
+            -DCMAKE_INSTALL_PREFIX={$brotli_prefix} \
+            -DBROTLI_SHARED_LIBS=OFF \
+            -DBROTLI_STATIC_LIBS=ON \
+            -DBROTLI_DISABLE_TESTS=OFF \
+            -DBROTLI_BUNDLED_MODE=OFF \
+            && \
+            cmake --build . --config Release --target install
+EOF
+            )
+            ->withScriptAfterInstall(
+                <<<EOF
+            rm -rf {$brotli_prefix}/lib/*.so.*
+            rm -rf {$brotli_prefix}/lib/*.so
+            rm -rf {$brotli_prefix}/lib/*.dylib
+            cp  -f {$brotli_prefix}/lib/libbrotlicommon-static.a {$brotli_prefix}/lib/libbrotli.a
+            mv     {$brotli_prefix}/lib/libbrotlicommon-static.a {$brotli_prefix}/lib/libbrotlicommon.a
+            mv     {$brotli_prefix}/lib/libbrotlienc-static.a    {$brotli_prefix}/lib/libbrotlienc.a
+            mv     {$brotli_prefix}/lib/libbrotlidec-static.a    {$brotli_prefix}/lib/libbrotlidec.a
+EOF
+            )
+            ->withPkgName('libbrotlicommon libbrotlidec libbrotlienc')
+            ->withBinPath($brotli_prefix . '/bin/')
+    );
+
     $p->addLibrary(
         (new Library('cares'))
             ->withUrl('https://c-ares.org/download/c-ares-1.19.0.tar.gz')
