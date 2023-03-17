89a07f15b305d08da178b697ad7ca57589392adf
好吧，你想说啥 <zonghengbaihe521@qq.com>
2023-03-17 14:19:47 +0800

    解决 gmp 生成动态链接库 和 mongodb 链接系统库的问题 (#75)
    * 解决mongodb 链接系统库的问题
    
    * gmp 明确指定仅构建静态库，为了解决macos 编译链接问题
    
    * mongodb 指定使用openssl

--- conf.d/mongodb.php
+++ conf.d/mongodb.php
@@ -6,8 +6,8 @@ use SwooleCli\Extension;
 return function (Preprocessor $p) {
     $p->addExtension(
         (new Extension('mongodb'))
-        ->withOptions('--enable-mongodb')
-        ->withPeclVersion('1.14.2')
-        ->depends('icu', 'openssl', 'zlib')
+            ->withOptions('--enable-mongodb --with-mongodb-system-libs=no --with-mongodb-ssl=openssl')
+            ->withPeclVersion('1.14.2')
+            ->depends('icu', 'openssl', 'zlib', 'libzstd')
     );
 };
