4c8b9c3514fef9199a28c79e912aeafe4dcd31b6
jingjingxyk <zonghengbaihe521@qq.com>
2023-03-18 13:27:50 +0800

    add libpq

--- conf.d/pdo_pgsql.php
+++ conf.d/pdo_pgsql.php
@@ -4,5 +4,5 @@ use SwooleCli\Preprocessor;
 use SwooleCli\Extension;
 
 return function (Preprocessor $p) {
-    $p->addExtension((new Extension('pdo_pgsql'))->withOptions('--with-pdo-pgsql=' . PGSQL_PREFIX));
+    $p->addExtension((new Extension('pdo_pgsql'))->withOptions('--with-pdo-pgsql=' . PGSQL_PREFIX)->depends('pgsql'));
 };
