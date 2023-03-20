d977b060854f7203b97887ae25f2263a1d0f9419
jingjingxyk <zonghengbaihe521@qq.com>
2023-03-20 22:33:01 +0800

    使用镜像地址下载依赖，添加参数：--with-download-mirror-url

--- sapi/Preprocessor.php
+++ sapi/Preprocessor.php
@@ -494,11 +494,7 @@ class Preprocessor
         if (empty($lib->file)) {
             $lib->file = basename($lib->url);
         }
-        if (
-            $this->getInputOption('enable-download-mirror')
-            &&
-            !empty($this->getInputOption('with-download-mirror-url'))
-        ) {
+        if (!empty($this->getInputOption('with-download-mirror-url'))) {
             $lib->url = $this->getInputOption('with-download-mirror-url') . '/libraries/' . $lib->file;
         }
         $skip_download = ($this->getInputOption('skip-download'));
@@ -531,15 +527,9 @@ class Preprocessor
             $ext->file = $ext->name . '-' . $ext->peclVersion . '.tgz';
             $ext->path = $this->extensionDir . '/' . $ext->file;
             $ext->url = "https://pecl.php.net/get/{$ext->file}";
-
-            if (
-                $this->getInputOption('enable-download-mirror')
-                &&
-                !empty($this->getInputOption('with-download-mirror-url'))
-            ) {
+            if (!empty($this->getInputOption('with-download-mirror-url'))) {
                 $ext->url = $this->getInputOption('with-download-mirror-url') . '/extensions/' . $ext->file;
             }
-
             if (!$this->getInputOption('skip-download')) {
                 if (!is_file($ext->path)) {
                     echo "[Extension] {$ext->file} not found, downloading: " . $ext->url . PHP_EOL;
