ca39b7a7ef5e702ea669397685d86fbadaa906bf
jingjingxyk <zonghengbaihe521@qq.com>
2023-03-21 01:31:11 +0800

    library 文件名显示改进

--- sapi/Preprocessor.php
+++ sapi/Preprocessor.php
@@ -529,7 +529,7 @@ class Preprocessor
                 unlink($file);
             }
             if (!is_file($file)) {
-                echo "[Library] { $file } not found, downloading: " . $lib->url . PHP_EOL;
+                echo "[Library] { $lib->file } not found, downloading: " . $lib->url . PHP_EOL;
                 $this->downloadFile($lib->url, $file);
             } else {
                 echo "[Library] file cached: " . $lib->file . PHP_EOL;
