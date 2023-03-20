e8118bb2c2c4c026c1e8e08dc627090729bcd6b6
jingjingxyk <zonghengbaihe521@qq.com>
2023-03-20 22:54:36 +0800

    fixed shell脚本字符串链接错误

--- sapi/download-box/download-box-get-archive-from-server.sh
+++ sapi/download-box/download-box-get-archive-from-server.sh
@@ -16,8 +16,7 @@ test -d ${__PROJECT__}/var || mkdir -p ${__PROJECT__}/var
 cd ${__PROJECT__}/var
 
 DOMAIN='http://127.0.0.1:8000'
-QUERY_STRING="${DOMAIN}/all-archive.zip"
-URL="${domain}${DOMAIN}"
+URL="${DOMAIN}/all-archive.zip"
 
 wget -O all-archive.zip ${URL}
 
