58768f36bf162a2378a908743557bfff65c304ba
jingjingxyk <zonghengbaihe521@qq.com>
2023-03-23 01:06:47 +0800

    save changed

--- library_install_builder_test.php
+++ library_install_builder_test.php
@@ -1554,6 +1554,8 @@ function install_libbpf(Preprocessor $p)
             ->withUrl('https://github.com/libbpf/libbpf/archive/refs/tags/v1.1.0.tar.gz')
             ->withFile('libbpf-v1.1.0.tar.gz')
             ->withManual('https://libbpf.readthedocs.io/en/latest/api.html')
+            ->withManual('https://libbpf.readthedocs.io/en/latest/program_types.html')
+            ->withManual('https://libbpf.readthedocs.io/en/latest/libbpf_build.html')
             ->withPrefix('/usr/libbpf')
             ->withCleanBuildDirectory()
             ->withConfigure(
@@ -1562,7 +1564,11 @@ function install_libbpf(Preprocessor $p)
                 BUILD_STATIC_ONLY=y  make
                 exit 0
                 mkdir build /usr/libbpf
-                BUILD_STATIC_ONLY=y OBJDIR=build DESTDIR=/usr/libbpf make install
+                PKG_CONFIG_PATH=/usr/libbpf/lib64/pkgconfig
+                BUILD_STATIC_ONLY=y \
+                OBJDIR=build \
+                DESTDIR=/usr/libbpf \
+                make install
                 eixt 0
 
 EOF
