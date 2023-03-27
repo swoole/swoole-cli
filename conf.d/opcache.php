76807d44ccb2479bbb4ea4236c6be7245cd571d5
jingjingxyk <zonghengbaihe521@qq.com>
2023-03-27 15:46:06 +0800

    save changed

--- conf.d/opcache.php
+++ conf.d/opcache.php
@@ -7,12 +7,12 @@ return function (Preprocessor $p) {
     $options = '--enable-opcache';
     $buildType = $p->getInputOption('with-build-type');
     if ($buildType == 'debug') {
-        $p->setExportVarable('VALGRIND_CFLAGS', '$(pkg-config  --cflags --static valgrind)');
-        $p->setExportVarable('VALGRIND_LIBS', '$(pkg-config    --libs   --static valgrind)');
+        $p->withExportVariable('VALGRIND_CFLAGS', '$(pkg-config  --cflags --static valgrind)');
+        $p->withExportVariable('VALGRIND_LIBS', '$(pkg-config    --libs   --static valgrind)');
 
         $capstone_prefix = CAPSTONE_PREFIX;
-        $p->setExportVarable('CAPSTONE_CFLAGS', $capstone_prefix . '/include');
-        $p->setExportVarable('CAPSTONE_LIBS', $capstone_prefix . '/lib');
+        # $p->withExportVariable('CAPSTONE_CFLAGS', $capstone_prefix . '/include');
+        # $p->withExportVariable('CAPSTONE_LIBS', $capstone_prefix . '/lib');
 
 
         $options .= ' --enable-memory-sanitizer ';
