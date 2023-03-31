ac7d9ec512a55fce32b95fd2d63654b1c40aa8cc
jingjingxyk <zonghengbaihe521@qq.com>
2023-03-31 13:10:52 +0800

    prepare macOS php runtime test

--- sapi/quickstart/prepare-php-runtime-macos-x86_64.sh
+++ sapi/quickstart/prepare-php-runtime-macos-x86_64.sh
@@ -16,9 +16,9 @@ mkdir -p  ${__PROJECT__}/var/runtime
 
 cd ${__PROJECT__}/var/runtime
 
-test -f swoole-cli-v5.0.2-macos-x64.tar.xz || wget -O swoole-cli-v5.0.2-macos-x64.tar.xz  https://github.com/swoole/swoole-src/releases/download/v5.0.2/swoole-cli-v5.0.2-macos-x64.tar.xz
-test -f  swoole-cli-v5.0.2-macos-x64.tar ||  xz -d -k swoole-cli-v5.0.2-macos-x64.tar.xz
-test -f swoole-cli ||  tar -xvf  swoole-cli-v5.0.2-macos-x64.tar
+test -f swoole-cli-v5.0.1-macos-x64.tar.xz || wget -O swoole-cli-v5.0.1-macos-x64.tar.xz  https://github.com/swoole/swoole-src/releases/download/v5.0.1/swoole-cli-v5.0.1-macos-x64.tar.xz
+test -f swoole-cli-v5.0.1-macos-x64.tar ||  xz -d -k swoole-cli-v5.0.1-macos-x64.tar.xz
+test -f swoole-cli ||  tar -xvf  swoole-cli-v5.0.1-macos-x64.tar
 chmod a+x swoole-cli
 
 test -f composer.phar ||  wget -O composer.phar https://getcomposer.org/download/2.5.5/composer.phar
