2f894e87ca5e0d9135a1bec42eec7f63e26831a3
jingjingxyk <zonghengbaihe521@qq.com>
2023-04-02 00:23:35 +0800

    修改 cygwin 安装多个库脚本

--- sapi/scripts/cygwin-install-depend.sh
+++ sapi/scripts/cygwin-install-depend.sh
@@ -31,23 +31,6 @@ setup-x86_64.exe  --no-desktop --no-shortcuts --no-startmenu --quiet-mode    --s
 
 exit 0
 
-./setup-x86_64.exe  --no-shortcuts  -q   -s http://mirrors.ustc.edu.cn/cygwin/   --packages     make
-
-
-#  安装apt-cyg 参考： https://zhuanlan.zhihu.com/p/66930502
-
-## apt-cyg必要环境
-# base tar wget bzip2 gawk xz
-# curl -Lo apt-cyg rawgit.com/transcode-open/apt-cyg/master/apt-cyg
-
-lynx -source rawgit.com/transcode-open/apt-cyg/master/apt-cyg > apt-cyg
-install apt-cyg /bin
-apt-cyg --help
-
-# apt-cyg mirror https://mirrors.ustc.edu.cn/cygwin/
-
-apt-cyg install wget tar libtool re2c bison gcc-g++ autoconf automake openssl libpcre2-devel libssl-devel libcurl-devel libxml2-devel libxslt-devel libgmp-devel ImageMagick libpng-devel libjpeg-devel libfreetype-devel libwebp-devel libsqlite3-devel zlib-devel libbz2-devel libreadline-devel  libintl-devel libpq-devel libssh2-devel libidn2-devel gettext-devel coreutils
-
 
 exit 0
 
@@ -73,5 +56,23 @@ setup-x86_64.exe --help
 setup-x86_64.exe -q -s http://mirror.internode.on.net
 setup-x86_64.exe -q -s http://mirrors.ustc.edu.cn/cygwin/
 
-C:/cygwin/setup-x86_64.exe  --no-shortcuts  --quiet-mode  --disable-buggy-antivirus   --packages wget tar libtool re2c bison gcc-g++ autoconf automake openssl
+C:/cygwin/setup-x86_64.exe  --no-shortcuts  --quiet-mode  --disable-buggy-antivirus   --packages wget,tar,libtool,re2c,bison,gcc-g++,autoconf,automake,openssl
+exit 0
+
+./setup-x86_64.exe  --no-shortcuts  -q   -s http://mirrors.ustc.edu.cn/cygwin/   --packages     make
+
+
+#  安装apt-cyg 参考： https://zhuanlan.zhihu.com/p/66930502
+
+## apt-cyg必要环境
+# base tar wget bzip2 gawk xz
+# curl -Lo apt-cyg rawgit.com/transcode-open/apt-cyg/master/apt-cyg
+
+lynx -source rawgit.com/transcode-open/apt-cyg/master/apt-cyg > apt-cyg
+install apt-cyg /bin
+apt-cyg --help
+
+# apt-cyg mirror https://mirrors.ustc.edu.cn/cygwin/
+
+apt-cyg install wget tar libtool re2c bison gcc-g++ autoconf automake openssl libpcre2-devel libssl-devel libcurl-devel libxml2-devel libxslt-devel libgmp-devel ImageMagick libpng-devel libjpeg-devel libfreetype-devel libwebp-devel libsqlite3-devel zlib-devel libbz2-devel libreadline-devel  libintl-devel libpq-devel libssh2-devel libidn2-devel gettext-devel coreutils
 
