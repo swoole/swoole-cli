24b5592ef45831ff2d9dcbc9484fa7c9bba0203a
jingjingxyk <zonghengbaihe521@qq.com>
2023-04-02 00:14:02 +0800

    修改 cygwin 安装多个库脚本

--- sapi/scripts/cygwin-install-depend.sh
+++ sapi/scripts/cygwin-install-depend.sh
@@ -24,10 +24,9 @@ cd ${__PROJECT__}
 
 ## 同时安装多个包 参考 https://ardupilot.org/dev/docs/building-setup-windows-cygwin.html
 
+## 命令行参数 https://www.cygwin.com/faq/faq.html#faq.setup.cli
 
-setup-x86_64.exe  --no-desktop --no-shortcuts --no-startmenu --quiet-mode    -s http://mirrors.ustc.edu.cn/cygwin/ ^
--P ^
-git,curl,wget,tar,libtool,re2c,bison,gcc-g++,autoconf,automake,openssl,libpcre2-devel,libssl-devel,libcurl-devel,libxml2-devel,libxslt-devel,libgmp-devel,ImageMagick,libpng-devel,libjpeg-devel,libfreetype-devel,libwebp-devel,libsqlite3-devel,zlib-devel,libbz2-devel,liblz4-devel,liblzma-devel,libzip-devel,libicu-devel,libonig-devel,libcares-devel,libsodium-devel,libyaml-devel,libMagick-devel,libzstd-devel,libbrotli-devel,libreadline-devel,libintl-devel,libpq-devel,libssh2-devel,libidn2-devel,gettext-devel,coreutils
+setup-x86_64.exe  --no-desktop --no-shortcuts --no-startmenu --quiet-mode    --site  http://mirrors.ustc.edu.cn/cygwin/ --packages  git,curl,wget,tar,libtool,re2c,bison,gcc-g++,autoconf,automake,openssl,libpcre2-devel,libssl-devel,libcurl-devel,libxml2-devel,libxslt-devel,libgmp-devel,ImageMagick,libpng-devel,libjpeg-devel,libfreetype-devel,libwebp-devel,libsqlite3-devel,zlib-devel,libbz2-devel,liblz4-devel,liblzma-devel,libzip-devel,libicu-devel,libonig-devel,libcares-devel,libsodium-devel,libyaml-devel,libMagick-devel,libzstd-devel,libbrotli-devel,libreadline-devel,libintl-devel,libpq-devel,libssh2-devel,libidn2-devel,gettext-devel,coreutils
 
 
 exit 0
