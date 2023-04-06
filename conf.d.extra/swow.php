4f123699fc263d95e81095aeda9ae177e4f4473e
jingjingxyk <zonghengbaihe521@qq.com>
2023-04-06 13:00:38 +0800

    调整测试例子

--- conf.d.extra/swow.php
+++ conf.d.extra/swow.php
@@ -2,21 +2,51 @@
 
 use SwooleCli\Preprocessor;
 use SwooleCli\Extension;
+use SwooleCli\Library;
 
 return function (Preprocessor $p) {
-    # Sanitizers 是谷歌开源的内存检测工具，包括AddressSanitizer、MemorySanitizer、ThreadSanitizer、LeakSanitizer。
-    # Sanitizers是LLVM的一部分。
-    # gcc不支持MemorySanitizer。
-    # --enable-swow-address-sanitizer --enable-swow-memory-sanitizer --enable-swow-undefined-sanitizer
+    # example 1
+    $libyuv_prefix = $p->getGlobalPrefix() . '/libyuv';
+    $p->addLibrary(
+        (new Library('libyuv'))
+            ->withUrl('https://chromium.googlesource.com/libyuv/libyuv')
+            ->withHomePage('https://chromium.googlesource.com/libyuv/libyuv')
+            ->withLicense('https://chromium.googlesource.com/libyuv/libyuv/+/refs/heads/main/LICENSE', Library::LICENSE_SPEC)
+            ->withManual('https://chromium.googlesource.com/libyuv/libyuv/+/HEAD/docs/getting_started.md')
+            ->withDownloadScript(
+                'libyuv',
+                <<<EOF
+            git clone -b main --depth=1 https://chromium.googlesource.com/libyuv/libyuv
+EOF
+            )
+            ->withPrefix($libyuv_prefix)
+            //->withCleanBuildDirectory()
+            //->withCleanPreInstallDirectory($libyuv_prefix)
+            ->withBuildScript(
+                <<<EOF
+                mkdir -p  out
+                cd out
+                cmake -DCMAKE_INSTALL_PREFIX="{$libyuv_prefix}" \
+                -DBUILD_STATIC_LIBS=ON \
+                -DBUILD_SHARED_LIBS=OFF  \
+                -DCMAKE_BUILD_TYPE="Release" ..
+                cmake --build . --config Release
+                cmake --build . --target install --config Release
 
-    # swow 启用不了
+EOF
+            )
+            ->withPkgName('')
+            ->withBinPath($libyuv_prefix . '/bin/')
+    );
+
+    # example 2
     $p->addExtension(
         (new Extension('swow'))
             ->withOptions('--enable-swow  --enable-swow-ssl --enable-swow-curl ')
             ->withHomePage('https://github.com/swow/swow')
             ->withLicense('https://github.com/swow/swow/blob/develop/LICENSE', Extension::LICENSE_APACHE2)
-            //->withFile('swow-v1.2.0.tar.gz')
-            ->withPeclVersion('1.2.0')
+            ->withFile('swow-v1.2.0.tar.gz')
+            //->withPeclVersion('1.2.0')
             ->withDownloadScript(
                 "swow",
                 <<<EOF
---
+++ conf.d.test/xlswriter.php
@@ -0,0 +1,49 @@
+<?php
+
+use SwooleCli\Preprocessor;
+use SwooleCli\Extension;
+use SwooleCli\Library;
+
+return function (Preprocessor $p) {
+    $libxlsxwriter_prefix = LIBXLSXWRITER_PREFIX;
+    $zlib_prefix =  ZLIB_PREFIX;
+    $lib = new Library('libxlsxwriter');
+    $lib->withHomePage('https://libxlsxwriter.github.io/')
+        ->withLicense('https://github.com/jmcnamara/libxlsxwriter/blob/main/License.txt', Library::LICENSE_SPEC)
+        ->withUrl('https://github.com/jmcnamara/libxlsxwriter/archive/refs/tags/RELEASE_1.1.5.tar.gz')
+        ->withFile('libxlsxwriter-1.1.5.tar.gz')
+        ->withPrefix($libxlsxwriter_prefix)
+        ->withBuildScript(
+            <<<EOF
+            # 启用DBUILD_TESTS 需要安装python3 pytest
+            mkdir build && cd build
+            cmake .. -DCMAKE_INSTALL_PREFIX={$libxlsxwriter_prefix} \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DZLIB_ROOT:STRING={$zlib_prefix} \
+            -DBUILD_TESTS=OFF \
+            -DBUILD_EXAMPLES=OFF \
+            -DUSE_STANDARD_TMPFILE=ON \
+            -DUSE_OPENSSL_MD5=ON \
+            && \
+            cmake --build . --config Release --target install
+EOF
+        )
+        ->depends('zlib')
+        ->withPkgName('xlsxwriter');
+    $p->addLibrary($lib);
+    $p->addExtension(
+        (new Extension('xlswriter'))
+            ->withOptions('--with-libxlsxwriter=' . LIBXLSXWRITER_PREFIX)
+            ->withPeclVersion('1.5.2')
+            ->withHomePage('https://github.com/viest/php-ext-xlswriter')
+            ->withLicense('https://github.com/viest/php-ext-xlswriter/blob/master/LICENSE', Extension::LICENSE_BSD)
+            ->depends('libxlsxwriter')
+            ->withFile('xlswriter-latest.tar.gz')
+            ->withDownloadScript(
+                'php-ext-xlswriter',
+                <<<EOF
+            git clone -b master --depth=1 https://github.com/viest/php-ext-xlswriter.git
+EOF
+            )
+    );
+};
