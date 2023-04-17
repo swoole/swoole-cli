fa4798383766a22faec507bf1d60263d3682cfb7
jingjingxyk <zonghengbaihe521@qq.com>
2023-04-17 11:40:11 +0800

    为升级到 openssl v3 做准备

--- conf.d/openssl.php
+++ conf.d/openssl.php
@@ -6,13 +6,17 @@ use SwooleCli\Extension;
 
 return function (Preprocessor $p) {
     $openssl_prefix = OPENSSL_PREFIX;
+    $static = $p->getOsType() === 'macos' ? '' : ' -static --static';
     $p->addLibrary(
         (new Library('openssl'))
          ->withHomePage('https://www.openssl.org/')
          ->withLicense('https://github.com/openssl/openssl/blob/master/LICENSE.txt', Library::LICENSE_APACHE2)
         ->withUrl('https://www.openssl.org/source/openssl-1.1.1p.tar.gz')
         ->withPrefix($openssl_prefix)
-        ->withConfigure('./config' . ($p->getOsType() === 'macos' ? '' : ' -static --static') . ' no-shared --prefix=' . OPENSSL_PREFIX)
+        ->withConfigure(<<<EOF
+        ./config {$static} no-shared --prefix=${openssl_prefix} --libdir=${openssl_prefix}/lib
+EOF
+        )
         ->withMakeInstallCommand('install_sw')
         ->withPkgName('openssl')
         ->withBinPath($openssl_prefix . '/bin/')
