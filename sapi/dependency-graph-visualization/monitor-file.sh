1b110053ad3eb97a16a71f93205e1ec2e52a91c7
jingjingxyk <zonghengbaihe521@qq.com>
2023-04-18 19:35:21 +0800

    完善生成扩展依赖图

--- sapi/dependency-graph-visualization/monitor-file.sh
+++ sapi/dependency-graph-visualization/monitor-file.sh
@@ -21,6 +21,16 @@ cd ${__DIR__}
 
 # inotifywait -mrq -e create,modify,delete /home
 
+if ! test -f input.template.dot ;then
+{
+  echo "graphviz 模板文件 input.template.dot 不存在"
+  echo "请运行 php prepare.php --with-dependency-graph=1 命令生成模板文件"
+  exit 3
+
+}
+fi
+
+
 inotifywait --monitor --timefmt '%Y%m%dT%H%M%SZ' --format '%T%w%f' -e modify,create,attrib ${__DIR__}/input.template.dot \
     | while read file
     do
--- sapi/dependency-graph-visualization/run.sh
+++ sapi/dependency-graph-visualization/run.sh
@@ -12,7 +12,19 @@ __PROJECT__=$(
 cd ${__DIR__}
 
 
+if test -f input.template.dot ;then
+{
+   {
+       dot -Tsvg   input.template.dot > ${__PROJECT__}/bin/output.svg
+       dot -Tpdf   input.template.dot > ${__PROJECT__}/bin/output.pdf
+   } || {
+     echo $?
+   }
 
+}
+else {
+  echo "graphviz 模板文件 input.template.dot 不存在，请生成模板文件"
+  echo "graphviz 详细信息： https://www.graphviz.org/ "
+}
+fi
 
-dot -Tsvg   input.template.dot > ${__PROJECT__}/bin/output.svg
-dot -Tpdf   input.template.dot > ${__PROJECT__}/bin/output.pdf
