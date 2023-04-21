3487ee3fbf6818277cb74a6ba3f2a3beb1977c93
好吧，你想说啥 <zonghengbaihe521@qq.com>
2023-04-21 13:14:23 +0800

    为升级到 openssl v3 做准备 (#110)
    * 为升级到 openssl v3 做准备
    
    * format code

--- conf.d/openssl.php
+++ conf.d/openssl.php
@@ -6,16 +6,21 @@ use SwooleCli\Extension;
 
 return function (Preprocessor $p) {
     $openssl_prefix = OPENSSL_PREFIX;
+    $static = $p->getOsType() === 'macos' ? '' : ' -static --static';
     $p->addLibrary(
         (new Library('openssl'))
-         ->withHomePage('https://www.openssl.org/')
-         ->withLicense('https://github.com/openssl/openssl/blob/master/LICENSE.txt', Library::LICENSE_APACHE2)
-        ->withUrl('https://www.openssl.org/source/openssl-1.1.1p.tar.gz')
-        ->withPrefix($openssl_prefix)
-        ->withConfigure('./config' . ($p->getOsType() === 'macos' ? '' : ' -static --static') . ' no-shared --prefix=' . OPENSSL_PREFIX)
-        ->withMakeInstallCommand('install_sw')
-        ->withPkgName('openssl')
-        ->withBinPath($openssl_prefix . '/bin/')
+            ->withHomePage('https://www.openssl.org/')
+            ->withLicense('https://github.com/openssl/openssl/blob/master/LICENSE.txt', Library::LICENSE_APACHE2)
+            ->withUrl('https://www.openssl.org/source/openssl-1.1.1p.tar.gz')
+            ->withPrefix($openssl_prefix)
+            ->withConfigure(
+                <<<EOF
+        ./config {$static} no-shared --prefix=${openssl_prefix} --libdir=${openssl_prefix}/lib
+EOF
+            )
+            ->withMakeInstallCommand('install_sw')
+            ->withPkgName('openssl')
+            ->withBinPath($openssl_prefix . '/bin/')
     );
     $p->addExtension(
         (new Extension('openssl'))
