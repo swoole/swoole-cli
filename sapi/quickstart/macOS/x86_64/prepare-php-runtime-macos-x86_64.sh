b389e69cd7b5e8800f55d1497dfd077004513e85
jingjingxyk <zonghengbaihe521@qq.com>
2023-04-26 16:10:59 +0800

    save changed

--- sapi/quickstart/macOS/x86_64/prepare-php-runtime-macos-x86_64.sh
+++ sapi/quickstart/macOS/x86_64/prepare-php-runtime-macos-x86_64.sh
@@ -18,13 +18,41 @@ mkdir -p  ${__PROJECT__}/var
 cd ${__PROJECT__}/var
 
 
+SWOOLE_CLI_DOWNLOAD_URL="https://github.com/swoole/swoole-src/releases/download/v4.8.13/swoole-cli-v4.8.13-macos-x64.tar.xz"
+COMPOSER_DOWNLOAD_URL="https://getcomposer.org/download/latest-stable/composer.phar"
 
-test -f swoole-cli-v5.0.1-macos-x64.tar.xz || wget -O swoole-cli-v5.0.1-macos-x64.tar.xz  https://github.com/swoole/swoole-src/releases/download/v5.0.1/swoole-cli-v5.0.1-macos-x64.tar.xz
-test -f swoole-cli-v5.0.1-macos-x64.tar ||  xz -d -k swoole-cli-v5.0.1-macos-x64.tar.xz
-test -f swoole-cli ||  tar -xvf  swoole-cli-v5.0.1-macos-x64.tar
+
+mirror=''
+while [ $# -gt 0 ]; do
+	case "$1" in
+		--mirror)
+			mirror="$2"
+			shift
+			;;
+		--*)
+			echo "Illegal option $1"
+			;;
+	esac
+	shift $(( $# > 0 ? 1 : 0 ))
+done
+
+case "$mirror" in
+	china)
+    SWOOLE_CLI_DOWNLOAD_URL="https://wenda-1252906962.file.myqcloud.com/dist/swoole-cli-v4.8.13-macos-x64.tar.xz"
+    COMPOSER_DOWNLOAD_URL="https://mirrors.aliyun.com/composer/composer.phar"
+		;;
+
+esac
+
+
+
+test -f swoole-cli-v4.8.13-macos-x64.tar.xz || wget -O swoole-cli-v4.8.13-macos-x64.tar.xz ${SWOOLE_CLI_DOWNLOAD_URL}
+test -f swoole-cli-v4.8.13-macos-x64.tar ||  xz -d -k swoole-cli-v4.8.13-macos-x64.tar.xz
+
+test -f swoole-cli ||  tar -xvf  swoole-cli-v4.8.13-macos-x64.tar
 chmod a+x swoole-cli
 
-test -f composer.phar ||  wget -O composer.phar https://getcomposer.org/download/2.5.5/composer.phar
+test -f composer.phar ||  wget -O composer.phar ${COMPOSER_DOWNLOAD_URL}
 chmod a+x composer.phar
 
 mv ${__PROJECT__}/var/swoole-cli ${__PROJECT__}/bin/runtime/php
