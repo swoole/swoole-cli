42eab380d877d7db4004025915837810c22a0aaf
jingjingxyk <zonghengbaihe521@qq.com>
2023-04-29 00:26:11 +0800

    change build.yml

--- sapi/src/Preprocessor.php
+++ sapi/src/Preprocessor.php
@@ -671,7 +671,6 @@ EOF;
 
     protected function mkdirIfNotExists(string $dir, int $permissions = 0777, bool $recursive = false)
     {
-        var_dump(func_get_args());
         if (!is_dir($dir)) {
             mkdir($dir, $permissions, $recursive);
         }
