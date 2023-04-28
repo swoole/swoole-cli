c595385f6483ed76c21e6d8425e68034ef0cd990
好吧，你想说啥 <zonghengbaihe521@qq.com>
2023-04-28 10:19:33 +0800

    Fix bug (#117)
    * 修复bug
    
    * fix bug
    
    * 停用 libind2 库，等libintl 解决以后在启用
    
    * 解决 依赖库 多次编译 链接问题

--- sapi/src/constants.php
+++ sapi/src/constants.php
@@ -44,3 +44,4 @@ const DOWNLOAD_FILE_WAIT_RETRY = 5 ;
 const DOWNLOAD_FILE_USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36';
 
 define("LIBSSH2_PREFIX", $p->getGlobalPrefix() . '/libssh2');
+define("LIBUNISTRING_PREFIX", $p->getGlobalPrefix() . '/libunistring');
