0ba728263b3d9f96b5844279eb4ce2979bf48338
jingjingxyk <zonghengbaihe521@qq.com>
2023-04-30 00:25:48 +0800

    save changed

--- sapi/quickstart/linux/x86_64/run-debian-11-container.sh
+++ sapi/quickstart/linux/x86_64/run-debian-11-container.sh
@@ -19,7 +19,7 @@ cd ${__DIR__}
 }
 cd ${__DIR__}
 IMAGE=debian:11
-IMAGE=ubuntu:22.04
+#IMAGE=ubuntu:22.04
 
 cd ${__DIR__}
 docker run --rm --name swoole-cli-dev -d -v ${__PROJECT__}:/work -w /work $IMAGE tail -f /dev/null
