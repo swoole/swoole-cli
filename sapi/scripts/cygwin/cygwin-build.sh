06f3ab81a1f3894955bd26d1286f24e486019349
jingjingxyk <zonghengbaihe521@qq.com>
2023-05-01 15:37:02 +0800

    test cygwin build

--- sapi/scripts/cygwin/cygwin-build.sh
+++ sapi/scripts/cygwin/cygwin-build.sh
@@ -22,6 +22,8 @@ mkdir -p bin/.libs
 export CPPFLAGS="-I/usr/include"
 export LDFLAGS="-L/usr/lib -all-static"
 export EXTRA_LDFLAGS_PROGRAM="-all-static  -L/usr/lib"
+export LIBS=
+-L/usr/lib
 make -j $(nproc) cli
 
 ${__PROJECT__}/php-src/sapi/cli/php.exe -v
