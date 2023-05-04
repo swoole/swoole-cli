a0d5a5503fd321ce529c6ec8f638c348ade1a6eb
jingjingxyk <zonghengbaihe521@qq.com>
2023-05-04 15:50:37 +0800

    update build depend library container script

--- sapi/multistage-build-dependencies-container/all-dependencies-container-run.sh
+++ sapi/multistage-build-dependencies-container/all-dependencies-container-run.sh
@@ -14,7 +14,7 @@ __PROJECT__=$(
 cd ${__PROJECT__}/
 
 
-TAG='all-dependencies-alpine-20230427T114837Z'
+TAG='all-dependencies-alpine-x86_64-20230504T074925Z'
 
 IMAGE="phpswoole/swoole-cli-builder:1.6"
 IMAGE="docker.io/jingjingxyk/build-swoole-cli:${TAG}"
