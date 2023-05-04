92cfd3b79b5ea995330129b66c0e1e95312574b1
jingjingxyk <zonghengbaihe521@qq.com>
2023-05-04 18:24:15 +0800

    save changed

--- prepare.php
+++ prepare.php
@@ -1,4 +1,5 @@
 <?php
+
 require __DIR__ . '/vendor/autoload.php';
 
 use SwooleCli\Preprocessor;
@@ -7,8 +8,6 @@ use SwooleCli\Library;
 $homeDir = getenv('HOME');
 $p = Preprocessor::getInstance();
 $p->parseArguments($argc, $argv);
-
-
 
 
 // Compile directly on the host machine, not in the docker container
@@ -21,6 +20,11 @@ if ($p->getInputOption('without-docker')) {
 // Sync code from php-src
 //重新设置 PHP 源码所在目录
 $p->setPhpSrcDir($p->getWorkDir() . '/php-src');
+//设置PHP 安装目录和版本号
+$version = '8.2.4';
+define("BUILD_PHP_VERSION", $version);
+define("BUILD_PHP_INSTALL_PREFIX", $p->getWorkDir() . '/bin/php-' . $version);
+
 
 $build_type = $p->getInputOption('with-build-type');
 if (!in_array($build_type, ['dev', 'debug'])) {
@@ -42,19 +46,12 @@ $p->execute();
 
 function install_libraries($p): void
 {
-
-
-    //设置PHP 安装目录和版本号
-    $version = '8.2.4';
-    define("BUILD_PHP_VERSION", $version);
-    define("BUILD_PHP_INSTALL_PREFIX", $p->getWorkDir() . '/bin/php-' . $version);
-
     $php_install_prefix = BUILD_PHP_INSTALL_PREFIX;
     $php_src = $p->getPhpSrcDir();
     $build_dir = $p->getBuildDir();
     $p->addLibrary(
         (new Library('php_src'))
-            ->withUrl('https://github.com/php/php-src/archive/refs/tags/php-' . $version . '.tar.gz')
+            ->withUrl('https://github.com/php/php-src/archive/refs/tags/php-' . BUILD_PHP_VERSION . '.tar.gz')
             ->withHomePage('https://www.php.net/')
             ->withLicense('https://github.com/php/php-src/blob/master/LICENSE', Library::LICENSE_PHP)
             ->withPrefix($php_install_prefix)
