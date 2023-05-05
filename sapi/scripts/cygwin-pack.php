8b32d466d220315882940d7ba52888409e20a807
好吧，你想说啥 <zonghengbaihe521@qq.com>
2023-05-05 10:47:08 +0800

    fix cygwin pack (#121)
    * fix cygin pack
    
    * fix

--- sapi/scripts/cygwin-pack.php
+++ sapi/scripts/cygwin-pack.php
@@ -1,7 +1,7 @@
 <?php
 
-$src = dirname(__DIR__);
-$name = "swoole-cli-v".SWOOLE_VERSION."-cygwin-x64";
+$src = realpath(__DIR__ . '/../../');
+$name = "swoole-cli-v" . SWOOLE_VERSION . "-cygwin-x64";
 $dst = "/tmp/{$name}";
 if (!is_dir($dst)) {
     mkdir($dst, 0777, true);
@@ -13,24 +13,27 @@ $list = `ldd {$src}/bin/swoole-cli.exe`;
 
 preg_match_all($patten, $list, $match);
 
-if (!is_dir($dst.'/bin')) {
-    mkdir($dst.'/bin');
+if (!is_dir($dst . '/bin')) {
+    mkdir($dst . '/bin');
 }
-if (!is_dir($dst.'/etc')) {
-    mkdir($dst.'/etc');
+if (!is_dir($dst . '/etc')) {
+    mkdir($dst . '/etc');
 }
 
 foreach ($match[2] as $file) {
     if (str_starts_with($file, '/cygdrive/')) {
         continue;
     }
-    echo $file."\n";
-    copy($file, $dst."/bin/".basename($file));
+    echo $file . "\n";
+    copy($file, $dst . "/bin/" . basename($file));
 }
 
 echo `chmod a+x {$src}/bin/swoole-cli.exe`;
-copy("{$src}/bin/swoole-cli.exe", $dst."/bin/swoole-cli.exe");
-copy("{$src}/bin/LICENSE", $dst."/LICENSE");
+copy("{$src}/bin/swoole-cli.exe", $dst . "/bin/swoole-cli.exe");
+if (is_file("{$src}/bin/LICENSE")) {
+    copy("{$src}/bin/LICENSE", $dst . "/LICENSE");
+}
+
 echo `cp -rL /etc/pki/ {$dst}/etc`;
 echo "done.\n";
 
