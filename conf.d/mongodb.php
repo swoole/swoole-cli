a09c289a8dfb1ba4a622b7d6a43ec604a9013347
jingjingxyk <zonghengbaihe521@qq.com>
2023-05-09 14:36:50 +0800

    save changed

--- conf.d/mongodb.php
+++ conf.d/mongodb.php
@@ -53,12 +53,15 @@ EOF
     $p->withExportVariable('PHP_MONGODB_SSL_LIBS', '$(pkg-config   --libs   --static libcrypto libssl  openssl)');
     $p->withExportVariable('PHP_MONGODB_ICU_CFLAGS', '$(pkg-config --cflags --static icu-i18n  icu-io  icu-uc)');
     $p->withExportVariable('PHP_MONGODB_ICU_LIBS', '$(pkg-config   --libs   --static icu-i18n  icu-io  icu-uc)');
+    $options =' --enable-mongodb --with-mongodb-system-libs=no --with-mongodb-ssl=openssl  ';
+    $options .=' --with-mongodb-sasl=no';
+
     $p->addExtension(
         (new Extension('mongodb'))
             ->withHomePage('https://www.php.net/mongodb')
             ->withHomePage('https://www.mongodb.com/docs/drivers/php/')
             ->withOptions(
-                '--enable-mongodb --with-mongodb-system-libs=no --with-mongodb-ssl=openssl  --with-mongodb-sasl=no '
+                $options
             )
             ->withPeclVersion('1.14.2')
             ->depends('icu', 'openssl', 'zlib', 'libzstd', 'snappy')
