de518ecb32ace462f764d06a85a86ea1f6ac2a16
jingjingxyk <zonghengbaihe521@qq.com>
2023-05-09 16:14:16 +0800

    add libpq arg

--- conf.d/swoole.php
+++ conf.d/swoole.php
@@ -5,14 +5,23 @@ use SwooleCli\Preprocessor;
 use SwooleCli\Extension;
 
 return function (Preprocessor $p) {
+    $depends = ['curl', 'openssl', 'cares', 'zlib', 'brotli', 'nghttp2'];
+
     $options = '--enable-swoole --enable-sockets --enable-mysqlnd --enable-swoole-curl --enable-cares ';
     $options .= ' --with-brotli-dir=' . BROTLI_PREFIX;
     $options .= ' --with-nghttp2-dir=' . NGHTTP2_PREFIX;
+    if ($p->getInputOption('with-libpg')) {
+        $options .= ' --enable-swoole-pgsql';
+
+        $depends[] = 'pgsql';
+    }
+
+
     $p->addExtension(
         (new Extension('swoole'))
             ->withOptions($options)
             ->withLicense('https://github.com/swoole/swoole-src/blob/master/LICENSE', Extension::LICENSE_APACHE2)
             ->withHomePage('https://github.com/swoole/swoole-src')
-            ->depends('curl', 'openssl', 'cares', 'zlib', 'brotli', 'nghttp2')
+            ->depends($depends)
     );
 };
