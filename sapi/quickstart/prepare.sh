f8ac000165eebedf9a33b70f997aade024afd67e
jingjingxyk <zonghengbaihe521@qq.com>
2023-05-11 16:22:56 +0800

    save changed

--- sapi/quickstart/prepare.sh
+++ sapi/quickstart/prepare.sh
@@ -25,4 +25,5 @@ composer update
 php prepare.php --with-build-type=release +apcu +ds @macos
 
 # linux
-php prepare.php --with-build-type=release +apcu +ds
+php prepare.php --with-build-type=release +apcu +ds -zip -swoole -curl -gd -imagick -mongodb -redis
+php prepare.php --with-build-type=release  -zip -swoole -curl -gd -imagick -mongodb -redis -sqlite3 -pdo_sqlite -mysqli -pdo_mysql -soap -xsl -gmp -yaml  +xlswriter
