51ae69405dc51a89be012eac2daeecd87b836d72
jingjingxyk <zonghengbaihe521@qq.com>
2023-05-19 17:27:39 +0800

    fix

--- prepare.php
+++ prepare.php
@@ -5,7 +5,7 @@ require __DIR__ . '/vendor/autoload.php';
 use SwooleCli\Preprocessor;
 use SwooleCli\Library;
 
-const BUILD_PHP_VERSION = '8.1.12';
+const BUILD_PHP_VERSION = '8.2.4';
 
 $homeDir = getenv('HOME');
 $p = Preprocessor::getInstance();
