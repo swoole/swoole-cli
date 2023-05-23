4a3fa406d4148189c9efb02618360d9b2373dbe6
好吧，你想说啥 <zonghengbaihe521@qq.com>
2023-05-23 13:40:37 +0800

    Fix docs (#153)
    * fix docks link no found
    
    * 更新clone 地址 协议为 https
    
    * update downloadbox README.md
    
    * 更新macos 构建文档
    
    * 下载器 由 wget  更换为 curl
    
    * 把 wget  更换为 curl
    
    * curl 指定超时时间
    
    * 更新 bison 下载地址
    
    * macos build.yml 移除 无效配置
    
    * macos 环境下，取消卸载 brotli
    
    * 添加指定下载器为 wget 选项
    
    * 解决macos mongodb 扩展链接动态库
    
    * macos 环境下 自动设置 工作目录 和 构建目录
    
    * 移除无效注释
    
    * change cygwin git clone
    
    * update docs

--- sapi/src/Preprocessor.php
+++ sapi/src/Preprocessor.php
@@ -305,7 +305,14 @@ class Preprocessor
     {
         $retry_number = DOWNLOAD_FILE_RETRY_NUMBE;
         $wait_retry = DOWNLOAD_FILE_WAIT_RETRY;
-        echo $cmd = "wget   {$url}  -O {$file}  -t {$retry_number} --wait={$wait_retry} -T 15 ";
+        $connect_timeout = DOWNLOAD_FILE_CONNECTION_TIMEOUT;
+        echo PHP_EOL;
+        if ($this->getInputOption('with-downloader') === 'wget') {
+            $cmd = "wget   {$url}  -O {$file}  -t {$retry_number} --wait={$wait_retry} -T {$connect_timeout} ";
+        } else {
+            $cmd = "curl  --connect-timeout {$connect_timeout} --retry {$retry_number}  --retry-delay {$wait_retry}  -Lo '{$file}' '{$url}' ";
+        }
+        echo $cmd;
         echo PHP_EOL;
         echo `$cmd`;
         echo PHP_EOL;
@@ -667,7 +674,7 @@ class Preprocessor
         }
     }
 
-    public function generateFile(string $templateFile, string $outFile): bool 
+    public function generateFile(string $templateFile, string $outFile): bool
     {
         if (!is_file($templateFile)) {
             return false;
@@ -769,7 +776,7 @@ class Preprocessor
 
         if ($this->getInputOption('with-dependency-graph')) {
             $this->generateFile(
-                __DIR__ . '/template/extension_ependency_graph.php', 
+                __DIR__ . '/template/extension_ependency_graph.php',
                 $this->rootDir . '/bin/ext-dependency-graph.graphviz.dot'
             );
         }
