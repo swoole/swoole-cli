2e4fd57b2b70d5a2d5c8042cbf18cf7d0ccdcfc8
jingjingxyk <zonghengbaihe521@qq.com>
2023-05-27 12:41:45 +0800

    remove folder

--- conf.d.extra/pgsql.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use SwooleCli\Preprocessor;
-use SwooleCli\Extension;
-
-return function (Preprocessor $p) {
-   // $p->addExtension((new Extension('pgsql'))->withOptions('--with-pgsql=' . PGSQL_PREFIX)->depends('pgsql'));
-};
