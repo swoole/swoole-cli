227b1605395f1df69dbbd968c1b3de1efe90b70f
jingjingxyk <zonghengbaihe521@qq.com>
2023-05-28 13:47:04 +0800

    swoole delete external nghttp2

--- sapi/src/builder/extension/swoole.php
+++ sapi/src/builder/extension/swoole.php
@@ -5,7 +5,7 @@ use SwooleCli\Preprocessor;
 use SwooleCli\Extension;
 
 return function (Preprocessor $p) {
-    $depends = ['curl', 'openssl', 'cares', 'zlib', 'brotli', 'nghttp2'];
+    $depends = ['curl', 'openssl', 'cares', 'zlib', 'brotli'];
 
     $options = ' --enable-swoole --enable-sockets --enable-mysqlnd --enable-swoole-curl --enable-cares ';
     $options .= ' --with-openssl-dir=' . OPENSSL_PREFIX;
