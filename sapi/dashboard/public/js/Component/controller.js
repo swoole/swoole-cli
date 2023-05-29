d252ce67b2ea0c8d3e3c31629116ff41ddf5a1ea
jingjingxyk <zonghengbaihe521@qq.com>
2023-05-30 01:50:41 +0800

    fix webui bug

--- sapi/dashboard/public/js/Component/controller.js
+++ sapi/dashboard/public/js/Component/controller.js
@@ -5,9 +5,11 @@ let gen = (e) => {
     let os = document.querySelector('select[name="os"]')
     let with_docker = document.querySelector('select[name="without-docker"]')
     let with_global_prefix = document.querySelector('input[name="with-global-prefix"]')
+    let with_http_proxy = document.querySelector('input[name="with-http-proxy"]')
     let with_downloader = document.querySelector('select[name="with-downloader"]')
     let skip_download = document.querySelector('select[name="skip-download"]')
     let with_download_mirror_url = document.querySelector('select[name="with-download-mirror-url"]')
+    let conf_path = document.querySelector('input[name="conf-path"]')
     let with_dependecny_graph = document.querySelector('select[name="with-dependency-graph"]')
     let with_web_ui = document.querySelector('select[name="with-web-ui"]')
     let cmd = "php prepare.php"
@@ -23,27 +25,41 @@ let gen = (e) => {
             cmd += "  --without-docker=1"
         }
     }
+
     if (with_global_prefix.value !== '/usr/local/swoole-cli') {
         cmd += "  --with-global-prefix=" + with_global_prefix.value
+    }
+
+    if (with_http_proxy.value.length > 0) {
+        cmd += "  --with-http-proxy=" + with_http_proxy.value
     }
 
     if (with_downloader.value === 'wget') {
         cmd += "  --with-downloader=" + with_downloader.value
     }
+
     if (skip_download.value === "1") {
         cmd += "  --skip-download=" + skip_download.value
     }
+
     if (with_download_mirror_url.value !== "0") {
         cmd += "  --with-download-mirror-url=" + with_download_mirror_url.value
+    }
+
+    if (conf_path.value.length > 0) {
+        cmd += "  --conf-path=" + conf_path.value
     }
     if (with_dependecny_graph.value === "1") {
         cmd += "  --with-dependency-graph=" + with_dependecny_graph.value
     }
     if (with_web_ui.value === "1") {
         cmd += "  --with-web-ui=" + with_web_ui.value
+        if (skip_download.value === "0") {
+            cmd += "  --skip-download=1"
+        }
     }
 
-    let extenion_list_obj = document.querySelectorAll('ul[name="all_extentions"] input[type=checkbox]')
+    let extenion_list_obj = document.querySelectorAll('#all_extentions input[type=checkbox]')
     if (extenion_list_obj.length > 0) {
         let extension_list = []
         extenion_list_obj.forEach((value, key, parent) => {
@@ -52,7 +68,10 @@ let gen = (e) => {
             }
 
         })
-        let default_ready_extension_list = window['default_ready_exteion_list']
+        let default_ready_extension_list = JSON.parse(
+            document.querySelector('#all_extentions')
+                .getAttribute('data-default-ready-extension-list')
+        )
 
         //交集
         let intersect = extension_list.filter(x => default_ready_extension_list.includes(x))
@@ -80,14 +99,8 @@ let gen = (e) => {
     document.querySelector('.exec-button').setAttribute('data-cmd', cmd)
 
 }
-let ButtonController = (e) => {
-    let submmit_btn = document.querySelector('.generate-cmd-button')
-    if (submmit_btn) {
-        submmit_btn.addEventListener('click', gen)
-    }
-}
 
-let selectedBindEvent = () => {
+let bindEvent = () => {
     let option_list = document.querySelector('.options-list')
     if (option_list) {
         option_list.addEventListener('click', (event) => {
@@ -104,11 +117,16 @@ let selectedBindEvent = () => {
     document.querySelector('input[name="with-global-prefix"]').onchange = (event) => {
         gen()
     }
-
-}
-let runButtonBindEvent = () => {
-    let run = document.querySelector('.exec-button')
-    run.addEventListener('click', (event) => {
+    document.querySelector('input[name="with-http-proxy"]').onchange = (event) => {
+        gen()
+    }
+    document.querySelector('input[name="conf-path"]').onchange = (event) => {
+        gen()
+    }
+    document.querySelector('.generate-cmd-button').addEventListener('click', (e) => {
+        gen()
+    })
+    document.querySelector('.exec-button').addEventListener('click', (event) => {
         let cmd = event.target.getAttribute('data-cmd');
         console.log(cmd)
         let message = {
@@ -117,12 +135,12 @@ let runButtonBindEvent = () => {
         }
         send(JSON.stringify(message));
     })
+
 }
+
 let show_controller = () => {
-    ButtonController()
-    selectedBindEvent()
+    bindEvent()
     gen()
-    runButtonBindEvent()
     init()
 }
 export {show_controller}
--- sapi/dashboard/public/js/Component/extensions-box.js
+++ sapi/dashboard/public/js/Component/extensions-box.js
@@ -12,63 +12,69 @@ let get_content = async (url) => {
     return await response.json();
 }
 
-let show_all_extension_list_template = (data) => {
+let show_all_extension_list_template = (all_extension_list, ready_extension_list) => {
     let children = '  '
-    data.map((value, index, array) => {
+    all_extension_list.map((value, index, array) => {
         children += `
                 <li data-value="${value}">
-                <label>
-                 <input name="ext_list[]" type="checkbox" data-value="${value}" value="${value}" />
+                <label  for="ext_${value}" >
+                 <input type="checkbox" id="ext_${value}" value="${value}" />
                  ${value}
                 </label>
                 </li>
                 `
     });
-    return children;
-}
-let show_default_extension_list_template = (data) => {
-    let children = '  '
-    data.map((value, index, array) => {
-        document.querySelector(`ul[name="all_extentions"] li input[data-value="${value}"]`).checked = true
+    document.querySelector('#all_extentions').innerHTML = children;
+    ready_extension_list.map((value, index, array) => {
+        document.querySelector(`#ext_${value}`).checked = true
     });
+    document.querySelector('#all_extentions').setAttribute(
+        'data-default-ready-extension-list',
+        JSON.stringify(ready_extension_list)
+    )
 }
 
-let show_extension_list = async (all_extension_box) => {
+
+let show_extension_list = async () => {
     let [all_extension_list, ready_extension_list] = await Promise.all([
         get_content('/data/extension_list.json'),
         get_content('/data/default_extension_list.json')
     ])
-    window['default_ready_exteion_list'] = ready_extension_list;
-    all_extension_box.innerHTML = show_all_extension_list_template(all_extension_list)
-    show_default_extension_list_template(ready_extension_list)
+    show_all_extension_list_template(all_extension_list, ready_extension_list)
 }
 
-let inputCheckBoxBindEvent = (all_extension_box, ready_extension_list_box) => {
+let inputCheckBoxBindEvent = () => {
 
-    all_extension_box.addEventListener('click', (event) => {
-        if (event.target.nodeName === 'INPUT' || event.target.nodeName === 'LABEL') {
-            let element = event.target
-            if (element.nodeName === 'INPUT') {
-                if (element.checked) {
-                    delete element.checked
-                }
-            }
+    document.querySelector('#all_extentions').addEventListener('click', (event) => {
+        if (event.target.nodeName === 'INPUT') {
             document.querySelector('.generate-cmd-button').click()
-        } else {
-            //  event.stopPropagation();
-            //  event.preventDefault();
         }
-    }, false)
+        event.stopPropagation();
+        //event.preventDefault();
+    })
 
     let reset_button = document.querySelector('.reset-cmd-button')
     reset_button.addEventListener('click', (event) => {
 
-        let default_ready_extension_list = window['default_ready_exteion_list']
+        let default_ready_extension_list = JSON.parse(
+            document.querySelector('#all_extentions')
+                .getAttribute('data-default-ready-extension-list')
+        )
+        let button = event.target;
+        let checked = true;
+        if (button.getAttribute('data-status') === 'enable') {
+            button.setAttribute('data-status', "disable")
+            button.innerText = '启用默认扩展'
+            checked = false;
+        } else {
+            button.setAttribute('data-status', "enable")
+            button.innerText = '停用默认扩展'
+            checked = true;
+        }
+
         default_ready_extension_list.map((value) => {
-            let ele = document.querySelector(`ul[name="all_extentions"] li input[data-value="${value}"]`)
-            if (ele.checked === true) {
-                ele.checked = false;
-            }
+            let element = document.querySelector(`#ext_${value}`)
+            element.checked = checked;
         })
         event.stopPropagation();
         event.preventDefault();
@@ -78,9 +84,8 @@ let inputCheckBoxBindEvent = (all_extension_box, ready_extension_list_box) => {
 
 
 let extension_list = () => {
-    let all_extension_box = document.querySelector('ul[name="all_extentions"]')
-    show_extension_list(all_extension_box);
-    inputCheckBoxBindEvent(all_extension_box);
+    show_extension_list();
+    inputCheckBoxBindEvent();
 }
 
 export {extension_list}
