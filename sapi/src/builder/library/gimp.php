97474051ff86e66a5348b49ef11ac7f853d77c53
jingjingxyk <zonghengbaihe521@qq.com>
2023-06-10 17:14:17 +0800

    merge code

---
+++ sapi/src/builder/library/gimp.php
@@ -0,0 +1,38 @@
+<?php
+
+use SwooleCli\Library;
+use SwooleCli\Preprocessor;
+
+return function (Preprocessor $p) {
+    $aom_prefix = AOM_PREFIX;
+    $lib = new Library('gimp');
+    $lib->withHomePage('https://aomedia.googlesource.com/aom')
+        ->withLicense('https://aomedia.googlesource.com/aom/+/refs/heads/main/LICENSE', Library::LICENSE_SPEC)
+        ->withManual('https://aomedia.googlesource.com/aom')
+        ->withUrl('https://aomedia.googlesource.com/aom')
+        ->withFile('aom.tar.gz')
+        ->withDownloadScript(
+            'aom',
+            <<<EOF
+            git clone -b main --depth=1  https://aomedia.googlesource.com/aom
+EOF
+        )
+        ->withPrefix($aom_prefix)
+        ->withConfigure(
+            <<<EOF
+            mkdir -p build
+            cd build
+             cmake ..  \
+            -DCMAKE_INSTALL_PREFIX={$aom_prefix} \
+            -DCMAKE_BUILD_TYPE=Release  \
+            -DBUILD_SHARED_LIBS=OFF  \
+            -DBUILD_STATIC_LIBS=ON \
+            -DENABLE_DOCS=0 \
+            -DENABLE_TESTS=0
+EOF
+        )
+        ->withBinPath($aom_prefix . '/bin/')
+        ->withPkgName('aom');
+
+    $p->addLibrary($lib);
+};
