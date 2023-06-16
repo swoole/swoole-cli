a62600c5f9ef289592b1819624e519efcd622fe9
jingjingxyk <zonghengbaihe521@qq.com>
2023-06-16 13:25:26 +0800

    add test case

---
+++ sapi/src/UnitTest/SwoolePGSQLTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SwooleCli\UnitTest;
+
+use PHPUnit\Framework\TestCase;
+
+final class SwoolePGSQLTest extends TestCase
+{
+    public function testPGSQL(): void
+    {
+    }
+}
