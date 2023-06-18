d893316272478e1fc49946f0b0811906ae7d74cc
jingjingxyk <zonghengbaihe521@qq.com>
2023-06-18 14:50:11 +0800

    update install homebrew use mirror

--- sapi/quickstart/macos/install-homebrew.sh
+++ sapi/quickstart/macos/install-homebrew.sh
@@ -31,6 +31,13 @@ done
 
 case "$mirror" in
 china)
+  export HOMEBREW_INSTALL_FROM_API=1
+  export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
+  export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+  export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+  export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+  export HOMEBREW_NO_ANALYTICS=1
+  export HOMEBREW_NO_AUTO_UPDATE=1
   git clone --depth=1 https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/install.git brew-install
   /bin/bash brew-install/install.sh
   rm -rf brew-install
