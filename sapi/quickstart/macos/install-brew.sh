6c74ed6011ffb649915cab8c3e917bee1ce5b0ed
jingjingxyk <zonghengbaihe521@qq.com>
2023-06-18 14:40:08 +0800

    add install homebrew

---
+++ sapi/quickstart/macos/install-brew.sh
@@ -0,0 +1,41 @@
+!/bin/bash
+
+set -x
+__DIR__=$(
+  cd "$(dirname "$0")"
+  pwd
+)
+__PROJECT__=$(
+  cd ${__DIR__}/../../../
+  pwd
+)
+cd ${__PROJECT__}
+
+mkdir -p ${__PROJECT__}/var
+
+cd ${__PROJECT__}/var
+
+mirror=''
+while [ $# -gt 0 ]; do
+  case "$1" in
+  --mirror)
+    mirror="$2"
+    shift
+    ;;
+  --*)
+    echo "Illegal option $1"
+    ;;
+  esac
+  shift $(($# > 0 ? 1 : 0))
+done
+
+case "$mirror" in
+china)
+  git clone --depth=1 https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/install.git brew-install
+  /bin/bash brew-install/install.sh
+  rm -rf brew-install
+  ;;
+*)
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  ;;
+esac
