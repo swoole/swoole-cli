8f430e9fa3fb4813916c3702dc7c4e6388364eab
jingjingxyk <zonghengbaihe521@qq.com>
2023-06-18 23:09:28 +0800

    stop curl http2 feature check

--- sapi/src/UnitTest/MainTest.php
+++ sapi/src/UnitTest/MainTest.php
@@ -109,8 +109,8 @@ final class MainTest extends TestCase
         $this->assertEquals('Yes', trim($matches[1]), 'library: zlib no found');
         preg_match('/^SSL (?:=>)?(.*)$/m', $output, $matches);
         $this->assertEquals('Yes', trim($matches[1]), 'library: openssl no found');
-        preg_match('/^HTTP2 (?:=>)?(.*)$/m', $output, $matches);
-        $this->assertEquals('Yes', trim($matches[1]), 'library: nghttp2 no found');
+        # preg_match('/^HTTP2 (?:=>)?(.*)$/m', $output, $matches);
+        # $this->assertEquals('Yes', trim($matches[1]), 'library: nghttp2 no found');
         preg_match('/^BROTLI (?:=>)?(.*)$/m', $output, $matches);
         $this->assertEquals('Yes', trim($matches[1]), 'library: brotli no found');
         preg_match('/^libSSH (?:Version =>)?(.*)$/m', $output, $matches);
@@ -122,7 +122,7 @@ final class MainTest extends TestCase
 
     public $userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36';
 
-    public function testCurlHTTP2Client(): void
+    public function stopTestCurlHTTP2Client(): void
     {
         $url = 'https://www.jingjingxyk.com/';
         $userAgent = $this->userAgent;
