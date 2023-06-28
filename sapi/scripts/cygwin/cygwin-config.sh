32d777f37e2ff2be461ab8a911fc5bdd6e18db7f
jingjingxyk <zonghengbaihe521@qq.com>
2023-06-28 22:08:15 +0800

    验证 cygwin

--- sapi/scripts/cygwin/cygwin-config.sh
+++ sapi/scripts/cygwin/cygwin-config.sh
@@ -48,10 +48,11 @@ test -f Makefile && make clean
   --with-sodium \
   --enable-xml --enable-simplexml --enable-xmlreader --enable-xmlwriter --enable-dom --with-libxml \
   --enable-gd --with-jpeg --with-freetype \
-  --enable-swoole --enable-sockets --enable-mysqlnd --enable-swoole-curl --enable-cares --enable-swoole-pgsql \
+  --enable-swoole --enable-sockets --enable-mysqlnd --enable-swoole-curl --enable-cares  \
   --enable-redis \
   --with-imagick \
   --with-yaml \
   --with-readline \
 #  --with-pdo-pgsql \
 #  --with-pgsql
+# --enable-swoole-pgsql
