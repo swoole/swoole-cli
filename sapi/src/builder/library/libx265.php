9dbc212637b4866b4b0d3d18442aa4e9cf7e750d
jingjingxyk <zonghengbaihe521@qq.com>
2023-07-16 01:42:22 +0800

    update

--- sapi/src/builder/library/libx265.php
+++ sapi/src/builder/library/libx265.php
@@ -25,13 +25,20 @@ EOF
         ->withCleanPreInstallDirectory($libx265_prefix)
         ->withConfigure(
             <<<EOF
+            # apk add nasm
             test -d .git && rm -rf .git
             mkdir -p build
             cd build
-            # apk add nasm
+            # bug  -lgcc -lgcc_s -lc -lgcc -lgcc_s
+            # set(CMAKE_C_IMPLICIT_LINK_LIBRARIES "ssp_nonshared;gcc;gcc_s;c;gcc;gcc_s")  CMakeFiles/3.24.4/CMakeCCompiler.cmake
+            #sed -i.save s@\${CMAKE_C_IMPLICIT_LINK_LIBRARIES}@@ CMakeLists.txt
+
+
             cmake \
             -G"Unix Makefiles" ../source  \
             -DCMAKE_INSTALL_PREFIX={$libx265_prefix} \
+            -DCMAKE_INSTALL_LIBDIR={$libx265_prefix}/lib \
+            -DCMAKE_INSTALL_INCLUDEDIR={$libx265_prefix}/include \
             -DCMAKE_CURRENT_SOURCE_DIR={$build_dir}/libx265/source \
             -DCMAKE_C_COMPILER={$p->get_C_COMPILER()} \
             -DCMAKE_CXX_COMPILER={$p->get_CXX_COMPILER()} \
@@ -39,6 +46,7 @@ EOF
             -DCMAKE_BUILD_TYPE=Release  \
             -DBUILD_SHARED_LIBS=OFF  \
             -DBUILD_STATIC_LIBS=ON \
+            -DCMAKE_C_IMPLICIT_LINK_LIBRARIES="" \
             -DENABLE_LIBNUMA=ON \
             -DNuma_ROOT={$numa_prefix} \
             -DENABLE_SHARED=OFF
