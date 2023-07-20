2ed78af4a0d579b35ec832005e700c41c9f9767a
jingjingxyk <zonghengbaihe521@qq.com>
2023-07-20 16:14:02 +0800

    test libyuv

--- sapi/src/builder/library/aom.php
+++ sapi/src/builder/library/aom.php
@@ -18,7 +18,7 @@ return function (Preprocessor $p) {
 EOF
         )
         ->withPrefix($aom_prefix)
-        ->withBuildLibraryCached(false)
+        ->withBuildLibraryCached(true)
         ->withCleanBuildDirectory()
         ->withCleanPreInstallDirectory($aom_prefix)
         ->withConfigure(
