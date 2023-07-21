270c5297d01f06bf7b018eca92ac24365c60422a
jingjingxyk <zonghengbaihe521@qq.com>
2023-07-22 05:22:38 +0800

    更新 添加预安装命令，用于构建前准备必要的组件

--- sapi/src/Preprocessor.php
+++ sapi/src/Preprocessor.php
@@ -840,7 +840,7 @@ class Preprocessor
             $this->generateLibraryDownloadLinks();
         }
 
-        $this->generateFile(__DIR__ . '/template/make_init.php', $this->rootDir . '/make_init.sh');
+        $this->generateFile(__DIR__ . '/template/make-install-deps.php', $this->rootDir . '/make-install-deps.sh');
         $this->generateFile(__DIR__ . '/template/make.php', $this->rootDir . '/make.sh');
         $this->mkdirIfNotExists($this->rootDir . '/bin');
         $this->generateFile(__DIR__ . '/template/license.php', $this->rootDir . '/bin/LICENSE');
