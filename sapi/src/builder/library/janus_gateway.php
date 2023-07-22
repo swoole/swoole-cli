5897c97af02d1458b7f0fa9b334f99210434e027
jingjingxyk <zonghengbaihe521@qq.com>
2023-07-23 07:01:34 +0800

    save changed

--- sapi/src/builder/library/janus_gateway.php
+++ sapi/src/builder/library/janus_gateway.php
@@ -22,11 +22,37 @@ EOF
         ->withConfigure(
             <<<EOF
             sh autogen.sh
-            ./configure --prefix={$janus_gateway_prefix} \
-             --enable-websockets --enable-postprocessing \
-             --enable-docs --enable-rest \
+            PACKAGES="openssl libpq spandsp sofia-sip-ua odbc libjpeg libturbojpeg liblzma libpng sqlite3 zlib libcurl"
+            PACKAGES="\$PACKAGES libcares  libbrotlicommon libbrotlidec libbrotlienc"
+            PACKAGES="\$PACKAGES libnghttp2 libnghttp3 "
+            PACKAGES="\$PACKAGES libngtcp2 libngtcp2_crypto_openssl "
+
+            CPPFLAGS="$(pkg-config  --cflags-only-I --static \$PACKAGES ) " \
+            LDFLAGS="$(pkg-config   --libs-only-L   --static \$PACKAGES ) " \
+            LIBS="$(pkg-config      --libs-only-l   --static \$PACKAGES ) " \
+            ./configure \
+            --prefix={$janus_gateway_prefix} \
+             --enable-websockets \
+             --enable-postprocessing \
+             --enable-docs \
+             --enable-rest \
              --enable-data-channels
 EOF
+        )
+        ->withDependentLibraries(
+            'jansson',
+            'libnice',
+            'openssl',
+            'libsrtp',
+            'usrsctp',
+            'libmicrohttpd',
+            'libwebsockets',
+            'curl',
+            'sofia_sip',
+            'libopus',
+            'ogg',
+            'zlib',
+            'rabbitmq_c',
         )
         ;
 
