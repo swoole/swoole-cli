d66dafd97fb9128126cea2e7548227442af6c510
jingjingxyk <zonghengbaihe521@qq.com>
2023-07-24 07:38:00 +0800

    save changed

--- sapi/src/builder/library/hiredis.php
+++ sapi/src/builder/library/hiredis.php
@@ -12,6 +12,7 @@ return function (Preprocessor $p) {
             ->withLicense('https://github.com/digitalocean/prometheus-client-c/blob/master/LICENSE', Library::LICENSE_GPL)
             ->withUrl('https://github.com/redis/hiredis/archive/refs/tags/v1.1.0.tar.gz')
             ->withFile('hiredis-v1.1.0.tar.gz')
+            ->withPrefix($hiredis_prefix)
             ->withConfigure(
                 <<<EOF
 
