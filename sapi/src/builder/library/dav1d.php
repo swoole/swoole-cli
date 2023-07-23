954c1fdf6bd7853c7a265c7043dbd44b442d191b
jingjingxyk <zonghengbaihe521@qq.com>
2023-07-24 05:39:28 +0800

    save changed

--- sapi/src/builder/library/dav1d.php
+++ sapi/src/builder/library/dav1d.php
@@ -13,24 +13,47 @@ return function (Preprocessor $p) {
             ->withFile('dav1d-1.1.0.tar.gz')
             ->withManual('https://code.videolan.org/videolan/dav1d')
             ->withPrefix($dav1d_prefix)
+            ->withBuildLibraryCached(false)
+            ->withCleanBuildDirectory()
+            ->withCleanPreInstallDirectory($dav1d_prefix)
             ->withBuildScript(
                 <<<EOF
-                # apk add ninja python3 py3-pip  nasm
-                # pip3 install meson
-                mkdir -p build
-                cd build
-                meson setup \
-                --backend=ninja \
-                --prefix={$dav1d_prefix} \
-                --default-library=static \
-                ..
-                ninja
-                ninja install
+                meson  -h
+                meson setup -h
+                # meson configure -h
+
+                meson setup  build \
+                -Dprefix={$dav1d_prefix} \
+                -Dbackend=ninja \
+                -Dbuildtype=release \
+                -Ddefault_library=static \
+                -Db_staticpic=true \
+                -Db_pie=true \
+                -Dprefer_static=true \
+                -Denable_asm=true
+                -Denable_tools=true
+                -Denable_examples=true
+                -Denable_tests=false
+                -Denable_docs=false
+                -Dlogging=true
+                -Dfuzzing_engine=none
+
+                meson compile -C build
+
+                ninja -C build
+                ninja -C build install
 
 
 EOF
             )
             ->withPkgName('dav1d')
             ->withBinPath($dav1d_prefix . '/bin/')
+            ->withPreInstallCommand(
+                <<<EOF
+# library dav1d :
+apk add ninja python3 py3-pip  nasm
+pip3 install meson
+EOF
+            )
     );
 };
