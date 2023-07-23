eaa0b1f3ef3352c3c771153e95215c33110db662
jingjingxyk <zonghengbaihe521@qq.com>
2023-07-23 08:49:13 +0800

    save changed

--- sapi/src/builder/library/openssl.php
+++ sapi/src/builder/library/openssl.php
@@ -23,7 +23,7 @@ return function (Preprocessor $p) {
 
 EOF
             )
-            ->withMakeOptions('build_sw')
+            //->withMakeOptions('build_sw')
             ->withMakeInstallCommand('install_sw')
             ->withPkgName('libcrypto')
             ->withPkgName('libssl')
