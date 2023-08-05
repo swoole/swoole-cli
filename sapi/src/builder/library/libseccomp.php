c255716733a4db7db862413c395caa09d9fac567
jingjingxyk <zonghengbaihe521@qq.com>
2023-08-05 14:31:36 +0800

    save changed

---
+++ sapi/src/builder/library/libseccomp.php
@@ -0,0 +1,53 @@
+<?php
+
+use SwooleCli\Library;
+use SwooleCli\Preprocessor;
+
+return function (Preprocessor $p) {
+    $example_prefix = EXAMPLE_PREFIX;
+    $openssl_prefix = OPENSSL_PREFIX;
+    $lib = new Library('libseccomp');
+    $lib->withHomePage('https://github.com/seccomp/libseccomp.git')
+        ->withLicense('http://www.gnu.org/licenses/lgpl-2.1.html', Library::LICENSE_LGPL)
+        ->withUrl('https://github.com/opencv/opencv/archive/refs/tags/4.7.0.tar.gz')
+        ->withManual('https://github.com/opencv/opencv.git')
+        ->withFile('libseccomp-latest.tar.gz')
+        ->withAutoUpdateFile()
+        ->withDownloadScript(
+            'libseccomp',
+            <<<EOF
+                git clone -b main  --depth=1 https://github.com/seccomp/libseccomp.git
+EOF
+        )
+
+        ->withPrefix($example_prefix)
+        ->withCleanBuildDirectory()
+        ->withCleanPreInstallDirectory($example_prefix)
+        ->withBuildLibraryCached(false)
+        ->withConfigure(
+            <<<EOF
+            libtoolize -ci
+            autoreconf -fi
+            ./configure --help
+
+            PACKAGES='openssl  '
+            PACKAGES="\$PACKAGES zlib"
+
+            CPPFLAGS="$(pkg-config  --cflags-only-I  --static \$PACKAGES)" \
+            LDFLAGS="$(pkg-config   --libs-only-L    --static \$PACKAGES) -static" \
+            LIBS="$(pkg-config      --libs-only-l    --static \$PACKAGES)" \
+            ./configure \
+            --prefix={$example_prefix} \
+            --enable-shared=no \
+            --enable-static=yes
+EOF
+        )
+        ->withPkgName('libseccomp')
+        ->withBinPath($example_prefix . '/bin/');
+
+    $p->addLibrary($lib);
+
+    $p->withVariable('CPPFLAGS', '$CPPFLAGS -I' . $example_prefix . '/include');
+    $p->withVariable('LDFLAGS', '$LDFLAGS -L' . $example_prefix . '/lib');
+    $p->withVariable('LIBS', '$LIBS -lssl ');
+};
