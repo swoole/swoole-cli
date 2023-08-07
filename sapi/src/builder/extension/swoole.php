13453880227618e6b12e219ae1500d815461cfe8
jingjingxyk <zonghengbaihe521@qq.com>
2023-08-07 16:10:50 +0800

    update code

--- sapi/src/builder/extension/swoole.php
+++ sapi/src/builder/extension/swoole.php
@@ -23,10 +23,6 @@ return function (Preprocessor $p) {
         ->withManual('https://wiki.swoole.com/#/')
         ->withUrl('https://github.com/swoole/swoole-src/archive/refs/tags/v5.0.3.tar.gz')
         ->withFile('swoole-v5.0.3.tar.gz')
-        ->withDownloadWithOriginURL() //明确申明 使用源地址下载
-        ->withAutoUpdateFile() //明确声明，每次都执行下载，不使用已下载的缓存文件
-        ->withHttpProxy(false) //明确申明 不使用代理
-        ->withBuildLibraryCached(false) //明确申明 不使用缓存缓存目录  例子： ext/swoole (每次都全新解压源代码）
         ->withDependentExtensions('curl', 'openssl', 'sockets', 'mysqlnd', 'pdo');
     call_user_func_array([$ext, 'withDependentLibraries'], $depends);
     $p->addExtension($ext);
