4541f90cf0207edd1b37eb88347444ef49c931b7
jingjingxyk <zonghengbaihe521@qq.com>
2023-08-22 18:56:06 +0800

    test

--- sapi/src/builder/library/boost.php
+++ sapi/src/builder/library/boost.php
@@ -45,7 +45,7 @@ return function (Preprocessor $p) {
              --prefix={$boost_prefix} \
             variant=release \
             toolset={$p->get_C_COMPILER()} \
-            link=static    \
+            threading=multi link=static runtime-link=static
             cxxflags="-std=c++11 -stdlib=libstdc++" \
             linkflags="-stdlib=libstdc++" \
             headers  install
