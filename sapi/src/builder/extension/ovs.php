7c6addb95199d8c373024dcd10b09ce3a4861bab
jingjingxyk <zonghengbaihe521@qq.com>
2023-09-07 23:22:41 +0800

    update build ovs ovn document

--- sapi/src/builder/extension/ovs.php
+++ sapi/src/builder/extension/ovs.php
@@ -13,17 +13,23 @@ return function (Preprocessor $p) {
         ->withLicense('https://github.com/openvswitch/ovs/blob/master/LICENSE', Extension::LICENSE_APACHE2);
     call_user_func_array([$ext, 'withDependentLibraries'], $depends);
     $p->addExtension($ext);
+
+
     $p->setExtHook('ovs', function (Preprocessor $p) {
 
         $workdir = $p->getWorkDir();
         $builddir = $p->getBuildDir();
-
+        $ovs_prefix = OVS_PREFIX;
         $cmd = <<<EOF
                 mkdir -p {$workdir}/bin/
                 cd {$builddir}/ovs/Documentation
                 cp -rf _build {$workdir}/bin/ovs_docs
                 cd {$builddir}/ovs/
                 cp -rf dist-docs {$workdir}/bin/ovs_docs/
+                cd {$builddir}/ovs/
+                cd $ovs_prefix/../
+                tar -cJvf ovs-vlatest-static-linux-x64.tar.xz ovs
+                cp -f ovs-vlatest-static-linux-x64.tar.xz {$workdir}/bin/
 
 EOF;
 
