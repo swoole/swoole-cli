362e4b98ce242e27f8668c047b47cc7f988aa30f
jingjingxyk <zonghengbaihe521@qq.com>
2023-09-09 23:11:03 +0800

    save changed

--- sapi/src/builder/library/libxdp.php
+++ sapi/src/builder/library/libxdp.php
@@ -45,7 +45,7 @@ EOF
         ->withMakeOptions('libxdp')
         ->withPkgName('example')
         ->withBinPath($libxdp_prefix . '/bin/')
-        ->withDependentLibraries('libpcap')
+        ->withDependentLibraries('libpcap','zlib')
 
     ;
 
