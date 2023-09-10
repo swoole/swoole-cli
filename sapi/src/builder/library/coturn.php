d4feea5bc436f5818c567a1e6d4df8ee7591b353
jingjingxyk <zonghengbaihe521@qq.com>
2023-09-10 18:37:59 +0800

    test cutron

--- sapi/src/builder/library/coturn.php
+++ sapi/src/builder/library/coturn.php
@@ -39,15 +39,13 @@ EOF
            mkdir -p build
            cd build
 
-
-
            export TURN_NO_PROMETHEUS=ON
            export TURN_NO_SYSTEMD=ON
            export TURN_NO_MYSQL=ON
            export TURN_NO_MONGO=ON
-           export TURN_NO_SQLITE=OFF
-           export TURN_NO_PQ=OFF
-           export TURN_NO_HIREDIS=ON
+           # export TURN_NO_SQLITE=OFF
+           # export TURN_NO_PQ=OFF
+           # export TURN_NO_HIREDIS=ON
            export TURN_NO_SCTP=OFF
 
 
@@ -63,19 +61,24 @@ EOF
            -DCMAKE_DISABLE_FIND_PACKAGE_libsystemd=ON \
            -DCMAKE_DISABLE_FIND_PACKAGE_Prometheus=ON \
            -DCMAKE_DISABLE_FIND_PACKAGE_MySQL=ON \
+           -DCMAKE_DISABLE_FIND_PACKAGE_hiredis=ON \
            -DOPENSSL_USE_STATIC_LIBS=ON \
            -DBUILD_TEST=OFF \
            -DFUZZER=OFF \
-           -DCMAKE_PREFIX_PATH="{$openssl_prefix};{$libevent_prefix};{$sqlite3_prefix};{$pgsql_prefix}"
+           -DOpenSSL_ROOT={$openssl_prefix} \
+           -DLibevent_ROOT={$libevent_prefix} \
+           -DSQLite_DIR={$sqlite3_prefix} \
+           -DPostgreSQL_DIR={$pgsql_prefix}
 
-           # -DOpenSSL_ROOT={$openssl_prefix} \
-           # -DLibevent_ROOT={$libevent_prefix} \
-           # -DSQLite_DIR={$sqlite3_prefix} \
-           # -DPostgreSQL_DIR={$pgsql_prefix} \
+           # -DCMAKE_PREFIX_PATH="{$openssl_prefix};{$libevent_prefix};{$sqlite3_prefix};{$pgsql_prefix}" \
+
+           # -DCMAKE_STATIC_LINKER_FLAGS="-lpgcommon -lpgport " \
+
+
            # -Dhiredis_ROOT={$hiredis_prefix} \
            # -DCMAKE_C_FLAGS="-Werror -pedantic" \
 
-           # -DCMAKE_MODULE_LINKER_FLAGS="-lpgcommon -lpgport" \
+
            # -DCMAKE_STATIC_LINKER_FLAGS="" \
            # -DCMAKE_EXE_LINKER_FLAGS="-static " \
 
@@ -84,7 +87,7 @@ EOF
            # TURN_NO_THREAD_BARRIERS
            # TURN_NO_GCM
 
-           # make -j {$p->getMaxJob()}
+           make -j {$p->getMaxJob()}
            # make install
 
 
