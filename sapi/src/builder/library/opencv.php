2f789ae9b2dc75a006f892014cbd0911c7c72c5e
jingjingxyk <zonghengbaihe521@qq.com>
2023-09-14 00:41:51 +0800

    update opencv build config

--- sapi/src/builder/library/opencv.php
+++ sapi/src/builder/library/opencv.php
@@ -103,7 +103,7 @@ EOF
         -DBUILD_EXAMPLES=ON \
         -DBUILD_opencv_apps=ON \
         -DCMAKE_PREFIX_PATH="{$CMAKE_PREFIX_PATH}" \
-        -DCMAKE_REQUIRED_LIBRARIES="lzma  zstd  lz "
+        -DCMAKE_REQUIRED_LIBRARIES="-L{$liblzma_prefix}/lib/ -L{$libzstd_prefix}/lib/ -L{$liblz4_prefix}/lib/ -llzma  -lzstd  -llz4"
 
 
         # -DCMAKE_STATIC_LINKER_FLAGS="{$liblzma_prefix}/lib/liblzma.a {$libzstd_prefix}/lib/libzstd.a {$liblz4_prefix}/lib/liblz4.a"
