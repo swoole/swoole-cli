f52573882051413570fc7e9d121b87c9c245b2a9
jingjingxyk <zonghengbaihe521@qq.com>
2023-09-16 00:40:01 +0800

    升级 libavif dav1d nghttp3 ngtcp2 svt_av1 openssl-quic

--- sapi/src/builder/library/nghttp3.php
+++ sapi/src/builder/library/nghttp3.php
@@ -10,8 +10,8 @@ return function (Preprocessor $p) {
             ->withHomePage('https://github.com/ngtcp2/nghttp3')
             ->withLicense('https://github.com/ngtcp2/nghttp3/blob/main/COPYING', Library::LICENSE_MIT)
             ->withManual('https://nghttp2.org/nghttp3/')
-            ->withUrl('https://github.com/ngtcp2/nghttp3/archive/refs/tags/v0.9.0.tar.gz')
-            ->withFile('nghttp3-v0.9.0.tar.gz')
+            ->withUrl('https://github.com/ngtcp2/nghttp3/archive/refs/tags/v0.15.0.tar.gz')
+            ->withFile('nghttp3-v0.15.0.tar.gz')
             ->withPrefix($nghttp3_prefix)
             ->withConfigure(
                 <<<EOF
