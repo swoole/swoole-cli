f0214dc491ddcb654b71a16125c804734c5b67e5
jingjingxyk <zonghengbaihe521@qq.com>
2023-09-15 21:55:05 +0800

    update download-box config

--- sapi/download-box/download-box-init.sh
+++ sapi/download-box/download-box-init.sh
@@ -11,15 +11,36 @@ __PROJECT__=$(
   pwd
 )
 
-cd ${__PROJECT__}
+cd ${__PROJECT__}/
 
-test -d ${__PROJECT__}/var || mkdir -p ${__PROJECT__}/var
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+  --proxy)
+    export http_proxy="$2"
+    export https_proxy="$2"
+    export NO_PROXY="127.0.0.0/8,10.0.0.0/8,100.64.0.0/10,172.16.0.0/12,192.168.0.0/16,198.18.0.0/15,169.254.0.0/16"
+    export NO_PROXY="\${NO_PROXY},127.0.0.1,localhost"
+    shift
+    ;;
+  --*)
+    echo "Illegal option $1"
+    ;;
+  esac
+  shift $(($# > 0 ? 1 : 0))
+done
 
 
 export COMPOSER_ALLOW_SUPERUSER=1
-composer update --no-dev --optimize-autoloader
+composer update  --optimize-autoloader
 
-php prepare.php --with-build-type=release +ds +inotify +apcu +protobuf +pgsql +pdo_pgsql --with-swoole-pgsql=1 --without-docker=1 --with-skip-download=1
+php prepare.php --with-build-type=release +ds +inotify +apcu +protobuf +pgsql +pdo_pgsql \
+--with-swoole-pgsql=1 \
+--without-docker=1 --with-skip-download=1 \
+--with-dependency-graph=1 --with-libavif=1
+
+
+test -d ${__PROJECT__}/var || mkdir -p ${__PROJECT__}/var
 
 cd ${__PROJECT__}
 # 生成扩展依赖图
