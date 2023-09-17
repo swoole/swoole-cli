003abc73b3cac2270c150a74d1ea2587fce3f981
jingjingxyk <zonghengbaihe521@qq.com>
2023-09-18 00:34:13 +0800

    导入 FREETYPE2_CFLAGS FREETYPE2_LIBS 环境变量，解决编译单个扩展时链接不到brotlicommon

--- sapi/src/builder/extension/gd.php
+++ sapi/src/builder/extension/gd.php
@@ -11,4 +11,6 @@ return function (Preprocessor $p) {
             ->withOptions('--enable-gd --with-jpeg --with-freetype --with-webp')
             ->withDependentLibraries('libjpeg', 'freetype', 'libwebp', 'libpng', 'libgif')
     );
+    $p->withExportVariable('FREETYPE2_CFLAGS', '$(pkg-config  --cflags --static  libbrotlicommon libbrotlidec libbrotlienc freetype2 zlib libpng)');
+    $p->withExportVariable('FREETYPE2_LIBS', '$(pkg-config    --libs   --static  libbrotlicommon libbrotlidec libbrotlienc freetype2 zlib libpng)');
 };
