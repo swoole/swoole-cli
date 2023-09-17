8b1856ecb5d012fcd8730ea78efd71ce6d08689d
jingjingxyk <zonghengbaihe521@qq.com>
2023-09-17 15:17:38 +0800

    updtae dav1d aom

--- sapi/src/builder/extension/gd.php
+++ sapi/src/builder/extension/gd.php
@@ -12,8 +12,8 @@ return function (Preprocessor $p) {
         $options .= ' --with-avif ';
         $depends[] = 'libavif';
 
-        $p->withExportVariable('AVIF_CFLAGS', '$(pkg-config  --cflags --static libavif libbrotlicommon libbrotlidec libbrotlienc SvtAv1Enc SvtAv1Dec aom)');
-        $p->withExportVariable('AVIF_LIBS', '$(pkg-config    --libs   --static libavif libbrotlicommon libbrotlidec libbrotlienc SvtAv1Enc SvtAv1Dec aom)');
+        $p->withExportVariable('AVIF_CFLAGS', '$(pkg-config  --cflags --static libavif libbrotlicommon libbrotlidec libbrotlienc SvtAv1Enc SvtAv1Dec aom dav1d libgav1)');
+        $p->withExportVariable('AVIF_LIBS', '$(pkg-config    --libs   --static libavif libbrotlicommon libbrotlidec libbrotlienc SvtAv1Enc SvtAv1Dec aom dav1d libgav1)');
         $p->withVariable('LIBS', '$LIBS -lbrotli');
     }
 
