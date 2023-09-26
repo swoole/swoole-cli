67ce7f061acb623481f9a6e53ef8aa107eeb1c96
jingjingxyk <zonghengbaihe521@qq.com>
2023-09-26 17:34:46 +0800

    update pdo_sqlite

--- sapi/src/builder/extension/pdo_sqlite.php
+++ sapi/src/builder/extension/pdo_sqlite.php
@@ -4,10 +4,16 @@ use SwooleCli\Preprocessor;
 use SwooleCli\Extension;
 
 return function (Preprocessor $p) {
+    $php_version_id = BUILD_CUSTOM_PHP_VERSION_ID;
+    $options = '--with-pdo-sqlite';
+    if ($php_version_id < 7040) {
+        $options = '--with-pdo-sqlite=' . SQLITE3_PREFIX;
+    }
+
     $p->addExtension(
         (new Extension('pdo_sqlite'))
             ->withHomePage('https://www.php.net/pdo_sqlite')
-            ->withOptions('--with-pdo-sqlite')
+            ->withOptions($options)
             ->withDependentLibraries('sqlite3')
             ->withDependentExtensions('pdo')
     );
--- sapi/src/builder/extension/sqlite3.php
+++ sapi/src/builder/extension/sqlite3.php
@@ -5,16 +5,10 @@ use SwooleCli\Preprocessor;
 use SwooleCli\Extension;
 
 return function (Preprocessor $p) {
-    $php_version_id = BUILD_CUSTOM_PHP_VERSION_ID;
-    $options = '--with-sqlite3';
-    if ($php_version_id < 7040) {
-        $options = '--with-pdo-sqlite=' . SQLITE3_PREFIX;
-    }
-
     $p->addExtension(
         (new Extension('sqlite3'))
             ->withHomePage(' https://www.php.net/sqlite3')
-            ->withOptions($options)
+            ->withOptions('--with-sqlite3')
             ->withDependentLibraries('sqlite3')
     );
 };
