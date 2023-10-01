e4058a9ac446d5f084fbba48c09ae83612b936da
jingjingxyk <zonghengbaihe521@qq.com>
2023-10-02 01:42:27 +0800

    swoole upgrade to v5.1.0

--- sapi/src/builder/extension/swoole.php
+++ sapi/src/builder/extension/swoole.php
@@ -1,29 +1,28 @@
 <?php
 
-use SwooleCli\Library;
 use SwooleCli\Preprocessor;
 use SwooleCli\Extension;
 
 return function (Preprocessor $p) {
-    $depends = ['curl', 'openssl', 'cares', 'zlib', 'brotli', 'nghttp2'];
-
-    $options = '--enable-swoole --enable-sockets --enable-mysqlnd --enable-swoole-curl --enable-cares ';
+    $dependent_libraries = ['curl', 'openssl', 'cares', 'zlib', 'brotli', 'nghttp2', 'sqlite3', 'unix_odbc', 'pgsql'];
+    $dependent_extensions = ['curl', 'openssl', 'sockets', 'mysqlnd', 'pdo'];
+    $options = ' --enable-swoole --enable-sockets --enable-mysqlnd --enable-swoole-curl --enable-cares ';
+    $options .= ' --enable-swoole-coro-time --enable-thread-context ';
     $options .= ' --with-brotli-dir=' . BROTLI_PREFIX;
     $options .= ' --with-nghttp2-dir=' . NGHTTP2_PREFIX;
-
-    if ($p->getInputOption('with-swoole-pgsql')) {
-        $options .= ' --enable-swoole-pgsql';
-        $depends[] = 'pgsql';
-    }
+    $options .= ' --enable-swoole-sqlite --enable-swoole-pgsql ';
+    $options .= ' --with-swoole-odbc=unixODBC,' . UNIX_ODBC_PREFIX . ' ';
 
     $ext = (new Extension('swoole'))
-        ->withOptions($options)
-        ->withLicense('https://github.com/swoole/swoole-src/blob/master/LICENSE', Extension::LICENSE_APACHE2)
         ->withHomePage('https://github.com/swoole/swoole-src')
+        ->withLicense('https://github.com/swoole/swoole-src/blob/master/LICENSE', Extension::LICENSE_APACHE2)
         ->withManual('https://wiki.swoole.com/#/')
-        ->withUrl('https://github.com/swoole/swoole-src/archive/refs/tags/v5.0.3.tar.gz')
-        ->withFile('swoole-v5.0.3.tar.gz')
-        ->withDependentExtensions('curl', 'openssl', 'sockets', 'mysqlnd', 'pdo');
-    call_user_func_array([$ext, 'withDependentLibraries'], $depends);
+        ->withOptions($options)
+        ->withManual('https://wiki.swoole.com/#/')
+        ->withUrl('https://github.com/swoole/swoole-src/archive/refs/tags/v5.1.0.tar.gz')
+        ->withFile('swoole-v5.1.0.tar.gz')
+        ->withBuildLibraryCached(false);
+    call_user_func_array([$ext, 'withDependentLibraries'], $dependent_libraries);
+    call_user_func_array([$ext, 'withDependentExtensions'], $dependent_extensions);
     $p->addExtension($ext);
 };
