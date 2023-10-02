3968afd8119c8b7825a738105cc832266f0dc23f
jingjingxyk <zonghengbaihe521@qq.com>
2023-10-02 12:37:35 +0800

    update gd config

--- sapi/src/builder/extension/gd.php
+++ sapi/src/builder/extension/gd.php
@@ -1,12 +1,23 @@
 <?php
 
-use SwooleCli\Library;
 use SwooleCli\Preprocessor;
 use SwooleCli\Extension;
 
 return function (Preprocessor $p) {
     $options = '--enable-gd --with-jpeg --with-freetype --with-webp ';
     $dependent_libraries = ['libjpeg', 'freetype', 'libwebp', 'libpng', 'libgif'];
+
+    if ($p->getInputOption('with-libavif')) {
+        $options .= ' --with-avif ';
+        $dependent_libraries[] = 'libavif';
+        if ($p->getOsType() == 'macos') {
+            $libcpp = '-lc++';
+        } else {
+            $libcpp = '-lstdc++';
+        }
+        $p->withExportVariable('AVIF_CFLAGS', '$(pkg-config  --cflags --static libavif libbrotlicommon libbrotlidec libbrotlienc SvtAv1Enc SvtAv1Dec aom dav1d libgav1)');
+        $p->withExportVariable('AVIF_LIBS', '$(pkg-config    --libs   --static libavif libbrotlicommon libbrotlidec libbrotlienc SvtAv1Enc SvtAv1Dec aom dav1d libgav1) ' . $libcpp);
+    }
 
     $ext = (new Extension('gd'))
         ->withHomePage('https://www.php.net/manual/zh/book.image.php')
