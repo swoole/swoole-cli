6b9d0252087df6d39a2f898c9c412c6436e957fe
jingjingxyk <zonghengbaihe521@qq.com>
2023-10-10 16:55:50 +0800

    update

--- sapi/src/library_builder/library_install_builder.php
+++ sapi/src/library_builder/library_install_builder.php
@@ -34,29 +34,7 @@ function install_openssl_v3(Preprocessor $p)
 
 function install_openssl_v3_quic(Preprocessor $p)
 {
-    $openssl_prefix = OPENSSL_PREFIX;
-    $static = $p->getOsType() === 'macos' ? '' : ' -static --static';
-    $p->addLibrary(
-        (new Library('openssl'))
-            ->withHomePage('https://www.openssl.org/')
-            ->withLicense('https://github.com/openssl/openssl/blob/master/LICENSE.txt', Library::LICENSE_APACHE2)
-            ->withManual('https://www.openssl.org/docs/')
-            ->withUrl('https://github.com/quictls/openssl/archive/refs/tags/openssl-3.0.8-quic1.tar.gz')
-            ->withPrefix($openssl_prefix)
-            ->withConfigure(
-                <<<EOF
-                 # ./Configure LIST
-                ./config {$static} no-shared  enable-tls1_3 --release --prefix={$openssl_prefix} --libdir=${openssl_prefix}/lib
 
-EOF
-            )
-            ->withMakeOptions('build_sw')
-            ->withMakeInstallCommand('install_sw')
-            ->withPkgName('libcrypto')
-            ->withPkgName('libssl')
-            ->withPkgName('openssl')
-            ->withBinPath($openssl_prefix . '/bin/')
-    );
 }
 
 function install_libiconv(Preprocessor $p): void
