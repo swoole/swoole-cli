08eed6ed8f0abc29ab2f89bd2807c2d2bfef3b85
jingjingxyk <zonghengbaihe521@qq.com>
2023-10-11 17:09:11 +0800

    test

--- sapi/src/builder/library/libeigen.php
+++ sapi/src/builder/library/libeigen.php
@@ -41,6 +41,7 @@ EOF
             -DCMAKE_BUILD_TYPE=Release  \
             -DBUILD_SHARED_LIBS=OFF  \
             -DBUILD_STATIC_LIBS=ON \
+            -DCMAKE_POLICY_DEFAULT_CMP0074=NEW \
             -DFFTW_ROOT={$fftw3_prefix} \
             -DCMAKE_PREFIX_PATH="{$suitesparse_prefix};{$boost_prefix};{$fftw3_prefix};{$mpfr_prefix}"
 
