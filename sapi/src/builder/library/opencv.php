aab133cfc7c5b46d37bd2e80d103d601944ebfbe
jingjingxyk <zonghengbaihe521@qq.com>
2023-11-04 04:14:39 +0800

    update

--- sapi/src/builder/library/opencv.php
+++ sapi/src/builder/library/opencv.php
@@ -190,7 +190,7 @@ EOF
         -DOPENCV_EXTRA_MODULE_LINKER_FLAG="-Wl,--no-dynamic-linker -Wl,-Bstatic \${LIBS}" \
         -DCMAKE_EXE_LINKER_FLAGS_INIT=" \${REQUIRED_LIBRARIES} " \
         -DWEBP_INCLUDE_DIR={$libwebp_prefix}/include/ \
-        -DWEBP_LIBRARY={$libwebp_prefix}/lib/libsharpyuv.a \
+        -DWEBP_LIBRARY="{$libwebp_prefix}/lib/libwebp.a {$libwebp_prefix}/lib/libsharpyuv.a "\
 
 
 
