1b9676c499e9df2df226e19a836accb64d5ef04f
jingjingxyk <zonghengbaihe521@qq.com>
2023-11-09 23:14:00 +0800

    update

--- sapi/quickstart/macos/macos-init.sh
+++ sapi/quickstart/macos/macos-init.sh
@@ -40,19 +40,13 @@ case "$MIRROR" in
     export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles"
 
     export HOMEBREW_PIP_INDEX_URL="https://pypi.tuna.tsinghua.edu.cn/simple"
-    export PIPENV_PYPI_MIRROR=https://pypi.tuna.tsinghua.edu.cn/simple
+    export PIPENV_PYPI_MIRROR="https://pypi.tuna.tsinghua.edu.cn/simple"
 
     # pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
     # pip3 config set global.index-url https://pypi.python.org/simple
 
     # 参考文档： https://help.mirrors.cernet.edu.cn/homebrew/
     ;;
-tuna)
-    export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
-    export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
-    export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
-    export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
-  ;;
 esac
 
 
