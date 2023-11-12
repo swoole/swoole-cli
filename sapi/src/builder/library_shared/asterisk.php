492808c8a0d133f3118f4e83b8b5a58280d59985
jingjingxyk <zonghengbaihe521@qq.com>
2023-11-12 15:25:24 +0800

    update

---
+++ sapi/src/builder/library_shared/asterisk.php
@@ -0,0 +1,59 @@
+<?php
+
+use SwooleCli\Library;
+use SwooleCli\Preprocessor;
+
+return function (Preprocessor $p) {
+    $asterisk_prefix = ASTERISK_PREFIX;
+    $lib = new Library('asterisk');
+    $lib->withHomePage('https://www.asterisk.org/')
+        ->withLicense('http://www.gnu.org/licenses/lgpl-2.1.html', Library::LICENSE_LGPL)
+        ->withManual('https://github.com/asterisk/asterisk')
+        ->withManual('https://github.com/asterisk/dahdi-linux/wiki')
+        ->withFile('asterisk-latest.tar.gz')
+        ->withDownloadScript(
+            'asterisk',
+            <<<EOF
+                git clone -b master --depth=1 https://github.com/asterisk/asterisk
+EOF
+        )
+        ->withPrefix($asterisk_prefix)
+        ->withCleanBuildDirectory()
+        ->withCleanPreInstallDirectory($asterisk_prefix)
+        ->withBuildCached(false)
+        ->withConfigure(
+            <<<EOF
+            ./configure --help
+
+            PACKAGES='openssl  '
+            PACKAGES="\$PACKAGES zlib"
+            PACKAGES="\$PACKAGES libsrtp2"
+
+            CPPFLAGS="$(pkg-config  --cflags-only-I  --static \$PACKAGES)" \
+            LDFLAGS="$(pkg-config   --libs-only-L    --static \$PACKAGES)" \
+            LIBS="$(pkg-config      --libs-only-l    --static \$PACKAGES)" \
+            ./configure \
+            --prefix={$asterisk_prefix} \
+            --without-pjproject-bundled \
+            --enable-shared=no \
+            --enable-static=yes
+EOF
+        )
+
+        ->withBinPath($asterisk_prefix . '/bin/')
+        ->withDependentLibraries(
+            'libpcap',
+            'openssl',
+            'zlib',
+           // 'libpri',
+            //'dahdi_linux',
+            //'dahdi_tools',
+            //'dahdi_complete',
+            'pjproject',
+            'libsrtp'
+        )
+
+    ;
+
+    $p->addLibrary($lib);
+};
---
+++ sapi/src/library_builder/README.md
@@ -0,0 +1,1 @@
+CNC(数控机床)是计算机数字控制机床(Computer Numerical Control)的简称
