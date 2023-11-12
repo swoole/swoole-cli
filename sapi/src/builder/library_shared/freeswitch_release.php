e01c277ee2e2b4979fe3d0ace767a75e7fae5075
jingjingxyk <zonghengbaihe521@qq.com>
2023-11-12 18:35:20 +0800

    update

--- sapi/src/builder/library_shared/freeswitch_release.php
+++ sapi/src/builder/library_shared/freeswitch_release.php
@@ -43,12 +43,16 @@ EOF
         apt-get update  -y
         apt-get install -y gnupg2 wget lsb-release
 
-        wget --http-user=signalwire --http-password=\$TOKEN -O /usr/share/keyrings/signalwire-freeswitch-repo.gpg https://freeswitch.signalwire.com/repo/deb/debian-release/signalwire-freeswitch-repo.gpg
+        if [ ! -f /etc/apt/sources.list.d/freeswitch.list ] ; then
 
-        echo "machine freeswitch.signalwire.com login signalwire password \$TOKEN" > /etc/apt/auth.conf
-        chmod 600 /etc/apt/auth.conf
-        echo "deb [signed-by=/usr/share/keyrings/signalwire-freeswitch-repo.gpg] https://freeswitch.signalwire.com/repo/deb/debian-release/ `lsb_release -sc` main" > /etc/apt/sources.list.d/freeswitch.list
-        echo "deb-src [signed-by=/usr/share/keyrings/signalwire-freeswitch-repo.gpg] https://freeswitch.signalwire.com/repo/deb/debian-release/ `lsb_release -sc` main" >> /etc/apt/sources.list.d/freeswitch.list
+            wget --http-user=signalwire --http-password=\$TOKEN -O /usr/share/keyrings/signalwire-freeswitch-repo.gpg https://freeswitch.signalwire.com/repo/deb/debian-release/signalwire-freeswitch-repo.gpg
+
+            echo "machine freeswitch.signalwire.com login signalwire password \$TOKEN" > /etc/apt/auth.conf
+            chmod 600 /etc/apt/auth.conf
+            echo "deb [signed-by=/usr/share/keyrings/signalwire-freeswitch-repo.gpg] https://freeswitch.signalwire.com/repo/deb/debian-release/ `lsb_release -sc` main" > /etc/apt/sources.list.d/freeswitch.list
+            echo "deb-src [signed-by=/usr/share/keyrings/signalwire-freeswitch-repo.gpg] https://freeswitch.signalwire.com/repo/deb/debian-release/ `lsb_release -sc` main" >> /etc/apt/sources.list.d/freeswitch.list
+
+        fi
 
         # you may want to populate /etc/freeswitch at this point.
         # if /etc/freeswitch does not exist, the standard vanilla configuration is deployed
