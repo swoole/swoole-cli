8be97b41485aa2ee566da31f9e10259c78d6fa6d
jingjingxyk <zonghengbaihe521@qq.com>
2023-11-14 06:11:24 +0800

    update

--- sapi/src/builder/library/nginx.php
+++ sapi/src/builder/library/nginx.php
@@ -24,7 +24,7 @@ return function (Preprocessor $p) {
             ->withDocumentation('https://nginx.org/en/docs/')
             //->withUrl('http://nginx.org/download/nginx-1.25.2.tar.gz')
             //->withAutoUpdateFile()
-            ->withFile('nginx-1.25.2.tar.gz')
+            ->withFile('nginx-latest.tar.gz')
             ->withDownloadScript(
                 'nginx',
                 <<<EOF
