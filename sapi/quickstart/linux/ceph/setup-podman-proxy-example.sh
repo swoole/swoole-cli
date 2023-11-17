4080c7185ba1c14fae7af43a344e362366764a98
jingjingxyk <zonghengbaihe521@qq.com>
2023-11-18 00:02:17 +0800

    use cri-socket

--- sapi/quickstart/linux/ceph/setup-podman-proxy-example.sh
+++ sapi/quickstart/linux/ceph/setup-podman-proxy-example.sh
@@ -7,3 +7,9 @@ export NO_PROXY=0.0.0.0/8,10.0.0.0/8,100.64.0.0/10,127.0.0.0/8,172.16.0.0/12,192
 
 
 podman pull registry.k8s.io/pause:3.9
+
+#  docker.io (docker hub公共镜像库)
+#  gcr.io (Google container registry)
+#  registry.k8s.io (等同于gcr.io/google-containers)
+#  quay.io (Red Hat运营的镜像库)
+#  ghcr.io (github 运营的镜像库)
