982aa7a2c9962233b1c4af7fc45092d03def31be
jingjingxyk <zonghengbaihe521@qq.com>
2023-11-17 18:40:59 +0800

    update

--- sapi/quickstart/linux/kubernetes/nginx-ingress.sh
+++ sapi/quickstart/linux/kubernetes/nginx-ingress.sh
@@ -1,4 +1,21 @@
 
+while [ $# -gt 0 ]; do
+  case "$1" in
+  --proxy)
+    export HTTP_PROXY="$2"
+    export HTTPS_PROXY="$2"
+    NO_PROXY="127.0.0.0/8,10.0.0.0/8,100.64.0.0/10,172.16.0.0/12,192.168.0.0/16"
+    NO_PROXY="${NO_PROXY},127.0.0.1,localhost"
+    export NO_PROXY="${NO_PROXY}"
+    ;;
+
+  --*)
+    echo "Illegal option $1"
+    ;;
+  esac
+  shift $(($# > 0 ? 1 : 0))
+done
+
 # nginx ingress
 # https://kubernetes.github.io/ingress-nginx/deploy/#bare-metal-clusters
 
