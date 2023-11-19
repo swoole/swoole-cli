9f14329e5fadf4b03bfcacd4c5072396dd4b99e2
jingjingxyk <zonghengbaihe521@qq.com>
2023-11-20 01:48:19 +0800

    init kubernetes

--- sapi/quickstart/linux/kubernetes/kubeadm-init.sh
+++ sapi/quickstart/linux/kubernetes/kubeadm-init.sh
@@ -29,8 +29,8 @@ sysctl net.bridge.bridge-nf-call-iptables net.bridge.bridge-nf-call-ip6tables ne
 
 
 kubeadm config images list --v=5 --kubernetes-version=$(kubelet --version |  awk -F ' ' '{print $2}')
-kubeadm config images pull --v=5 --kubernetes-version=$(kubelet --version |  awk -F ' ' '{print $2}') --cri-socket="unix:///var/run/containerd/containerd.sock"
-# kubeadm config images pull --v=5 --kubernetes-version=$(kubelet --version |  awk -F ' ' '{print $2}') --cri-socket="unix:///var/run/containerd/containerd.sock"
+kubeadm config images pull --v=5 --kubernetes-version=$(kubelet --version |  awk -F ' ' '{print $2}') --cri-socket "unix:///var/run/containerd/containerd.sock"
+# kubeadm config images pull --v=5 --kubernetes-version=$(kubelet --version |  awk -F ' ' '{print $2}') --cri-socket "unix:///var/run/containerd/containerd.sock"
 
 IP=$(ip address show | grep eth0 | grep 'inet' | awk '{print $2}' | awk -F '/' '{print $1}')
 # ip=$(ip address show | grep enp0s3 | grep 'inet' | awk '{print $2}' | awk -F '/' '{print $1}')
@@ -44,13 +44,13 @@ kubeadm init  \
 --token-ttl 0 \
 --v=5 \
 --apiserver-advertise-address="${IP}" \
---cri-socket="unix:///var/run/containerd/containerd.sock"
+--cri-socket "unix:///var/run/containerd/containerd.sock"
 
 # --control-plane-endpoint='control-plane-endpoint-api.intranet.jingjingxyk.com:6443'
 #--apiserver-advertise-address="${ip}"
 
-# --cri-socket="unix:///var/run/containerd/containerd.sock"
-# --cri-socket="unix:///var/run/cri-dockerd.sock"
+# --cri-socket "unix:///var/run/containerd/containerd.sock"
+# --cri-socket "unix:///var/run/cri-dockerd.sock"
 
 
 mkdir -p $HOME/.kube
