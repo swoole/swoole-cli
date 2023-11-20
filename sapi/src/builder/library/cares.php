1664d254284d41091c7bf3d27fb3d2e0a6fa5040
jingjingxyk <zonghengbaihe521@qq.com>
2023-11-20 14:54:24 +0800

    update cares

--- sapi/src/builder/library/cares.php
+++ sapi/src/builder/library/cares.php
@@ -10,6 +10,7 @@ return function (Preprocessor $p) {
             ->withManual('https://c-ares.org/')
             ->withLicense('https://c-ares.org/license.html', Library::LICENSE_MIT)
             ->withUrl('https://c-ares.org/download/c-ares-1.20.1.tar.gz')
+            ->withUrl('https://github.com/c-ares/c-ares/releases/download/cares-1_22_1/c-ares-1.22.1.tar.gz')
             ->withPrefix(CARES_PREFIX)
             ->withConfigure('./configure --prefix=' . CARES_PREFIX . ' --enable-static --disable-shared')
             ->withPkgName('libcares')
