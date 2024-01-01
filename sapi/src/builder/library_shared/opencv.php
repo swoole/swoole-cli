20047b4331cd411d4e8c639fb6224f603b8bfeda
jingjingxyk <zonghengbaihe521@qq.com>
2024-01-01 18:06:24 +0800

    update

--- sapi/src/builder/library_shared/opencv.php
+++ sapi/src/builder/library_shared/opencv.php
@@ -90,7 +90,7 @@ EOF
         -DOPENCV_GENERATE_PKGCONFIG=ON \
         -DBUILD_TESTS=OFF \
         -DBUILD_PERF_TESTS=OFF \
-        -DBUILD_EXAMPLES=OFF \
+        -DBUILD_EXAMPLES=ON \
         -DBUILD_opencv_apps=ON \
         -DBUILD_opencv_js=OFF \
         -DBUILD_JAVA=OFF \
