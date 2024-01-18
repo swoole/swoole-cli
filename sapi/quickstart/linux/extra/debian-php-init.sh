0a1447963523f58c748d0fe8a6db88c7053d0e97
jingjingxyk <zonghengbaihe521@qq.com>
2024-01-18 23:50:43 +0800

    update workflow mips64le riscv64

--- sapi/quickstart/linux/extra/debian-php-init.sh
+++ sapi/quickstart/linux/extra/debian-php-init.sh
@@ -1,6 +1,6 @@
 
 apt install -y  php-cli php-pear php-dev php-curl php-intl
-apt install -y  php-iconv php-mbstring php-openssl php-posix  php-tokenizer php-dom php-xmlwriter php-xml php-simplexml
+apt install -y   php-mbstring php-openssl  php-tokenizer  php-xml
 
 
 # apt install -y  php-mysqlnd php-pgsql php-sqlite3  php-redis php-mongodb
