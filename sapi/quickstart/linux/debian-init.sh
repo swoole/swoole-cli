2fc641dec63eef113b94d9fa1d7cd6f63af578ca
jingjingxyk <zonghengbaihe521@qq.com>
2024-03-16 16:36:31 +0800

    update

--- sapi/quickstart/linux/debian-init.sh
+++ sapi/quickstart/linux/debian-init.sh
@@ -110,7 +110,11 @@ ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 apt install -y git curl wget ca-certificates
 apt install -y xz-utils autoconf automake clang-tools clang lld libtool cmake bison re2c gettext coreutils lzip zip unzip
 apt install -y pkg-config bzip2 flex p7zip
-apt install -y gcc g++ musl-tools libtool-bin autopoint
+apt install -y gcc g++  libtool-bin autopoint
+apt install -y linux-headers-generic
+apt install -y musl-dev musl-tools
+
+# apt install -y linux-headers-$(uname -r)
 
 
 # apt install build-essential linux-headers-$(uname -r)
