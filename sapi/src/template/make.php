4e3555c05bb5846fb4a2b22f6403632a4457dd34
jingjingxyk <zonghengbaihe521@qq.com>
2024-03-17 14:01:03 +0800

    test

--- sapi/src/template/make.php
+++ sapi/src/template/make.php
@@ -227,7 +227,6 @@ export_variables() {
 <?php endif; ?>
 
 <?php if ($this->isLinux() && ($this->get_C_COMPILER() == 'musl-gcc')) : ?>
-    set +x
     #  -I/usr/include/x86_64-linux-gnu/c++/12 -I/usr/include/x86_64-linux-gnu
     export CPPFLAGS="$CPPFLAGS   -I/usr/include/x86_64-linux-musl  "
     export LDFLAGS="$LDFLAGS -static -fpie -static-pie -static-libstdc++ -L/usr/lib/gcc/x86_64-linux-gnu/12/ -L/usr/lib/x86_64-linux-musl/ "
