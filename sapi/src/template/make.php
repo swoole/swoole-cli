a515e44b6b064204e134b06945376068c760e6b6
jingjingxyk <zonghengbaihe521@qq.com>
2024-03-18 10:12:14 +0800

    test

--- sapi/src/template/make.php
+++ sapi/src/template/make.php
@@ -229,8 +229,8 @@ export_variables() {
 <?php if ($this->isLinux() && ($this->get_C_COMPILER() == 'musl-gcc')) : ?>
     #  -I/usr/include/x86_64-linux-gnu/c++/12  -I/usr/include/x86_64-linux-musl
     export CPPFLAGS="$CPPFLAGS  -I/usr/include/x86_64-linux-gnu/  -I/usr/include/  "
-    # -L/usr/lib/x86_64-linux-musl/ -static-pie -fpie
-    export LDFLAGS="$LDFLAGS -static   -static-libstdc++ -L/usr/lib/gcc/x86_64-linux-gnu/12/ -L/usr/lib/x86_64-linux-gnu/ "
+    #  -static-pie -fpie
+    export LDFLAGS="$LDFLAGS -static   -static-libstdc++ -L/usr/lib/gcc/x86_64-linux-gnu/12/ -L/usr/lib/x86_64-linux-gnu/ -L/usr/lib/x86_64-linux-musl/ "
 <?php endif ;?>
 
     export CPPFLAGS=$(echo $CPPFLAGS | tr ' ' '\n' | sort | uniq | tr '\n' ' ')
