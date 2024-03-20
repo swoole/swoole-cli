c5da2db28d3c036f5f0592a892ae3454fcc6c371
jingjingxyk <zonghengbaihe521@qq.com>
2024-03-20 16:21:37 +0800

    update

--- sapi/src/builder/library/musl_cross_make.php
+++ sapi/src/builder/library/musl_cross_make.php
@@ -5,6 +5,25 @@ use SwooleCli\Preprocessor;
 
 return function (Preprocessor $p) {
     $musl_cross_make_prefix = MUSL_CROSS_MAKE_PREFIX;
+    $make_options = "TARGET='x86_64-linux-musl' ";
+    $make_options .= "OUTPUT='{$musl_cross_make_prefix}' ";
+    $make_options .= "DL_CMD='curl -C - -L -o' ";
+    $make_options .= "GCC_VER='11.2.0' ";
+
+    $make_common_options = 'CC="x86_64-linux-musl-gcc -static --static" ';
+    $make_common_options .= 'CXX="x86_64-linux-musl-g++ -static --static" ';
+    $make_common_options .= 'CFLAGS="-g0 -Os" ';
+    $make_common_options .= 'CXXFLAGS="-g0 -Os" ';
+    $make_common_options .= 'LDFLAGS="-s" ';
+
+    $make_gcc_options = '--disable-libquadmath --disable-decimal-float ';
+    $make_gcc_options .= '--disable-libitm ';
+    $make_gcc_options .= '--disable-fixed-point ';
+    $make_gcc_options .= '--enable-languages=c,c++ ';
+    $make_gcc_options .= '--disable-nls ';
+
+    $make_options = $make_options . "COMMON_CONFIG='{$make_common_options}' ";
+    $make_options = $make_options . "GCC_CONFIG='{$make_gcc_options}' ";
 
     //文件名称 和 库名称一致
     $lib = new Library('musl_cross_make');
@@ -17,15 +36,14 @@ return function (Preprocessor $p) {
             'musl-cross-make',
             <<<EOF
                 git clone -b master  --depth=1 https://github.com/richfelker/musl-cross-make.git
+
 EOF
         )
         ->withPrefix($musl_cross_make_prefix)
         ->withBuildLibraryHttpProxy()
-        ->withMakeOptions("TARGET='x86_64-linux-musl' OUTPUT={$musl_cross_make_prefix} DL_CMD='curl -C - -L -o'")
-        ->withMakeInstallOptions("OUTPUT={$musl_cross_make_prefix}")
-        ->withBinPath($musl_cross_make_prefix . '/bin/')
-
-    ;
+        ->withBuildCached(false)
+        ->withMakeOptions($make_options)
+        ->withBinPath($musl_cross_make_prefix . '/bin/');
 
     $p->addLibrary($lib);
 
