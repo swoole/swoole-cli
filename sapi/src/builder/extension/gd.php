1bf9d5426f1e241a6b8d811902ab93061c323243
jingjingxyk <zonghengbaihe521@qq.com>
2024-03-20 17:03:54 +0800

    add build musl-libc

--- sapi/src/builder/extension/gd.php
+++ sapi/src/builder/extension/gd.php
@@ -11,9 +11,9 @@ return function (Preprocessor $p) {
     if ($p->getInputOption('with-libavif')) {
         $options .= ' --with-avif ';
         $dependent_libraries[] = 'libavif';
-        $libcpp = '-lc++';
+        $libs = $p->isMacos() ? '-lc++' : ' -lstdc++ ';
         $p->withExportVariable('AVIF_CFLAGS', '$(pkg-config  --cflags --static libavif libbrotlicommon libbrotlidec libbrotlienc SvtAv1Enc SvtAv1Dec aom dav1d libgav1)');
-        $p->withExportVariable('AVIF_LIBS', '$(pkg-config    --libs   --static libavif libbrotlicommon libbrotlidec libbrotlienc SvtAv1Enc SvtAv1Dec aom dav1d libgav1) ' . $libcpp);
+        $p->withExportVariable('AVIF_LIBS', '$(pkg-config    --libs   --static libavif libbrotlicommon libbrotlidec libbrotlienc SvtAv1Enc SvtAv1Dec aom dav1d libgav1) ' . $libs);
     }
 
     $ext = (new Extension('gd'))
