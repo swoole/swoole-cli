e2f21b1414b620a087100a03ba91dc1ffe24cc31
jingjingxyk <zonghengbaihe521@qq.com>
2024-03-20 19:34:02 +0800

    test

--- sapi/src/builder/library/pgsql.php
+++ sapi/src/builder/library/pgsql.php
@@ -5,6 +5,7 @@ use SwooleCli\Preprocessor;
 
 return function (Preprocessor $p) {
     $pgsql_prefix = PGSQL_PREFIX;
+    $ldflags = $p->isMacos() ? '' : ' -static  ';
     $libs = $p->isMacos() ? '-lc++' : ' -lstdc++ ';
     $p->addLibrary(
         (new Library('pgsql'))
