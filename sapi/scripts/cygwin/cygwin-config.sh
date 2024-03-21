6292b8f04ab3c4424790228ba069388438633616
jingjingxyk <zonghengbaihe521@qq.com>
2024-03-21 18:29:52 +0800

    update

--- sapi/scripts/cygwin/cygwin-config.sh
+++ sapi/scripts/cygwin/cygwin-config.sh
@@ -25,46 +25,46 @@ cd ${__PROJECT__}/php-src/
 ./buildconf --force
 test -f Makefile && make clean
 ./configure --prefix=/usr --disable-all \
-    --disable-fiber-asm \
-    --enable-opcache \
-    --without-pcre-jit \
-    --with-openssl --enable-openssl \
-    --with-curl \
-    --with-iconv \
-    --enable-intl \
-    --with-bz2 \
-    --enable-bcmath \
-    --enable-filter \
-    --enable-session \
-    --enable-tokenizer \
-    --enable-mbstring \
-    --enable-ctype \
-    --with-zlib \
-    --with-zip \
-    --enable-posix \
-    --enable-sockets \
-    --enable-pdo \
-    --with-sqlite3 \
-    --enable-phar \
-    --enable-pcntl \
-    --enable-mysqlnd \
-    --with-mysqli \
-    --enable-fileinfo \
-    --with-pdo_mysql \
-    --enable-soap \
-    --with-xsl \
-    --with-gmp \
-    --enable-exif \
-    --with-sodium \
-    --enable-xml --enable-simplexml --enable-xmlreader --enable-xmlwriter --enable-dom --with-libxml \
-    --enable-gd --with-jpeg  --with-freetype \
-    --enable-swoole --enable-sockets --enable-mysqlnd --enable-swoole-curl --enable-cares \
-    --enable-swoole-pgsql \
-    --enable-swoole-sqlite \
-    --enable-redis \
-    --with-imagick \
-    --with-yaml \
-    --with-readline
+  --disable-fiber-asm \
+  --enable-opcache \
+  --without-pcre-jit \
+  --with-openssl --enable-openssl \
+  --with-curl \
+  --with-iconv \
+  --enable-intl \
+  --with-bz2 \
+  --enable-bcmath \
+  --enable-filter \
+  --enable-session \
+  --enable-tokenizer \
+  --enable-mbstring \
+  --enable-ctype \
+  --with-zlib \
+  --with-zip \
+  --enable-posix \
+  --enable-sockets \
+  --enable-pdo \
+  --with-sqlite3 \
+  --enable-phar \
+  --enable-pcntl \
+  --enable-mysqlnd \
+  --with-mysqli \
+  --enable-fileinfo \
+  --with-pdo_mysql \
+  --enable-soap \
+  --with-xsl \
+  --with-gmp \
+  --enable-exif \
+  --with-sodium \
+  --enable-xml --enable-simplexml --enable-xmlreader --enable-xmlwriter --enable-dom --with-libxml \
+  --enable-gd --with-jpeg  --with-freetype \
+  --enable-swoole --enable-sockets --enable-mysqlnd --enable-swoole-curl --enable-cares \
+  --enable-swoole-pgsql \
+  --enable-swoole-sqlite \
+  --enable-redis \
+  --with-imagick \
+  --with-yaml \
+  --with-readline
 
 # --with-zip   #  cygwin libzip-devel 版本库暂不支持函数 zip_encryption_method_supported （2020年新增函数)
 # --with-pdo-pgsql \
