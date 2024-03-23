8f04d4f63feacfe864bdd62eedb803b10461c7fa
jingjingxyk <zonghengbaihe521@qq.com>
2024-03-23 11:50:23 +0800

    验证 swoole v6.0

---
+++ sapi/src/builder/extension/swoole_v6000.php
@@ -0,0 +1,49 @@
+<?php
+
+use SwooleCli\Preprocessor;
+use SwooleCli\Extension;
+
+return function (Preprocessor $p) {
+
+    $swoole_tag = 'v6.0';
+    $file = "swoole-{$swoole_tag}.tar.gz";
+
+    $url = "https://github.com/swoole/swoole-src/archive/refs/tags/{$swoole_tag}.tar.gz";
+
+    $dependentLibraries = ['curl', 'openssl', 'cares', 'zlib', 'brotli', 'nghttp2', 'sqlite3', 'unix_odbc', 'pgsql'];
+    $dependentExtensions = ['curl', 'openssl', 'sockets', 'mysqlnd', 'pdo'];
+    $options = ' --enable-swoole --enable-sockets --enable-mysqlnd --enable-swoole-curl --enable-cares ';
+    $options .= ' --enable-swoole-coro-time ';
+    $options .= ' --enable-thread-context ';
+    $options .= ' --with-brotli-dir=' . BROTLI_PREFIX;
+    $options .= ' --with-nghttp2-dir=' . NGHTTP2_PREFIX;
+    $options .= ' --enable-swoole-pgsql ';
+    $options .= ' --enable-swoole-sqlite ';
+    $options .= ' --with-swoole-odbc=unixODBC,' . UNIX_ODBC_PREFIX . ' ';
+
+
+    $ext = (new Extension('swoole'))
+        ->withHomePage('https://github.com/swoole/swoole-src')
+        ->withLicense('https://github.com/swoole/swoole-src/blob/master/LICENSE', Extension::LICENSE_APACHE2)
+        ->withManual('https://wiki.swoole.com/#/')
+        ->withOptions($options)
+        ->withFile($file)
+        ->withDownloadScript(
+            'swoole-src',
+            <<<EOF
+            git clone -b {$swoole_tag} --depth=1 https://github.com/swoole/swoole-src.git
+EOF
+        )
+        ->withBuildCached(false)
+        ->withDependentLibraries(...$dependentLibraries)
+        ->withDependentExtensions(...$dependentExtensions)
+    ;
+
+    //call_user_func_array([$ext, 'withDependentLibraries'], $dependentLibraries);
+    //call_user_func_array([$ext, 'withDependentExtensions'], $dependentExtensions);
+
+    $p->addExtension($ext);
+
+    $libs = $p->isMacos() ? '-lc++' : ' -lstdc++ ';
+    $p->withVariable('LIBS', '$LIBS ' . $libs);
+};
