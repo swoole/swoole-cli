c04debc0766ed27dd76ee065205f880face43ea3
jingjingxyk <zonghengbaihe521@qq.com>
2024-03-30 17:30:38 +0800

    test

--- sapi/src/builder/extension/swoole_v6000.php
+++ sapi/src/builder/extension/swoole_v6000.php
@@ -37,6 +37,7 @@ return function (Preprocessor $p) {
 EOF
         )
         ->withBuildCached(false)
+        ->withAutoUpdateFile()
         ->withDependentLibraries(...$dependentLibraries)
         ->withDependentExtensions(...$dependentExtensions)
     ;
