7b76f4d80c8465a56e2eb318a9a2f712a8cab8db
jingjingxyk <zonghengbaihe521@qq.com>
2024-04-15 15:46:45 +0800

    fix phpy static build bug

--- sapi/src/builder/extension/phpy.php
+++ sapi/src/builder/extension/phpy.php
@@ -34,7 +34,7 @@ EOF
 
     $libs = $p->isMacos() ? '-lc++' : ' -lstdc++ ';
     $p->withVariable('LIBS', '$LIBS ' . $libs);
-    $p->withVariable('CPPFLAGS', '$CPPFLAGS -I' . $p->getPhpSrcDir(). '/ext/phpy/include');
+    $p->withVariable('CPPFLAGS', '$CPPFLAGS -I' . $p->getWorkDir(). '/ext/phpy/include');
 
 
 };
