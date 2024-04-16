c8bedae142af64d687a93ec5bca8a825d786bc86
jingjingxyk <zonghengbaihe521@qq.com>
2024-04-16 15:25:00 +0800

    优化composer配置

--- build-release-example.sh
+++ build-release-example.sh
@@ -131,7 +131,9 @@ if [ ${WITH_PHP_COMPOSER} -eq 1 ] ; then
     # composer suggests --all
     # composer dump-autoload
 
-    composer update  --optimize-autoloader
+    # composer update  --optimize-autoloader
+    composer install  --no-interaction --no-autoloader --no-scripts # --no-dev
+    composer dump-autoload --optimize --profile
     composer config -g --unset repos.packagist
 fi
 
