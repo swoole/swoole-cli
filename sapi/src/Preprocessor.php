e04a5c07d92cc8b8f00c1ae7aefb7895787ab053
jingjingxyk <zonghengbaihe521@qq.com>
2024-04-21 13:27:43 +0800

    updae hash signature

--- sapi/src/Preprocessor.php
+++ sapi/src/Preprocessor.php
@@ -391,9 +391,9 @@ class Preprocessor
             } else {
                 echo "[Library] file cached: " . $lib->file . PHP_EOL;
                 if ($this->getInputOption('show-tarball-hash')) {
-                    echo "md5:   " . hash_file('md5', $lib->path) . PHP_EOL;
-                    echo "sha1:  " . hash_file('sha1', $lib->path) . PHP_EOL;
-                    echo "sha256:" . hash_file('sha256', $lib->path) . PHP_EOL;
+                    echo "md5:    " . hash_file('md5', $lib->path) . PHP_EOL;
+                    echo "sha1:   " . hash_file('sha1', $lib->path) . PHP_EOL;
+                    echo "sha256: " . hash_file('sha256', $lib->path) . PHP_EOL;
                 }
             }
         }
@@ -434,9 +434,9 @@ class Preprocessor
                 } else {
                     echo "[Extension] file cached: " . $ext->file . PHP_EOL;
                     if ($this->getInputOption('show-tarball-hash')) {
-                        echo "md5:   " . hash_file('md5', $ext->path) . PHP_EOL;
-                        echo "sha1:  " . hash_file('sha1', $ext->path) . PHP_EOL;
-                        echo "sha256:" . hash_file('sha256', $ext->path) . PHP_EOL;
+                        echo "md5:    " . hash_file('md5', $ext->path) . PHP_EOL;
+                        echo "sha1:   " . hash_file('sha1', $ext->path) . PHP_EOL;
+                        echo "sha256: " . hash_file('sha256', $ext->path) . PHP_EOL;
                     }
                 }
                 $dst_dir = "{$this->rootDir}/ext/{$ext->name}";
