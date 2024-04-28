8e4866b88a672d7c5fdaf032cd4eff6229619e4a
jingjingxyk <zonghengbaihe521@qq.com>
2024-04-28 10:58:03 +0800

    fix macos-init.sh

--- sapi/quickstart/macos/macos-init.sh
+++ sapi/quickstart/macos/macos-init.sh
@@ -86,4 +86,4 @@ ln -sf /usr/local/bin/glibtoolize /usr/local/bin/libtoolize
 
 brew uninstall --ignore-dependencies --force snappy
 brew uninstall --ignore-dependencies --force capstone
-brew cleanup --prune-prefix
+
