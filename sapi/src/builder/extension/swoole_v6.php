5942e5231d1210c55cb263e3bb1e49928d47823c
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-01 13:44:21 +0800

    验证 swoole v6

---
+++ sapi/src/builder/extension/swoole_v6.php
@@ -0,0 +1,61 @@
+<?php
+
+use SwooleCli\Preprocessor;
+use SwooleCli\Extension;
+
+return function (Preprocessor $p) {
+
+    $swoole_tag = 'v6.0';
+    $file = "swoole-{$swoole_tag}.tar.gz";
+
+    $url = "https://github.com/swoole/swoole-src/archive/refs/tags/{$swoole_tag}.tar.gz";
+
+    $dependentLibraries = ['curl', 'openssl', 'cares', 'zlib', 'brotli', 'nghttp2', 'sqlite3', 'unix_odbc', 'pgsql'];
+    $dependentExtensions = ['curl', 'openssl', 'sockets', 'mysqlnd', 'pdo'];
+    $options = ' --enable-swoole --enable-sockets --enable-mysqlnd --enable-swoole-curl --enable-cares ';
+    $options .= ' --enable-swoole-coro-time ';
+    # $options .= ' --enable-thread-context ';
+    $options .= ' --with-brotli-dir=' . BROTLI_PREFIX;
+    $options .= ' --with-nghttp2-dir=' . NGHTTP2_PREFIX;
+    $options .= ' --enable-swoole-pgsql ';
+    $options .= ' --enable-swoole-sqlite ';
+    $options .= ' --with-swoole-odbc=unixODBC,' . UNIX_ODBC_PREFIX . ' ';
+    $options .= ' --enable-swoole-thread ' ;
+    $options .= ' --enable-zts ' ;
+
+    $ext = (new Extension('swoole_v6'))
+        ->withHomePage('https://github.com/swoole/swoole-src')
+        ->withLicense('https://github.com/swoole/swoole-src/blob/master/LICENSE', Extension::LICENSE_APACHE2)
+        ->withManual('https://wiki.swoole.com/#/')
+        ->withOptions($options)
+        ->withBuildCached(false)
+        ->withDependentLibraries(...$dependentLibraries)
+        ->withDependentExtensions(...$dependentExtensions)
+    ;
+
+    $p->addExtension($ext);
+
+    $libs = $p->isMacos() ? '-lc++' : ' -lstdc++ ';
+    $p->withVariable('LIBS', '$LIBS ' . $libs);
+
+
+    // 扩展钩子 写法
+    $p->withBeforeConfigureScript('swoole_v6', function (Preprocessor $p) {
+        $workdir = $p->getWorkDir();
+        $cmd = <<<EOF
+        cd {$workdir}
+        mkdir -p {$workdir}/ext/
+        mkdir -p {$workdir}var/cache/
+        cd {$workdir}/var/cache/
+        git clone -b v6.0 --depth=1  https://github.com/swoole/swoole-src.git
+        if [ -d {$workdir}/ext/swoole ] ; then
+            rm -rf {$workdir}/ext/swoole
+        fi
+        mv swoole-src {$workdir}/ext/swoole
+
+EOF;
+
+        return $cmd;
+    });
+
+};
