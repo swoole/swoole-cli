372e4127462f11de7b57965a2c71db4e57bc2c65
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-02 19:59:19 +0800

    add macos php-init.sh

---
+++ sapi/quickstart/macos/extra/php-init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+__DIR__=$(
+  cd "$(dirname "$0")"
+  pwd
+)
+__PROJECT__=$(
+  cd ${__DIR__}/../../../../
+  pwd
+)
+cd ${__PROJECT__}
+
+
+export HOMEBREW_NO_ANALYTICS=1
+export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_INSTALL_FROM_API=1
+
+brew install php
+
