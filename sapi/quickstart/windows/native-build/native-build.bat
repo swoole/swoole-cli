f664258fa3943a7545c12de450c8656c66d6a146
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-08 12:06:44 +0800

    update

--- sapi/quickstart/windows/native-build/native-build.bat
+++ sapi/quickstart/windows/native-build/native-build.bat
@@ -1,10 +1,18 @@
 @echo off
 echo %~dp0
+SET  CURRENT_DIR=%~dp0
+cd %~dp0
 
-cd php-sdk-binary-tools
+cd ..\..\..\..\
+echo %cd%
+
+SET PROJECT_DIR=echo %cd%
+
+cd %PROJECT_DIR%\php-sdk-binary-tools
+
 phpsdk-vs17-x64.bat
 
-cd php-src
+cd %PROJECT_DIR%\php-src
 
 buildconf
 configure --help
