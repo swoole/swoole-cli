ef21079fb7bcb47f7d244c5e11b99eb8748efcb6
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-08 12:17:24 +0800

    update

--- sapi/quickstart/windows/native-build/msys2-download-source-code.sh
+++ sapi/quickstart/windows/native-build/msys2-download-source-code.sh
@@ -15,7 +15,11 @@ test -d php-src && rm -rf php-src
 test -d php-sdk-2.2.0 && rm -rf php-sdk-binary-tools
 
 
-git clone -b php-8.3.6     --depth=1 https://github.com/php/php-src.git
+# git clone -b php-8.3.6     --depth=1 https://github.com/php/php-src.git
+curl -Lo php-8.3.7.tar.gz  https://github.com/php/php-src/archive/refs/tags/php-8.3.7.tar.gz
+mkdir -p php-src
+tar --strip-components=1 -C php-src -xf php-8.3.7.tar.gz
+
 # git clone -b php-sdk-2.2.0 --depth=1 https://github.com/php/php-sdk-binary-tools.git
 git clone -b master --depth=1 https://github.com/php/php-sdk-binary-tools.git
 
