0c129d664a5be6006b3d81845ad0c1f4249b4844
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-21 15:59:55 +0800

    update

--- setup-php-runtime.sh
+++ setup-php-runtime.sh
@@ -112,7 +112,7 @@ if [ $OS = 'windows' ]; then
     #cp -f ${SWOOLE_CLI_RUNTIME}/bin/swoole-cli.exe ${SWOOLE_CLI_RUNTIME}/bin/php.exe
     test -d ${__PROJECT__}/runtime/ && rm -rf ${__PROJECT__}/runtime/
     mv ${SWOOLE_CLI_RUNTIME}/ ${__PROJECT__}/runtime/
-    cp -f ${__PROJECT__}/runtime/swoole-cli.exe ${__PROJECT__}/runtime/php.exe
+    cp -f ${__PROJECT__}/runtime/bin/swoole-cli.exe ${__PROJECT__}/runtime/bin/php.exe
     echo
     exit 0
   }
