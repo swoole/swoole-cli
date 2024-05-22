b17a89f0b379e8af70246bccf4615243b281b591
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-22 19:27:18 +0800

    update

---
+++ sapi/quickstart/windows/zlib.bat
@@ -0,0 +1,17 @@
+@echo off
+
+cmake --version
+
+mkdir thirdparty/zlib
+
+tar --strip-components=1 -C thirdparty/zlib  -xf pool/lib/zlib-1.3.tar.gz
+
+cd thirdparty/zlib
+mkdir build
+cd build
+cmake .. ^
+-DCMAKE_BUILD_TYPE=Release  ^
+-DBUILD_SHARED_LIBS=OFF  ^
+-DBUILD_STATIC_LIBS=ON
+
+cmake --build . --config Release --target install
