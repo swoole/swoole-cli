0da3184dbb732bb1d0bfe1b67a651a26c2658b80
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-22 21:50:11 +0800

    update

---
+++ sapi/quickstart/windows/build-php.bat
@@ -0,0 +1,9 @@
+@echo off
+
+
+ configure ^
+ --disable-all  --disable-cgi --enable-cli ^
+--enable-sockets  --enable-mbstring  --enable-ctype  --enable-pdo --enable-phar  ^
+--enable-xmlreader  --enable-xmlwriter ^
+--with-zlib --with-zlib-dir="C:/Program Files (x86)/zlib/" ^
+--with-openssl --with-openssl-dir=" C:/Program Files/OpenSSL/"
