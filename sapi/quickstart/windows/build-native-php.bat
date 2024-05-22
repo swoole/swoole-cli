2cbabbe3536e0ab84f6899eef637f9ec34bc9e4b
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-23 01:08:24 +0800

    update

--- sapi/quickstart/windows/build-native-php.bat
+++ sapi/quickstart/windows/build-native-php.bat
@@ -2,6 +2,10 @@
 
 : set LDFLAGS=" -L"C:/Program Files/OpenSSL/lib/" -lssl -lcrypto -lssl -L"C:/Program Files (x86)/zlib/lib" -lz "
 
+set INCLUDE="%INCLUDE%;C:\Program Files\OpenSSL\include\;C:\Program Files (x86)\zlib\include"
+set LIB="%LIB%;C:\Program Files\OpenSSL\lib\;C:\Program Files (x86)\zlib\lib"
+
+
 configure ^
 --disable-all      --disable-cgi      --enable-cli ^
 --enable-sockets    --enable-mbstring  --enable-ctype  --enable-pdo --enable-phar  ^
@@ -9,6 +13,7 @@ configure ^
 --enable-xmlreader  --enable-xmlwriter ^
 --enable-zlib ^
 --with-openssl ^
---with-curl ^
---with-extra-includes="'C:\Program Files\OpenSSL\include\':'C:\Program Files (x86)\zlib\include/'" ^
---with-extra-libs="'C:\Program Files\OpenSSL\lib\':'C:\Program Files (x86)\zlib\lib/'"
+--with-curl
+
+: --with-extra-includes="'C:\Program Files\OpenSSL\include\':'C:\Program Files (x86)\zlib\include/'" ^
+: --with-extra-libs="'C:\Program Files\OpenSSL\lib\':'C:\Program Files (x86)\zlib\lib/'"
