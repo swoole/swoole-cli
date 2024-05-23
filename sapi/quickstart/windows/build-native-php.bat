b80981cf24d179c11416bc528521b75b2e2083fd
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-23 10:52:22 +0800

    update

--- sapi/quickstart/windows/build-native-php.bat
+++ sapi/quickstart/windows/build-native-php.bat
@@ -2,12 +2,12 @@
 
 : set LDFLAGS=" -L"C:/Program Files/OpenSSL/lib/" -lssl -lcrypto -lssl -L"C:/Program Files (x86)/zlib/lib" -lz "
 
-set CPPFLAGS="%INCLUDE%;c:\php-cli\openssl\include\;c:\php-cli\zlib\include"
-set LDFLAGS="%LIB%;c:\php-cli\openssl\lib\;c:\php-cli\zlib\lib"
+set INCLUDE="%INCLUDE%;c:\php-cli\openssl\include\;c:\php-cli\zlib\include"
 set LIB="%LIB%;c:\php-cli\openssl\lib\;c:\php-cli\zlib\lib"
-set LIBPATH="%LIBPATH%;c:\php-cli\openssl\lib\;c:\php-cli\zlib\lib"
+set LIBPATH="%LIBPATH%;c:\php-cli\openssl\lib\;c:\php-cli\zlib\lib\"
 echo %INCLUDE%
 echo %LIB%
+echo %LIBPATH%
 
 configure ^
 --disable-all      --disable-cgi      --enable-cli ^
@@ -16,8 +16,11 @@ configure ^
 --enable-xmlreader  --enable-xmlwriter ^
 --enable-zlib ^
 --with-openssl=static ^
---with-extra-includes="c:\php-cli\openssl\include\;c:\php-cli\zlib\include" ^
---with-extra-libs="c:\php-cli\openssl\lib\;c:\php-cli\zlib\lib"
+--with-fileinfo=static
+
+
+: --with-extra-includes="c:\php-cli\openssl\include\;c:\php-cli\zlib\include" ^
+: --with-extra-libs="c:\php-cli\openssl\lib\;c:\php-cli\zlib\lib"
 
 
 : --with-curl=static
