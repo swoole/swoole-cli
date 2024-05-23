90482aff3880fd0e2dcfa233c70c74ce55b7c53b
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-23 23:46:16 +0800

    update

--- sapi/quickstart/windows/native-build/msys2/msys2-download-vs.sh
+++ sapi/quickstart/windows/native-build/msys2/msys2-download-vs.sh
@@ -16,5 +16,6 @@ unset HTTPS_PROXY
 unset NO_PROXY
 
 # curl -Lo VisualStudioSetup.exe 'https://c2rsetup.officeapps.live.com/c2r/downloadVS.aspx?sku=community&channel=Release&version=VS2022'
-curl -Lo VisualStudioSetup.exe 'https://aka.ms/vs/17/release/vs_community.exe'
+curl -Lo VisualStudioSetup.exe 'https://c2rsetup.officeapps.live.com/c2r/downloadVS.aspx?sku=community&channel=Release&version=VS2019'
+# curl -Lo VisualStudioSetup.exe 'https://aka.ms/vs/17/release/vs_community.exe'
 # curl -Lo vs_buildtools.exe 'https://aka.ms/vs/17/release/vs_buildtools.exe'
