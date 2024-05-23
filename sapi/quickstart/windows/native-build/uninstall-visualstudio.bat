23869e6b3f64d1126ec00269ace9a91183995c8d
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-23 21:59:06 +0800

    update

--- sapi/quickstart/windows/native-build/uninstall-visualstudio.bat
+++ sapi/quickstart/windows/native-build/uninstall-visualstudio.bat
@@ -7,5 +7,12 @@ cd ..\..\..\..\
 set __PROJECT__=%cd%
 echo %cd%
 
-VisualStudioSetup.exe uninstall	--passive  --force --norestart
+VisualStudioSetup.exe export 	--passive  --force --norestart
+
+
+VisualStudioSetup.exe uninstall	--passive  --force --norestart ^
+--add Microsoft.VisualStudio.Component.Windows11SDK.22000   ^
+--add Microsoft.VisualStudio.Workload.NativeDesktop ^
+--add Microsoft.VisualStudio.Component.VC.CLI.Support ^
+--add Microsoft.VisualStudio.Component.VC.Redist.MSM
 
