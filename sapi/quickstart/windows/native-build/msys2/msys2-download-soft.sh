31d5887cf2a6144ab29da0f96c1b910ef1e4fc7b
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-23 15:06:33 +0800

    update

--- sapi/quickstart/windows/native-build/msys2/msys2-download-soft.sh
+++ sapi/quickstart/windows/native-build/msys2/msys2-download-soft.sh
@@ -15,7 +15,7 @@ cd ${__PROJECT__}
 
 # https://www.nasm.us/pub/nasm/releasebuilds/2.16.03/
 test -f  nasm-2.16.03-win64.zip || curl -Lo nasm-2.16.03-win64.zip https://www.nasm.us/pub/nasm/releasebuilds/2.16.03/win64/nasm-2.16.03-win64.zip
-test -d  nasm-2.16.03 && rm -rf  nasm-2.16.03
+test -d  nasm && rm -rf  nasm
 unzip nasm-2.16.03-win64.zip
 mv  nasm-2.16.03 nasm
 ls -lh nasm
