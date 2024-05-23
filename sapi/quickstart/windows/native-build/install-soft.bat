0c38f5db86c900e6877115bc73d04fa01d486893
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-23 14:59:13 +0800

    update

---
+++ sapi/quickstart/windows/native-build/install-soft.bat
@@ -0,0 +1,16 @@
+@echo off
+cd %~dp0
+echo %~dp0
+
+cd ..\..\..\..\
+
+echo %cd%
+
+: 命令行静默安装 msi
+:  msiexec /i strawberry-perl-5.38.2.2-64bit.msi /quiet
+
+msiexec /i strawberry-perl-5.38.2.2-64bit.msi /passive
+
+
+set PATH="%PATH%;%cd%\php\;%cd%\nasm"
+echo %PATH%
--- sapi/quickstart/windows/native-build/msys2/msys2-download-source-code.sh
+++ sapi/quickstart/windows/native-build/msys2/msys2-download-source-code.sh
@@ -27,14 +27,3 @@ git clone -b php-8.3.7 --depth=1 https://github.com/php/php-src.git
 
 cp -rf ext/* php-src/ext/
 ls -lh php-src/ext/
-
-
-# https://www.nasm.us/pub/nasm/releasebuilds/2.16.03/
-test -f  nasm-2.16.03-win64.zip || curl -Lo nasm-2.16.03-win64.zip https://www.nasm.us/pub/nasm/releasebuilds/2.16.03/win64/nasm-2.16.03-win64.zip
-test -d nasm && rm -rf nasm
-unzip -f  nasm-2.16.03-win64.zip
-mv nasm-2.16.03-win64 nasm
-ls -lh nasm
-
-curl -Lo strawberry-perl-5.38.2.2-64bit.msi https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_53822_64bit/strawberry-perl-5.38.2.2-64bit.msi
-
