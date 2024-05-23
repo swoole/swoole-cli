5ca18cfe1f9ba26bd93edbe98d6fdfd2f175bf65
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-24 00:45:42 +0800

    update

--- sapi/quickstart/windows/native-build/native-build-php-build-release.bat
+++ sapi/quickstart/windows/native-build/native-build-php-build-release.bat
@@ -8,6 +8,10 @@ set __PROJECT__=%cd%
 echo %cd%
 cd %__PROJECT__%\php-src\
 
+set "INCLUDE=%INCLUDE%;%__PROJECT__%\openssl\include\;%__PROJECT__%\zlib\include"
+set "LIB=%LIB%;%__PROJECT__%\openssl\lib\;%__PROJECT__%\zlib\lib"
+set "LIBPATH=%LIBPATH%;%__PROJECT__%\openssl\lib\;%__PROJECT__%\zlib\lib\"
+
 nmake
 
 cd %__PROJECT__%
