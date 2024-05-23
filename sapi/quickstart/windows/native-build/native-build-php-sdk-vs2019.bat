97823696ae0ddbbe8bd8b016a4e92a327ab59c9c
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-24 00:38:04 +0800

    update

--- sapi/quickstart/windows/native-build/native-build-php-sdk-vs2019.bat
+++ sapi/quickstart/windows/native-build/native-build-php-sdk-vs2019.bat
@@ -19,11 +19,11 @@ set PHP_SDK_MSYS2_PATH=%PROJECT_DIR%\php-sdk-binary-tools\msys2\usr\bin\
 set PHP_SDK_OS_ARCH=x64
 set PHP_SDK_PHP_CMD=%PROJECT_DIR%\php-sdk-binary-tools\bin\php\do_php.bat
 set PHP_SDK_ROOT_PATH=%PROJECT_DIR%\php-sdk-binary-tools\
-set "PHP_SDK_VC_DIR=C:\Program Files\Microsoft Visual Studio\2019\Community\VC"
+set "PHP_SDK_VC_DIR=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC"
 set "PHP_SDK_VC_TOOLSET_VER=%VCToolsVersion%"
 set PHP_SDK_VS=vs16
 set PHP_SDK_VS_NUM=16
-set "PHP_SDK_VS_SHELL_CMD=C:\Program Files\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat amd64"
+set "PHP_SDK_VS_SHELL_CMD=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat amd64"
 set "PATH=%PROJECT_DIR%\php-sdk-binary-tools\bin;%PROJECT_DIR%\php-sdk-binary-tools\msys2\usr\bin;%PATH%"
 echo %PATH%
 
