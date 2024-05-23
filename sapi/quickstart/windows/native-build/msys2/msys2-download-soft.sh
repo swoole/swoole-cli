31d850facc464d7aa4c8a520ba6286e345e89e0c
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-23 15:04:42 +0800

    update

--- sapi/quickstart/windows/native-build/msys2/msys2-download-soft.sh
+++ sapi/quickstart/windows/native-build/msys2/msys2-download-soft.sh
@@ -20,7 +20,7 @@ unzip nasm-2.16.03-win64.zip
 mv  nasm-2.16.03 nasm
 ls -lh nasm
 
-curl -Lo strawberry-perl-5.38.2.2-64bit.msi https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_53822_64bit/strawberry-perl-5.38.2.2-64bit.msi
+test -f strawberry-perl-5.38.2.2-64bit.msi ||  curl -Lo strawberry-perl-5.38.2.2-64bit.msi https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_53822_64bit/strawberry-perl-5.38.2.2-64bit.msi
 
 
 
