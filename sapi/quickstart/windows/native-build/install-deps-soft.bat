11b00f16cc7c4782df49a47d1eaa02742b298337
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-24 13:20:17 +0800

    update

--- sapi/quickstart/windows/native-build/install-deps-soft.bat
+++ sapi/quickstart/windows/native-build/install-deps-soft.bat
@@ -12,11 +12,10 @@ echo %cd%
 :: 命令行静默安装 msi
 ::  msiexec /i strawberry-perl-5.38.2.2-64bit.msi /quiet
 
-msiexec /i strawberry-perl-5.38.2.2-64bit.msi /TARGETDIR="C:\perl\" /passive
+msiexec /i strawberry-perl-5.38.2.2-64bit.msi  /passive
 
-dir C:\perl\
 
-set "PATH=%PATH%;%__PROJECT__%\php\;%__PROJECT__%\nasm\;c:\perl\"
+set "PATH=%PATH%;%__PROJECT__%\php\;%__PROJECT__%\nasm\;"
 
 
 echo %PATH%
--- sapi/quickstart/windows/native-build/library/openssl.bat
+++ sapi/quickstart/windows/native-build/library/openssl.bat
@@ -6,14 +6,15 @@ cd ..\..\..\..\..\
 
 set __PROJECT__=%cd%
 cd %__PROJECT__%
-mkdir build
+mkdir  build
 
 
-set "PATH=%__PROJECT__%\nasm\;c:\perl"
+set "PATH=%__PROJECT__%\nasm\;"
 
 cd %__PROJECT__%\thirdparty\openssl
 dir
 echo %cd%
+perl -v
 
 perl Configure VC-WIN64A threads no-shared  no-tests --release --prefix="%__PROJECT__%\build\openssl"  --openssldir="%__PROJECT__%\build\openssl\ssl"
 
