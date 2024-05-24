d2f90e1265682630c2f9b7c74adebc8b2264200d
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-24 14:19:25 +0800

    upadte

---
+++ sapi/quickstart/windows/native-build/library/ngtcp2.bat
@@ -0,0 +1,23 @@
+@echo off
+
+echo %~dp0
+cd %~dp0
+cd ..\..\..\..\..\
+
+set __PROJECT__=%cd%
+cd %__PROJECT__%
+mkdir  build
+
+cd thirdparty\ngtcp2\
+dir
+mkdir  build-dir
+cd build-dir
+cmake .. ^
+-DCMAKE_INSTALL_PREFIX="%__PROJECT__%\build\ngtcp2" ^
+-DCMAKE_BUILD_TYPE=Release  ^
+-DBUILD_SHARED_LIBS=OFF  ^
+-DBUILD_STATIC_LIBS=ON
+
+cmake --build . --config Release --target install
+
+cd %__PROJECT__%
