b817452f9e4a42f21b3c9b2b2e681d3bb9a17c18
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-24 13:00:23 +0800

    update

--- sapi/quickstart/windows/native-build/install-deps-soft.bat
+++ sapi/quickstart/windows/native-build/install-deps-soft.bat
@@ -12,12 +12,15 @@ echo %cd%
 :: 命令行静默安装 msi
 ::  msiexec /i strawberry-perl-5.38.2.2-64bit.msi /quiet
 
-msiexec /i strawberry-perl-5.38.2.2-64bit.msi /passive
+msiexec /i strawberry-perl-5.38.2.2-64bit.msi /TARGETDIR="C:\perl\" /passive
 
+dir C:\perl\
 
-set "PATH=%PATH%;%__PROJECT__%\php\;%__PROJECT__%\nasm\"
+set "PATH=%PATH%;%__PROJECT__%\php\;%__PROJECT__%\nasm\;c:\perl\"
+
 
 echo %PATH%
 
+perl -v
 php -v
 nasm -v
