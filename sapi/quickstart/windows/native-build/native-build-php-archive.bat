1d7f1e3def3050b9bafd419340d2bc0e48584549
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-25 10:36:15 +0800

    update

--- sapi/quickstart/windows/native-build/native-build-php-archive.bat
+++ sapi/quickstart/windows/native-build/native-build-php-archive.bat
@@ -2,14 +2,14 @@
 
 echo %~dp0
 cd %~dp0
-cd ..\..\..\..\
+cd /d  ..\..\..\..\
 
 set __PROJECT__=%cd%
-cd %__PROJECT__%\
+cd /d %__PROJECT__%\
 dir
 
 rem cd %__PROJECT__%\php-src\x64\Release_TS\
-cd %__PROJECT__%\php-src\x64\Release\
+cd /d %__PROJECT__%\php-src\x64\Release\
 dir
 
 .\php -v
