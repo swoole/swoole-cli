e3e024b1df2ed043d7c06212fbbeeea0bf1e8eb1
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-25 15:25:45 +0800

    update

--- sapi/quickstart/windows/native-build/native-build-php-build-release.bat
+++ sapi/quickstart/windows/native-build/native-build-php-build-release.bat
@@ -16,7 +16,7 @@ set "LIBPATH=%LIBPATH%;%__PROJECT__%\openssl\lib\;%__PROJECT__%\zlib\lib\"
 
 set CL=/MP
 set RTLIBCFG=static
-nmake dist   mode=static debug=false
+nmake   mode=static debug=false
 
 rem nmake install
 
