effa6e75f20373158568704edfbbeb35c85ae827
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-26 21:24:17 +0800

    update

--- sapi/quickstart/windows/native-build/msys2/msys2-download-deps-soft.sh
+++ sapi/quickstart/windows/native-build/msys2/msys2-download-deps-soft.sh
@@ -23,3 +23,5 @@ ls -lh nasm
 # https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/
 test -f strawberry-perl-5.38.2.2-64bit.msi ||  curl -Lo strawberry-perl-5.38.2.2-64bit.msi https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_53822_64bit/strawberry-perl-5.38.2.2-64bit.msi
 
+
+test -f Git-2.45.1-32-bit.exe ||  curl -Lo Git-2.45.1-32-bit.exe https://github.com/git-for-windows/git/releases/download/v2.45.1.windows.1/Git-2.45.1-32-bit.exe
