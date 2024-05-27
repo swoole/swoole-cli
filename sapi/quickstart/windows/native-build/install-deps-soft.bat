d9088b920e8299ee2b4a78ea2ef3d1e66f99a558
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-27 12:15:15 +0800

    update

--- sapi/quickstart/windows/native-build/install-deps-soft.bat
+++ sapi/quickstart/windows/native-build/install-deps-soft.bat
@@ -18,32 +18,13 @@ rem msiexec /i strawberry-perl-5.38.2.2-64bit.msi /quiet
 msiexec /i strawberry-perl-5.38.2.2-64bit.msi  /passive
 
 
-rem start /wait "" "安装程序路径.exe" /SILENT /NORESTART
-
-
-:: 设置Git安装路径
-set "INSTALL_PATH=C:\Program Files\Git"
-
-:: 创建安装目录
-if not exist "%INSTALL_PATH%" mkdir "%INSTALL_PATH%"
-
-:: 静默安装Git
-:: 查看git 安装参数
-:: %__PROJECT__%\Git-2.45.1-64-bit.exe /?
-start /wait "" "%__PROJECT__%\Git-2.45.1-64-bit.exe" /VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEONEXIT=1 /DIR="%INSTALL_PATH%"
-
-
-
-:: 更新环境变量
-
-echo git installing
-set "PATH=%PATH%;%__PROJECT__%\php\;%__PROJECT__%\nasm\;C:\Strawberry\perl\bin;C:\Program Files\Git\bin;"
+set "PATH=%PATH%;%__PROJECT__%\php\;%__PROJECT__%\nasm\;C:\Strawberry\perl\bin;"
 echo %PATH%
 
 
 perl -v
 php -v
 nasm -v
-git version
+
 
 endlocal
