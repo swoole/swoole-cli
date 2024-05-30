96aba073224f4d2595aa5bce7f4de3e5ae97420c
jingjingxyk <zonghengbaihe521@qq.com>
2024-05-30 16:17:36 +0800

    update

--- sapi/quickstart/windows/native-build/install-visualstudio.bat
+++ sapi/quickstart/windows/native-build/install-visualstudio.bat
@@ -23,6 +23,7 @@ VisualStudioSetup.exe ^
 --add Microsoft.VisualStudio.Component.CoreBuildTools ^
 --add Microsoft.VisualStudio.Component.Windows10SDK.20348	^
 --add Microsoft.VisualStudio.Component.Windows10SDK ^
+--add Microsoft.VisualStudio.Component.Windows11SDK.22000   ^
 --add Microsoft.Component.VC.Runtime.UCRTSDK	^
 --add Microsoft.Component.MSBuild ^
 --add Microsoft.VisualStudio.Workload.MSBuildTools ^
@@ -30,7 +31,6 @@ VisualStudioSetup.exe ^
 
 
 
-rem --add Microsoft.VisualStudio.Component.Windows11SDK.22000   ^
 rem --add Microsoft.VisualStudio.Workload.NativeDesktop ^
 rem --add Microsoft.VisualStudio.Component.VC.Redist.14.Latest	^
 rem --add Microsoft.VisualStudio.Component.VC.CLI.Support ^
