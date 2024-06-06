546593023a46157921a2c7a31f1bbee914d75a7e
jingjingxyk <zonghengbaihe521@qq.com>
2024-06-06 10:25:33 +0800

    update

---
+++ sapi/quickstart/windows/native-build/native-build-php-release-show-variable.bat
@@ -0,0 +1,18 @@
+@echo off
+
+setlocal enabledelayedexpansion
+rem show current file location
+echo %~dp0
+cd /d %~dp0
+cd /d ..\..\..\..\
+
+set "__PROJECT__=%cd%"
+echo %cd%
+cd %__PROJECT__%\php-src\
+
+
+nmake show-variable
+
+cd %__PROJECT__%
+endlocal
+
