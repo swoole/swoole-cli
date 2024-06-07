6b05ba95b0e6be54bd0edfb083c2d9cc255d7c33
jingjingxyk <zonghengbaihe521@qq.com>
2024-06-07 11:44:11 +0800

    merge code

--- sapi/src/builder/extension/swoole.php
+++ sapi/src/builder/extension/swoole.php
@@ -5,7 +5,7 @@ use SwooleCli\Extension;
 
 return function (Preprocessor $p) {
 
-    $swoole_tag = 'v5.1.2';
+    $swoole_tag = 'v5.1.3';
     $file = "swoole-{$swoole_tag}.tar.gz";
 
     $url = "https://github.com/swoole/swoole-src/archive/refs/tags/{$swoole_tag}.tar.gz";
