df8c68f40de5c590f64e6e34274eb28fe3ae2191
jingjingxyk <zonghengbaihe521@qq.com>
2024-06-08 22:45:34 +0800

    test swoole v6

--- sapi/src/builder/extension/swoole_v6.php
+++ sapi/src/builder/extension/swoole_v6.php
@@ -24,6 +24,8 @@ return function (Preprocessor $p) {
     if ($p->isLinux()) {
         $options .= ' --enable-iouring ';
         $dependentLibraries[] = 'liburing';
+        $p->withExportVariable('URING_CFLAGS', '$(pkg-config  --cflags --static  liburing)');
+        $p->withExportVariable('URING_LIBS', '$(pkg-config    --libs   --static  liburing)');
     }
 
     $options .= ' --enable-zts ';
