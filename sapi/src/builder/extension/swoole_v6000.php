5ad4600fd75937c2839ba5924a8f93549a6f02c4
jingjingxyk <zonghengbaihe521@qq.com>
2024-06-08 15:35:48 +0800

    update swow and fix swoole build error

--- sapi/src/builder/extension/swoole_v6000.php
+++ sapi/src/builder/extension/swoole_v6000.php
@@ -49,4 +49,6 @@ EOF
 
     $libs = $p->isMacos() ? '-lc++' : ' -lstdc++ ';
     $p->withVariable('LIBS', '$LIBS ' . $libs);
+    $p->withExportVariable('CARES_CFLAGS', '$(pkg-config  --cflags --static  libcares)');
+    $p->withExportVariable('CARES_LIBS', '$(pkg-config    --libs   --static  libcares)');
 };
