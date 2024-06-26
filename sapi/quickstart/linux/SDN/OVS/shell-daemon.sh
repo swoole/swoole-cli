5609409acd4405afd06137225293d12ab6e0f114
jingjingxyk <zonghengbaihe521@qq.com>
2024-06-26 16:00:02 +0800

    update

--- sapi/quickstart/linux/SDN/OVS/shell-daemon.sh
+++ sapi/quickstart/linux/SDN/OVS/shell-daemon.sh
@@ -1,2 +1,4 @@
 
+# ip netns exec vm1 python3 -m http.server 80 -d
+# ip netns exec vm1 nohup  python3 -m http.server 80 -d /tmp/  > /tmp/output.log 2> /tmp/error.log &
 nohup ./run > output.log 2> error.log &
--- sapi/quickstart/linux/SDN/OVS/test-download-file.sh
+++ sapi/quickstart/linux/SDN/OVS/test-download-file.sh
@@ -8,7 +8,7 @@ set -uex
 
 RESULT_CODE=0
 while [ $RESULT_CODE -eq 0 ]; do
-  curl -Lo /dev/null http://10.1.20.4/all-deps.zip
+  curl -fSLo /dev/null http://10.1.20.7/all-deps.zip
   RESULT_CODE=$?
   sleep 1  # 每隔1秒检查一次
 done
