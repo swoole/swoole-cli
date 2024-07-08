bd48737698644934142147d2c056dea374781344
jingjingxyk <zonghengbaihe521@qq.com>
2024-07-08 20:37:47 +0800

    update

--- sapi/quickstart/clean-folder-for-extra-build.sh
+++ sapi/quickstart/clean-folder-for-extra-build.sh
@@ -81,6 +81,7 @@ test -f setup-privoxy-runtime.sh  && rm -rf setup-privoxy-runtime.sh
 test -f setup-socat-runtime.sh    && rm -rf setup-socat-runtime.sh
 test -f setup-webBenchmark-runtime.sh    && rm -rf setup-webBenchmark-runtime.sh
 test -f setup-socat-runtime.sh    && rm -rf setup-socat-runtime.sh
+test -f setup-swoole-cli-pre-runtime.sh    && rm -rf setup-swoole-cli-pre-runtime.sh
 
 cd ${__PROJECT__}
 
