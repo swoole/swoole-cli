ef6ef879149e198761176db79c0898758aed5c15
jingjingxyk <zonghengbaihe521@qq.com>
2024-07-12 12:18:54 +0800

    update

--- sapi/quickstart/linux/extra/swoole-install/debian-init-for-swoole.sh
+++ sapi/quickstart/linux/extra/swoole-install/debian-init-for-swoole.sh
@@ -1,2 +1,2 @@
 apt-get install -y libc-ares-dev libcurl4-openssl-dev
-apt-get install -y libpcre3 libpcre3-dev libpq-dev unixodbc-dev
+apt-get install -y libpcre3 libpcre3-dev libpq-dev libsqlite3-dev unixodbc-dev
