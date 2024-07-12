dfc7b214e06e1e5ee65a1771201f08075d625770
jingjingxyk <zonghengbaihe521@qq.com>
2024-07-12 16:55:43 +0800

    update

--- sapi/quickstart/linux/extra/debian-php-init.sh
+++ sapi/quickstart/linux/extra/debian-php-init.sh
@@ -2,7 +2,7 @@
 apt install -y  php-cli php-pear php-dev php-curl php-intl
 apt install -y  php-mbstring  php-tokenizer  php-xml
 
-# apt install -y  php-mysqlnd php-pgsql php-sqlite3  php-redis php-mongodb
+apt install -y  php-mysqlnd php-pgsql php-sqlite3  php-redis php-mongodb
 
 # curl -Lo  /usr/local/bin/composer.phar https://getcomposer.org/download/latest-stable/composer.phar
 # ln -sf /usr/local/bin/composer.phar /usr/local/bin/composer
---
+++ sapi/quickstart/swoole-install/FAQ.md
@@ -0,0 +1,7 @@
+## 配置扩展加载优先级
+    每个扩展配置一个配置文件
+    配置文件名称以数字开头 , .ini结尾
+
+## php.ini 配置文件 curl、socks5、mysqld 扩展应该在 swoole 扩展之前加载
+    https://github.com/swoole/swoole-src/issues/4085
+
