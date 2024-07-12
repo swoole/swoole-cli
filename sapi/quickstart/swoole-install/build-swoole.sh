e839496b11e165663f647c118fcd612e3ebd09c9
jingjingxyk <zonghengbaihe521@qq.com>
2024-07-12 14:19:03 +0800

    update

--- sapi/quickstart/swoole-install/build-swoole.sh
+++ sapi/quickstart/swoole-install/build-swoole.sh
@@ -2,8 +2,9 @@
 set -x
 mkdir -p /tmp/build
 
+# shellcheck disable=SC2164
 cd /tmp/build/
-test -d swoole-src  && rm -rf ./swoole-src
+test -d swoole-src  && rm -rf ./swoole-src ;
 
 
 MIRROR=''
@@ -41,30 +42,41 @@ esac
 cd swoole-src
 
 UNIXODBC_PREFIX='/usr/'
+SWOOLE_ODBC_OPTIONS="--with-swoole-odbc=\"unixODBC,${UNIXODBC_PREFIX}\""
 OS=$(uname -s)
 ARCH=$(uname -m)
 case "$OS-$ARCH" in
 'Darwin-x86_64')
   export PKG_CONFIG_PATH=/usr/local/opt/libpq/lib/pkgconfig/:/usr/local/opt/unixodbc/lib/pkgconfig/
   UNIXODBC_PREFIX='/usr/local/opt/unixodbc/'
+  SWOOLE_ODBC_OPTIONS="--with-swoole-odbc=\"unixODBC,${UNIXODBC_PREFIX}\""
   ;;
 'Darwin-arm64')
   export PKG_CONFIG_PATH=/opt/homebrew/opt/libpq/lib/pkgconfig/:/opt/homebrew/opt/unixodbc/lib/pkgconfig/
   UNIXODBC_PREFIX='/opt/homebrew/opt/unixodbc/'
+  SWOOLE_ODBC_OPTIONS="--with-swoole-odbc=\"unixODBC,${UNIXODBC_PREFIX}\""
+  ;;
+  'Linux-*')
+  OS_RELEASE=$(awk -F= '/^ID=/{print $2}' /etc/os-release |tr -d '\n' | tr -d '\"')
+  case "$OS_RELEASE" in
+    'rocky' | 'almalinux' | 'rhel' |  'centos' | 'fedora' )
+    SWOOLE_ODBC_OPTIONS=''
+  esac
   ;;
 *)
   ;;
 esac
 
-OPTIONS='';
+
+SWOOLE_DEBUG_OPTIONS='';
 if [ $DEBUG -eq 1 ] ;then
-  OPTIONS=' --enable-debug --enable-debug-log --enable-trace-log '
+  SWOOLE_DEBUG_OPTIONS=' --enable-debug --enable-debug-log --enable-trace-log '
 fi
 
 
 phpize
 ./configure \
-$OPTIONS  \
+"${SWOOLE_DEBUG_OPTIONS}"  \
 --enable-openssl \
 --enable-sockets \
 --enable-mysqlnd \
@@ -72,7 +84,7 @@ $OPTIONS  \
 --enable-swoole-curl \
 --enable-swoole-pgsql \
 --enable-swoole-sqlite \
---with-swoole-odbc="unixODBC,${UNIXODBC_PREFIX}" \
+"${SWOOLE_ODBC_OPTIONS}" \
 
 
 
