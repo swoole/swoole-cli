f30c13f71834a695b21188cd4aa3e5d1d2ad494f
jingjingxyk <zonghengbaihe521@qq.com>
2024-07-12 16:57:17 +0800

    update

--- sapi/quickstart/linux/extra/alpine-php-init.sh
+++ sapi/quickstart/linux/extra/alpine-php-init.sh
@@ -1,3 +1,3 @@
 apk add php-cli php81-dev
-apk add php-iconv php-mbstring php-phar php-openssl php-posix php-curl php-tokenizer php-dom php-xmlwriter php-xml php-simplexml
+apk add php-iconv php-mbstring php-phar php-openssl php-posix php-curl php-tokenizer php-dom php-xmlwriter php-xml php-simplexml php-mysqlnd
 
