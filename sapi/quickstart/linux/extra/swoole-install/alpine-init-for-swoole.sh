90d6cecc26fd83095a0138d5376d1e89e1e0c810
jingjingxyk <zonghengbaihe521@qq.com>
2024-07-12 11:50:16 +0800

    update

--- sapi/quickstart/linux/extra/swoole-install/alpine-init-for-swoole.sh
+++ sapi/quickstart/linux/extra/swoole-install/alpine-init-for-swoole.sh
@@ -1,2 +1,2 @@
-apk add autoconf automake make libtool cmake bison re2c gcc g++
-apk add curl-dev c-ares-dev
+apk add autoconf automake make libtool cmake bison re2c gcc g++ libpq-dev sqlite-dev
+apk add curl-dev c-ares-dev libpq-dev sqlite-dev
