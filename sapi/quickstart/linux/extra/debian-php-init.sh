fb6b1ee4d73969e0acaffe9812b4923c4b7d3b32
jingjingxyk <zonghengbaihe521@qq.com>
2024-07-12 17:10:30 +0800

    update

--- sapi/quickstart/linux/extra/debian-php-init.sh
+++ sapi/quickstart/linux/extra/debian-php-init.sh
@@ -1,3 +1,6 @@
+export DEBIAN_FRONTEND=noninteractive
+export TZ="Etc/UTC"
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
 apt install -y  php-cli php-pear php-dev php-curl php-intl
 apt install -y  php-mbstring  php-tokenizer  php-xml
