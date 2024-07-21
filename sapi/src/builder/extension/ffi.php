016489bf873c6896ad7ae0a82e38b6dfbfc676f3
jingjingxyk <zonghengbaihe521@qq.com>
2024-07-21 17:32:52 +0800

    add extension ffi

--- sapi/src/builder/extension/ffi.php
+++ sapi/src/builder/extension/ffi.php
@@ -5,19 +5,24 @@ use SwooleCli\Extension;
 
 return function (Preprocessor $p) {
     $php_version = BUILD_PHP_VERSION;
+    $libffi_prefix = LIBFFI_PREFIX;
     $p->addExtension(
-        (new Extension('pgsql'))
+        (new Extension('ffi'))
             ->withHomePage('https://www.php.net/pgsql')
             ->withLicense('https://github.com/php/php-src/blob/master/LICENSE', Extension::LICENSE_PHP)
-            ->withOptions('--with-pgsql=' . PGSQL_PREFIX)
-            ->withFile('pgsql-' . $php_version . '.tgz')
+            ->withOptions('--with-ffi=' . $libffi_prefix)
+            ->withFile('ffi-' . $php_version . '.tgz')
+            ->withBuildCached(false)
+            ->withAutoUpdateFile()
             ->withDownloadScript(
-                'pgsql',
+                'ffi',
                 <<<EOF
                 git clone -b php-{$php_version} --depth=1 https://github.com/php/php-src.git
                 cd php-src/ext/
+
+
 EOF
             )
-            ->withDependentLibraries('pgsql')
+            ->withDependentLibraries('libffi')
     );
 };
