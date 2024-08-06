3a09e4214961c70a87e959f50a54ed7a0fbdcef7
jingjingxyk <zonghengbaihe521@qq.com>
2024-08-06 14:06:33 +0800

    update

---
+++ sapi/quickstart/linux/extra/archlinux-php-init.sh
@@ -0,0 +1,5 @@
+pacman -Sy --noconfirm git curl wget openssl which
+pacman -Sy --noconfirm cmake autoconf libtool make gcc
+pacman -Sy --noconfirm pkg-config unixodbc c-are
+pacman -Sy --noconfirm extra/php
+pacman -Sy --noconfirm extra/php-redis extra/php-pgsql extra/php-sqlite
