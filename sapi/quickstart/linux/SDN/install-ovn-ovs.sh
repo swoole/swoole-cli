072188f20e941740601cf97bb0d0037d22173ee7
jingjingxyk <zonghengbaihe521@qq.com>
2024-08-11 15:10:03 +0800

    update

--- sapi/quickstart/linux/SDN/install-ovn-ovs.sh
+++ sapi/quickstart/linux/SDN/install-ovn-ovs.sh
@@ -4,9 +4,6 @@
 set -eux
 set -o pipefail
 
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-export LANGUAGE=en_US.UTF-8
 
 __DIR__=$(cd "$(dirname "$0")";pwd)
 cd ${__DIR__}
@@ -21,9 +18,17 @@ while [ $# -gt 0 ]; do
    export HTTP_PROXY="$2"
    export HTTPS_PROXY="$2"
    NO_PROXY="127.0.0.0/8,10.0.0.0/8,100.64.0.0/10,172.16.0.0/12,192.168.0.0/16"
-   NO_PROXY="${NO_PROXY},127.0.0.1,localhost"
-   NO_PROXY="${NO_PROXY},.aliyuncs.com,.aliyun.com"
-   export NO_PROXY="${NO_PROXY},.tsinghua.edu.cn,.ustc.edu.cn,.npmmirror.com,.tencent.com"
+   NO_PROXY="${NO_PROXY},::1/128,fe80::/10,fd00::/8,ff00::/8"
+   NO_PROXY="${NO_PROXY},.aliyuncs.com,.aliyun.com,.tencent.com"
+   NO_PROXY="${NO_PROXY},.tsinghua.edu.cn,.ustc.edu.cn,.npmmirror.com"
+   NO_PROXY="${NO_PROXY},ftpmirror.gnu.org"
+   NO_PROXY="${NO_PROXY},gitee.com,gitcode.com"
+   NO_PROXY="${NO_PROXY},.myqcloud.com,.swoole.com"
+   NO_PROXY="${NO_PROXY},dl-cdn.alpinelinux.org"
+   NO_PROXY="${NO_PROXY},deb.debian.org,security.debian.org"
+   NO_PROXY="${NO_PROXY},archive.ubuntu.com,security.ubuntu.com"
+   NO_PROXY="${NO_PROXY},pypi.python.org,bootstrap.pypa.io"
+   export NO_PROXY="${NO_PROXY},localhost"
    ;;
  --*)
    echo "Illegal option $1"
@@ -66,7 +71,14 @@ prepare(){
   apt update -y
   apt install -y locales
   locale-gen en_US.UTF-8
+
+  export LANGUAGE="en_US.UTF-8"
+  export LC_ALL="en_US.UTF-8"
+  export LC_CTYPE="en_US.UTF-8"
+  export LANG="en_US.UTF-8"
+
   update-locale LANG=en_US.UTF-8
+
   apt install -y git curl python3 python3-pip python3-dev wget   sudo file
   apt install -y libssl-dev ca-certificates
 
@@ -103,7 +115,7 @@ then
     cd ${__DIR__}/ovs/
     # git   pull --depth=1 --progress --rebase
 else
-    git clone -b v3.3.0 https://github.com/openvswitch/ovs.git --depth=1 --progress
+    git clone -b v3.3.1 https://github.com/openvswitch/ovs.git --depth=1 --progress
 fi
 
 cd ${__DIR__}
