60f5684f6401ae12aad863be5cfd7b48a060c097
jingjingxyk <zonghengbaihe521@qq.com>
2024-08-12 09:02:52 +0800

    更新 libiconv 构建配置，启用多字节

--- sapi/src/builder/library/libiconv.php
+++ sapi/src/builder/library/libiconv.php
@@ -11,9 +11,21 @@ return function (Preprocessor $p) {
             ->withManual('https://www.gnu.org/software/libiconv/')
             ->withLicense('https://www.gnu.org/licenses/old-licenses/gpl-2.0.html', Library::LICENSE_GPL)
             ->withUrl('https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.17.tar.gz')
-            ->withFileHash('md5','d718cd5a59438be666d1575855be72c3')
+            ->withFileHash('md5', 'd718cd5a59438be666d1575855be72c3')
             ->withPrefix($libiconv_prefix)
-            ->withConfigure('./configure --prefix=' . $libiconv_prefix . ' enable_static=yes enable_shared=no')
+            ->withBuildCached(false)
+            ->withInstallCached(false)
+            ->withConfigure(
+                <<<EOF
+            ./configure --help
+
+            ./configure \
+            --prefix={$libiconv_prefix} \
+            --enable-static=yes \
+            --enable-shared=no \
+            --enable-extra-encodings
+EOF
+            )
             ->withBinPath($libiconv_prefix . '/bin/')
     );
     $p->withVariable('CPPFLAGS', '$CPPFLAGS -I' . ICONV_PREFIX . '/include');
