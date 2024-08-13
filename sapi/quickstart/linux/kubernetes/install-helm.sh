0c65ad86e11f48a92435a9b59a2cab433a9b4b7d
jingjingxyk <zonghengbaihe521@qq.com>
2024-08-13 16:12:44 +0800

    update

---
+++ sapi/quickstart/linux/kubernetes/install-helm.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -x
+__DIR__=$(
+  cd "$(dirname "$0")"
+  pwd
+)
+cd ${__DIR__}
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+  --proxy)
+    export HTTP_PROXY="$2"
+    export HTTPS_PROXY="$2"
+    NO_PROXY="127.0.0.0/8,10.0.0.0/8,100.64.0.0/10,172.16.0.0/12,192.168.0.0/16"
+    NO_PROXY="${NO_PROXY},127.0.0.1,localhost"
+    NO_PROXY="${NO_PROXY},.aliyuncs.com,.aliyun.com"
+    NO_PROXY="${NO_PROXY},.tsinghua.edu.cn,.ustc.edu.cn"
+    NO_PROXY="${NO_PROXY},.tencent.com"
+    NO_PROXY="${NO_PROXY},.sourceforge.net"
+    NO_PROXY="${NO_PROXY},.npmmirror.com"
+    export NO_PROXY="${NO_PROXY}"
+    ;;
+
+  --*)
+    echo "Illegal option $1"
+    ;;
+  esac
+  shift $(($# > 0 ? 1 : 0))
+done
+
+# shellcheck disable=SC2034
+OS=$(uname -s)
+# shellcheck disable=SC2034
+ARCH=$(uname -m)
+
+mkdir -p helm
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+
+chmod a+x ./get_helm.sh
+bash ./get_helm.sh
