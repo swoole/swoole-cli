3048f5fe50eadca5b753cf839210e0fed80a7f3b
jingjingxyk <zonghengbaihe521@qq.com>
2024-08-13 14:57:36 +0800

    update

--- sapi/quickstart/linux/kubernetes/kubeadm-reset.sh
+++ sapi/quickstart/linux/kubernetes/kubeadm-reset.sh
@@ -2,8 +2,14 @@
 
 # kubectl drain <node name> --delete-emptydir-data --force --ignore-daemonsets
 
+# kubectl -n kube-system get cm kubeadm-config -o yaml
+
 kubeadm reset
 
 iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
 
 ipvsadm -C
+ipvsadm --clear
+test -f $HOME/.kube/config && rm -f $HOME/.kube/config
+
+# reboot
