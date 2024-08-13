63d28d6c6ef60a549eb9fdcee7cdaf06643164d4
jingjingxyk <zonghengbaihe521@qq.com>
2024-08-13 10:25:43 +0800

    update kubernetes

--- sapi/quickstart/linux/kubernetes/install-cni-kube-ovn.sh
+++ sapi/quickstart/linux/kubernetes/install-cni-kube-ovn.sh
@@ -27,3 +27,21 @@ VERSION="release-1.12"
 curl -fSLo kube-ovn-${VERSION}-install.sh https://raw.githubusercontent.com/kubeovn/kube-ovn/${VERSION}/dist/images/install.sh
 
 bash kube-ovn-${VERSION}-install.sh
+
+# 卸载 和 清理残余
+curl -fSLo kube-ovn-${VERSION}-uninstall.sh https://raw.githubusercontent.com/kubeovn/kube-ovn/${VERSION}/dist/images/cleanup.sh
+
+cat >kube-ovn-${VERSION}-clean.sh <<EOF
+rm -rf /var/run/openvswitch
+rm -rf /var/run/ovn
+rm -rf /etc/origin/openvswitch/
+rm -rf /etc/origin/ovn/
+rm -rf /etc/cni/net.d/00-kube-ovn.conflist
+rm -rf /etc/cni/net.d/01-kube-ovn.conflist
+rm -rf /var/log/openvswitch
+rm -rf /var/log/ovn
+rm -fr /var/log/kube-ovn
+
+# reboot
+
+EOF
