e46b8477167f065c8216e4e3f2146e44a9608275
jingjingxyk <zonghengbaihe521@qq.com>
2024-08-18 16:34:49 +0800

    update

--- sapi/quickstart/linux/debian-linglong.sh
+++ sapi/quickstart/linux/debian-linglong.sh
@@ -2,13 +2,12 @@
 
 set -x
 
-
 MIRROR=''
 while [ $# -gt 0 ]; do
   case "$1" in
   --mirror)
     case "$2" in
-    china | ustc | tuna )
+    china | ustc | tuna)
       MIRROR="$2"
       ;;
     esac
@@ -20,9 +19,9 @@ while [ $# -gt 0 ]; do
   shift $(($# > 0 ? 1 : 0))
 done
 
-
 apt install -y sudo apt-transport-https ca-certificates curl gpg
 # https://download.opensuse.org/repositories/home:/kamiyadm/Debian_12/
+apt install -y xdg-utils
 
 mkdir -p /etc/apt/keyrings/
 
@@ -30,10 +29,11 @@ curl -fsSL https://download.opensuse.org/repositories/home:/kamiyadm/Debian_12/R
 
 echo "deb [signed-by=/etc/apt/keyrings/linglong-apt-keyring.gpg] https://download.opensuse.org/repositories/home:/kamiyadm/Debian_12/ ./" | sudo tee /etc/apt/sources.list.d/linglong.list
 
-
+# 此方法设置不生效
 # sudo bash -c "echo 'deb [trusted=yes] https://download.opensuse.org/repositories/home:/kamiyadm/Debian_12/ ./' > /etc/apt/sources.list.d/linglong.list"
-
-
 
 # 镜像地址
 # https://help.mirrors.cernet.edu.cn/
+
+# gnome-desktop
+# apt install task-gnome-desktop
