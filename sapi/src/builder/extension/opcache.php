252394b06b2ee28b3340059b8125c8a7c5e89b24
Tianfeng.Han <rango@swoole.com>
2024-08-23 17:56:32 +0800

    Version 6.0: Added Thread Supports (#746)
    * enable zend thread safe
    
    * test swoole v6
    
    * update github action workflow config
    
    * 默认禁用 opcache 扩展 和 拉取 swoole v6 最新代码
    
    * enable cygwin build zts mode
    
    * v6.0
    
    * update cygwin
    
    * remove zts prefix
    
    * remove zts prefix
    
    * optimize code
    
    * optimize code [2]
    
    ---------
    
    Co-authored-by: jingjingxyk <zonghengbaihe521@qq.com>

--- sapi/src/builder/extension/opcache.php
+++ sapi/src/builder/extension/opcache.php
@@ -7,6 +7,7 @@ return function (Preprocessor $p) {
     $p->addExtension(
         (new Extension('opcache'))
             ->withHomePage('https://www.php.net/opcache')
-            ->withOptions('--enable-opcache')
+            # zts 模式下， 静态编译 opcache-jit 报错 ，更多信息： https://github.com/php/php-src/issues/15074
+            ->withOptions('--enable-opcache --disable-opcache-jit')
     );
 };
