5eb8fc4c3840f36b425e094d270a54a347f6fba3
hantianfeng <rango@swoole.com>
2024-08-23 17:41:18 +0800

    optimize code

--- sapi/src/builder/extension/swoole.php
+++ sapi/src/builder/extension/swoole.php
@@ -5,49 +5,40 @@ use SwooleCli\Preprocessor;
 use SwooleCli\Extension;
 
 return function (Preprocessor $p) {
-
-    $swoole_tag = 'v6.0';
-    $file = "swoole-{$swoole_tag}.tar.gz";
-
-    $url = "https://github.com/swoole/swoole-src/archive/refs/tags/{$swoole_tag}.tar.gz";
-
     $dependentLibraries = ['curl', 'openssl', 'cares', 'zlib', 'brotli', 'nghttp2', 'sqlite3', 'unix_odbc', 'pgsql'];
     $dependentExtensions = ['curl', 'openssl', 'sockets', 'mysqlnd', 'pdo'];
-    $options = ' --enable-swoole --enable-sockets --enable-mysqlnd --enable-swoole-curl --enable-cares ';
-    $options .= ' --enable-swoole-coro-time ';
-    # $options .= ' --enable-thread-context ';
-    $options .= ' --with-brotli-dir=' . BROTLI_PREFIX;
-    $options .= ' --with-nghttp2-dir=' . NGHTTP2_PREFIX;
-    $options .= ' --enable-swoole-pgsql ';
-    $options .= ' --enable-swoole-sqlite ';
-    $options .= ' --with-swoole-odbc=unixODBC,' . UNIX_ODBC_PREFIX . ' ';
-    $options .= ' --enable-swoole-thread ';
-    $options .= ' --enable-zts ';
 
-    //alpine linux 环境下  # 启用 opcache 扩展时构建报错
+    $options[] = '--enable-swoole';
+    $options[] = '--enable-sockets';
+    $options[] = '--enable-mysqlnd';
+    $options[] = '--enable-swoole-curl';
+    $options[] = '--enable-cares';
+    $options[] = '--with-brotli-dir=' . BROTLI_PREFIX;
+    $options[] = '--with-nghttp2-dir=' . NGHTTP2_PREFIX;
+    $options[] = '--enable-swoole-pgsql ';
+    $options[] = '--enable-swoole-sqlite ';
+    $options[] = '--with-swoole-odbc=unixODBC,' . UNIX_ODBC_PREFIX;
+    $options[] = '--enable-swoole-thread';
+    $options[] = '--enable-zts';
 
-    if ($p->isLinux() && 0) {
+    if ($p->isLinux() && $p->getInputOption('with-iouring')) {
         // 构建报错
-        $options .= ' --enable-iouring ';
+        $options[] = '--enable-iouring';
         $dependentLibraries[] = 'liburing';
         $p->withExportVariable('URING_CFLAGS', '$(pkg-config  --cflags --static  liburing)');
         $p->withExportVariable('URING_LIBS', '$(pkg-config    --libs   --static  liburing)');
     }
 
-    $ext = (new Extension('swoole'))
+    $p->addExtension((new Extension('swoole'))
         ->withHomePage('https://github.com/swoole/swoole-src')
         ->withLicense('https://github.com/swoole/swoole-src/blob/master/LICENSE', Extension::LICENSE_APACHE2)
         ->withManual('https://wiki.swoole.com/#/')
-        ->withOptions($options)
+        ->withOptions(implode(' ', $options))
         ->withBuildCached(false)
         ->withDependentLibraries(...$dependentLibraries)
-        ->withDependentExtensions(...$dependentExtensions);
+        ->withDependentExtensions(...$dependentExtensions));
 
-    $p->addExtension($ext);
-
-    $libs = $p->isMacos() ? '-lc++' : ' -lstdc++ ';
-    $p->withVariable('LIBS', '$LIBS ' . $libs);
-
+    $p->withVariable('LIBS', '$LIBS ' . ($p->isMacos() ? '-lc++' : ' -lstdc++ '));
     $p->withExportVariable('CARES_CFLAGS', '$(pkg-config  --cflags --static  libcares)');
     $p->withExportVariable('CARES_LIBS', '$(pkg-config    --libs   --static  libcares)');
 };
