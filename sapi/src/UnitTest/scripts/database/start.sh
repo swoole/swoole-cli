4488c1e8515ffeba770c55bcc0e80b6025e227b9
jingjingxyk <zonghengbaihe521@qq.com>
2024-09-02 20:51:36 +0800

    修改 swoole pg 客户端 测试例子

--- sapi/src/UnitTest/scripts/database/start.sh
+++ sapi/src/UnitTest/scripts/database/start.sh
@@ -9,3 +9,10 @@ __DIR__=$(
 cd ${__DIR__}
 
 docker-compose -f docker-compose.yaml up -d
+docker-compose -f docker-compose.yaml ps
+
+docker container ls -a
+docker inspect --format='{{json .State.Status}}' "postgresql"
+
+while ! nc -z 127.0.0.1 5432; do sleep 1; done
+echo "Can you connect to a PostgreSQL database."
