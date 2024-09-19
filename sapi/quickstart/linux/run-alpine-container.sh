f73541b7531dd60a140fe71fc47ccae7e2d64732
好吧，你想说啥 <zonghengbaihe521@qq.com>
2024-09-19 22:27:21 +0800

    dockerfile 和 dockerfile-arm64 合并为一个 (#777)
    * 使用 tini 正确地回收其子进程
    
    * format bash script
    
    * 合并dockerfile dockerfile-arm64
    
    * 自动指定构建容器的架构
    
    * 构建容器指定架构

--- sapi/quickstart/linux/run-alpine-container.sh
+++ sapi/quickstart/linux/run-alpine-container.sh
@@ -21,4 +21,4 @@ cd ${__DIR__}
 IMAGE=alpine:3.18
 
 cd ${__DIR__}
-docker run --rm --name swoole-cli-alpine-dev -d -v ${__PROJECT__}:/work -w /work $IMAGE tail -f /dev/null
+docker run --rm --name swoole-cli-alpine-dev -d -v ${__PROJECT__}:/work -w /work --init $IMAGE tail -f /dev/null
