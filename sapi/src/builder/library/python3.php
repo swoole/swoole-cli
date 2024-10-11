1e06594787b7772da9329cacffc5d3116c19f6c9
jingjingxyk <zonghengbaihe521@qq.com>
2024-10-12 07:25:41 +0800

    update

--- sapi/src/builder/library/python3.php
+++ sapi/src/builder/library/python3.php
@@ -11,17 +11,17 @@ return function (Preprocessor $p) {
     $libiconv_prefix = ICONV_PREFIX;
     $bzip2_prefix = BZIP2_PREFIX;
 
-    $ldflags = $p->isMacos() ? '' : ' -static  ';
+    $static_flag = $p->isMacos() ? '' : ' -static  ';
     $libs = $p->isMacos() ? '-lc++' : ' -lstdc++ ';
 
     $lib = new Library('python3');
     $lib->withHomePage('https://www.python.org/')
         ->withLicense('https://docs.python.org/3/license.html', Library::LICENSE_LGPL)
         ->withManual('https://www.python.org')
+        ->withManual('https://github.com/python/cpython.git')
         ->withUrl('https://www.python.org/ftp/python/3.12.2/Python-3.12.2.tgz')
         ->withPrefix($python3_prefix)
         ->withBuildCached(false)
-        //->withInstallCached(false)
         ->withBuildScript(
             <<<EOF
 
@@ -38,10 +38,10 @@ return function (Preprocessor $p) {
         PACKAGES="\$PACKAGES libmpdec"
         PACKAGES="\$PACKAGES libb2"
 
-        # -Wl,–no-export-dynamic
-        CFLAGS="-DOPENSSL_THREADS {$ldflags}  "
-        CPPFLAGS="$(pkg-config  --cflags-only-I  --static \$PACKAGES)  {$ldflags}  "
-        LDFLAGS="$(pkg-config   --libs-only-L    --static \$PACKAGES)   {$ldflags} -DOPENSSL_THREADS  "
+
+        CFLAGS="-DOPENSSL_THREADS {$static_flag} "
+        CPPFLAGS="$(pkg-config  --cflags-only-I  --static \$PACKAGES)  {$static_flag}  "
+        LDFLAGS="$(pkg-config   --libs-only-L    --static \$PACKAGES)  {$static_flag}  -DOPENSSL_THREADS  "
         LIBS="$(pkg-config      --libs-only-l    --static \$PACKAGES)  {$libs}"
 
         CPPFLAGS=" \$CPPFLAGS -I{$bzip2_prefix}/include/ "
@@ -57,50 +57,17 @@ return function (Preprocessor $p) {
         LDFLAGS=" \$LDFLAGS -L{$libiconv_prefix}/lib/ "
         LIBS=" \$LIBS -liconv "
 
-        echo \$CFLAGS
-        echo \$CPPFLAGS
-        echo \$LDFLAGS
-        echo \$LIBS
-
-        export CFLAGS="\$CFLAGS "
-        export CPPFLAGS="\$CPPFLAGS "
-        export LDFLAGS="\$LDFLAGS "
-        export LIBS="\$LIBS "
-        export LINKFORSHARED=" "
-
-        export CCSHARED=""
-        export LDSHARED=""
-        export LDCXXSHARED=""
-
-        export LIBLZMA_CFLAGS="\$(pkg-config  --cflags --static liblzma)"
-        export LIBLZMA_LIBS="\$(pkg-config    --libs   --static liblzma)"
-
-        export CURSES_CFLAGS="\$(pkg-config  --cflags --static ncursesw)"
-        export CURSES_LIBS="\$(pkg-config    --libs   --static ncursesw)"
-
-        export PANEL_CFLAGS="\$(pkg-config  --cflags --static panelw)"
-        export PANEL_LIBS="\$(pkg-config    --libs   --static panelw)"
-
-        export LIBMPDEC_CFLAGS="\$(pkg-config  --cflags --static libmpdec)"
-        export LIBMPDEC_LDFLAGS="\$(pkg-config    --libs   --static libmpdec)"
-
-        export LIBEXPAT_CFLAGS="\$(pkg-config  --cflags --static expat)"
-        export LIBEXPAT_LDFLAGS="\$(pkg-config    --libs   --static expat)"
-
-        export OPENSSL_LDFLAGS="\$(pkg-config     --libs-only-L     --static openssl)"
-        export OPENSSL_LIBS="\$(pkg-config        --libs-only-l     --static openssl)"
-        export OPENSSL_INCLUDES="\$(pkg-config    --cflags-only-I   --static openssl)"
-
-        export LIBB2_CFLAGS="\$(pkg-config  --cflags --static libb2)"
-        export LIBB2_LIBS="\$(pkg-config    --libs   --static libb2)"
-
+        CFLAGS="\$CFLAGS " \
+        CPPFLAGS="\$CPPFLAGS " \
+        LDFLAGS="\$LDFLAGS  " \
+        LIBS="\$LIBS " \
         ./configure \
         --prefix={$python3_prefix} \
         --enable-shared=no \
         --disable-test-modules \
         --with-static-libpython \
         --with-system-expat=yes \
-        --with-system-libmpdec=no \
+        --with-system-libmpdec=yes \
         --with-readline=readline \
         --with-builtin-hashlib-hashes="md5,sha1,sha2,sha3,blake2" \
         --with-openssl={$openssl_prefix} \
@@ -109,18 +76,26 @@ return function (Preprocessor $p) {
         --without-dtrace \
         --with-ensurepip=install
 
-        # --with-libs='expat libmpdec openssl zlib sqlite3 liblzma ncursesw panelw formw menuw ticw readline uuid '
-        # --enable-optimizations \
-        # --without-system-ffi \
+        # 只能动态构建的扩展 请查看 Modules/Setup.stdlib 描述,找到并注释
+        # 注释方法： sed -i 's/^pattern/;\1/' file.txt
+        # \1 表示匹配到的内容
 
-        # echo '*static*' >> Modules/Setup.local
+        sed -i.backup 's/^_ctypes _ctypes\/_ctypes\.c/# \1/' Modules/Setup.stdlib
+        sed -i.backup 's/^_scproxy _scproxy\.c/# \1/' Modules/Setup.stdlib
+        sed -i.backup 's/^xxlimited xxlimited\.c/# \1/' Modules/Setup.stdlib
+        sed -i.backup 's/^xxlimited_35 xxlimited_35\.c/# \1/' Modules/Setup.stdlib
+        sed -i.backup 's/^_decimal _decimal\/_decimal\.c/# \1/' Modules/Setup.stdlib
 
-        sed -i.bak "s/^\*shared\*/\*static\*/g" Modules/Setup.stdlib
-        cat Modules/Setup.stdlib > Modules/Setup.local
+        sed -i.backup "s/^\*shared\*/\*static\*/g" Modules/Setup.stdlib
 
-        # make -j {$p->getMaxJob()} LDFLAGS="\$LDFLAGS " LINKFORSHARED=" "
+        cp -f Modules/Setup.stdlib  Modules/Setup.local
 
-        make -j {$p->getMaxJob()}
+         CFLAGS="\$CFLAGS " \
+         CPPFLAGS="\$CPPFLAGS " \
+         LDFLAGS="\$LDFLAGS  " \
+         LIBS="\$LIBS " \
+         LINKFORSHARED=" "  \
+         make -j {$p->getMaxJob()}
 
         make install
 
@@ -131,48 +106,15 @@ return function (Preprocessor $p) {
         {$python3_prefix}/bin/python3-config --ldflags
         {$python3_prefix}/bin/python3-config --libs
 
+        PYTHONPATH=$({$python3_prefix}/bin/python3 -c "import site, os; print(os.path.join(site.USER_BASE, 'lib', 'python', 'site-packages'))")
+        echo \${PYTHONPATH}
 
         mkdir -p {$python3_prefix}/python_hacl
         cp -rf {$p->getBuildDir()}/python3/Modules/_hacl/* {$python3_prefix}/python_hacl/
-
-
-        unset CFLAGS
-        unset CPPFLAGS
-        unset LDFLAGS
-        unset LIBS
-        unset LINKFORSHARED
-
-        unset CCSHARED
-        unset LDSHARED
-        unset LDCXXSHARED
-
-        unset LIBLZMA_CFLAGS
-        unset LIBLZMA_LIBS
-
-        unset CURSES_CFLAGS
-        unset CURSES_LIBS
-
-        unset PANEL_CFLAGS
-        unset PANEL_LIBS
-
-        unset LIBMPDEC_CFLAGS
-        unset LIBMPDEC_LDFLAGS
-
-        unset LIBEXPAT_CFLAGS
-        unset LIBEXPAT_LDFLAGS
-
-        unset OPENSSL_LDFLAGS
-        unset OPENSSL_LIBS
-        unset OPENSSL_INCLUDES
-
-        unset LIBB2_CFLAGS
-        unset LIBB2_LIBS
-
-
 EOF
         )
         ->withPkgName('python3')
-        //->withPkgName('python3-embed')
+        ->withPkgName('python3-embed')
         ->withDependentLibraries(
             'zlib',
             'openssl',
@@ -192,15 +134,15 @@ EOF
 
     $p->withVariable('CPPFLAGS', '$CPPFLAGS -I' . $python3_prefix . '/python_hacl/');
     $p->withVariable('CPPFLAGS', '$CPPFLAGS -I' . $python3_prefix . '/python_hacl/include/');
-    # $p->withVariable('LDFLAGS', '$LDFLAGS -l:' . $python3_prefix . '/python_hacl/libHacl_Hash_SHA2.a');
     $p->withVariable('LDFLAGS', '$LDFLAGS -L' . $python3_prefix . '/python_hacl/');
     $p->withVariable('LIBS', '$LIBS -lHacl_Hash_SHA2');
-
-    $p->withVariable('CPPFLAGS', '$CPPFLAGS -I' . $python3_prefix . '/include/python3.12/');
-    $p->withVariable('LDFLAGS', '$LDFLAGS -L' . $python3_prefix . '/lib/');
-    $p->withVariable('LIBS', '$LIBS -lpython3.12');
     if ($p->isMacos()) {
         $p->withVariable('LDFLAGS', '$LDFLAGS -framework CoreFoundation ');
+    }
+
+    if ($p->isMacos()) {
+        //module  _scproxy needs SystemConfiguration and CoreFoundation framework
+        //$p->withVariable('LDFLAGS', '$LDFLAGS -framework SystemConfiguration -framework CoreFoundation ');
     }
 
 };
