fce82fe2ca0739b322b84266bac4af08aa9a865b
jingjingxyk <zonghengbaihe521@qq.com>
2024-10-21 07:34:32 +0800

    update build python3

--- sapi/src/builder/extension/phpy.php
+++ sapi/src/builder/extension/phpy.php
@@ -8,10 +8,12 @@ return function (Preprocessor $p) {
     // anaconda 安装包
     // https://repo.anaconda.com/archive/
 
+
     # $options .= ' --with-python-version=3.12';
     # $options .= ' --with-python-dir=/opt/anaconda3';
 
     $tag = 'v1.0.4';
+
 
     $python3_prefix = PYTHON3_PREFIX;
     $options = [];
@@ -20,8 +22,10 @@ return function (Preprocessor $p) {
     $options[] = ' --with-python-dir=' . $python3_prefix;
     $options[] = ' --with-python-config=' . $python3_prefix . '/bin/python3-config';
 
+
     $dependentLibraries = ['python3'];
     $dependentExtensions = [];
+
 
     $ext = (new Extension('phpy'))
         ->withOptions(implode(' ', $options))
@@ -39,6 +43,19 @@ EOF
         ->withDependentExtensions(...$dependentExtensions)
         ->withDependentLibraries(...$dependentLibraries);
     $p->addExtension($ext);
+
+    $p->withBeforeConfigureScript('phpy', function (Preprocessor $p) {
+        $php_src = $p->getPhpSrcDir();
+        $cmd = <<<EOF
+
+        cd {$php_src}/
+        sed -i.backup "s/ -z now/  /g" ext/phpy/config.m4
+        rm -f ext/phpy/config.m4.backup
+EOF;
+
+        return $cmd;
+    });
+
 
     $libs = $p->isMacos() ? '-lc++' : ' -lstdc++ ';
     $p->withVariable('LIBS', '$LIBS ' . $libs);
