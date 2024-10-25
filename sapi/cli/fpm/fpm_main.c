0440379b2eda3753f872d89300947622d122e12f
jingjingxyk <zonghengbaihe521@qq.com>
2024-10-26 05:16:44 +0800

    update  sync-source-code.php for fpm

--- sapi/cli/fpm/fpm_main.c
+++ sapi/cli/fpm/fpm_main.c
@@ -141,7 +141,7 @@ static const opt_struct OPTIONS[] = {
 	{'D', 0, "daemonize"},
 	{'F', 0, "nodaemonize"},
 	{'O', 0, "force-stderr"},
-    {'P', 0, "fpm"},
+	{'P', 0, "fpm"},
 	{'-', 0, NULL} /* end of args */
 };
 
@@ -1690,8 +1690,8 @@ int fpm_main(int argc, char *argv[])
 				force_stderr = 1;
 				break;
 
-			case 'P':
-			    break;
+			case 'P': /* enable fpm */
+				break;
 
 			default:
 			case 'h':
