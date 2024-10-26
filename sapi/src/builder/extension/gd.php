f0b03a043d492634e7d642ece9f81b04bdccff4b
jingjingxyk <zonghengbaihe521@qq.com>
2024-10-26 20:35:09 +0800

    gd 扩展支持 avif 格式图片

--- sapi/src/builder/extension/gd.php
+++ sapi/src/builder/extension/gd.php
@@ -8,9 +8,14 @@ return function (Preprocessor $p) {
     $p->addExtension(
         (new Extension('gd'))
             ->withHomePage('https://www.php.net/manual/zh/book.image.php')
-            ->withOptions('--enable-gd --with-jpeg --with-freetype --with-webp')
-            ->withDependentLibraries('libjpeg', 'freetype', 'libwebp', 'libpng', 'libgif')
+            ->withOptions('--enable-gd --with-jpeg --with-freetype --with-webp --with-avif ')
+            ->withDependentLibraries('libjpeg', 'freetype', 'libwebp', 'libpng', 'libgif', 'libavif')
     );
+
     $p->withExportVariable('FREETYPE2_CFLAGS', '$(pkg-config  --cflags --static  libbrotlicommon libbrotlidec libbrotlienc freetype2 zlib libpng)');
     $p->withExportVariable('FREETYPE2_LIBS', '$(pkg-config    --libs   --static  libbrotlicommon libbrotlidec libbrotlienc freetype2 zlib libpng)');
+
+    $p->withExportVariable('AVIF_CFLAGS', '$(pkg-config  --cflags --static libavif libbrotlicommon libbrotlidec libbrotlienc SvtAv1Enc SvtAv1Dec aom dav1d libgav1)');
+    $p->withExportVariable('AVIF_LIBS', '$(pkg-config    --libs   --static libavif libbrotlicommon libbrotlidec libbrotlienc SvtAv1Enc SvtAv1Dec aom dav1d libgav1) ' . ($p->isMacos() ? '-lc++' : '-lstdc++'));
+
 };
