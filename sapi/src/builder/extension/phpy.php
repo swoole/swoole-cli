1ffb29f973877641efc0996b4737801ea50dea3f
jingjingxyk <zonghengbaihe521@qq.com>
2024-11-07 18:06:45 +0800

    update phpy.php

--- sapi/src/builder/extension/phpy.php
+++ sapi/src/builder/extension/phpy.php
@@ -45,7 +45,7 @@ EOF
     $p->addExtension($ext);
 
     $p->withBeforeConfigureScript('phpy', function (Preprocessor $p) {
-        $php_src = $p->getPhpSrcDir();
+        $php_src = $p->getWorkDir();
         $cmd = <<<EOF
 
         cd {$php_src}/
@@ -59,5 +59,5 @@ EOF;
 
     $libs = $p->isMacos() ? '-lc++' : ' -lstdc++ ';
     $p->withVariable('LIBS', '$LIBS ' . $libs);
-    $p->withVariable('CPPFLAGS', '$CPPFLAGS -I' . $p->getPhpSrcDir() . '/ext/phpy/include');
+    $p->withVariable('CPPFLAGS', '$CPPFLAGS -I' . $p->getWorkExtDir() . '/phpy/include');
 };
