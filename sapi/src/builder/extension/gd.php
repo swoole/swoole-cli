7edd08ddc02f3c689e2cc1ad5760efdb4da54917
jingjingxyk <zonghengbaihe521@qq.com>
2024-12-08 08:37:46 +0800

    update gd build config

--- sapi/src/builder/extension/gd.php
+++ sapi/src/builder/extension/gd.php
@@ -4,10 +4,8 @@ use SwooleCli\Preprocessor;
 use SwooleCli\Extension;
 
 return function (Preprocessor $p) {
-    $options = '--enable-gd --with-jpeg --with-freetype --with-webp ';
-    $dependent_libraries = ['libjpeg', 'freetype', 'libwebp', 'libpng', 'libgif'];
-
-    $options = '--enable-gd --with-jpeg --with-freetype --with-webp';
+    $options = '--enable-gd --with-jpeg --with-freetype --with-webp --with-avif ';
+    $dependent_libraries = ['libjpeg', 'freetype', 'libwebp', 'libpng', 'libgif', 'libavif'];
 
     $p->addExtension(
         (new Extension('gd'))
@@ -17,4 +15,11 @@ return function (Preprocessor $p) {
     );
     $p->withExportVariable('FREETYPE2_CFLAGS', '$(pkg-config  --cflags --static  libbrotlicommon libbrotlidec libbrotlienc freetype2 zlib libpng)');
     $p->withExportVariable('FREETYPE2_LIBS', '$(pkg-config    --libs   --static  libbrotlicommon libbrotlidec libbrotlienc freetype2 zlib libpng)');
+    if ($p->getOsType() == 'macos') {
+        $libcpp = '-lc++';
+    } else {
+        $libcpp = '-lstdc++';
+    }
+    $p->withExportVariable('AVIF_CFLAGS', '$(pkg-config  --cflags --static libavif libbrotlicommon libbrotlidec libbrotlienc SvtAv1Enc SvtAv1Dec aom dav1d libgav1)');
+    $p->withExportVariable('AVIF_LIBS', '$(pkg-config    --libs   --static libavif libbrotlicommon libbrotlidec libbrotlienc SvtAv1Enc SvtAv1Dec aom dav1d libgav1) ' . $libcpp);
 };
