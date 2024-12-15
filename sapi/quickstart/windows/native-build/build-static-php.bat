0ac52a01474d09479f26b71a6cfed6cf18b5f237
jingjingxyk <zonghengbaihe521@qq.com>
2024-12-15 21:39:54 +0800

    update

--- sapi/quickstart/windows/native-build/build-static-php.bat
+++ sapi/quickstart/windows/native-build/build-static-php.bat
@@ -15,4 +15,12 @@ echo %cd%
 call "%__PROJECT__%\var\windows-build-deps\php-sdk-binary-tools\phpsdk-vs17-x64.bat"
 call "%__PROJECT__%\sapi\quickstart\windows\native-build\native-build-php-config.bat"
 
+
+if %ERRORLEVEL% neq 0 (
+    echo 命令执行失败，退出状态为: %ERRORLEVEL%
+    exit /b %ERRORLEVEL%
+)
+
+exit /b 0
+
 endlocal
--- sapi/quickstart/windows/native-build/build-static-php.ps1
+++ sapi/quickstart/windows/native-build/build-static-php.ps1
@@ -8,7 +8,28 @@ Write-Host  $__DIR__
 Write-Host  $__PROJECT__
 Write-Host (Get-Location).Path
 
+<#
 
+var\windows-build-deps\php-sdk-binary-tools\phpsdk-vs17-x64.bat
+cd var\windows-build-deps\php-src\
+
+.\buildconf.bat -f
+.\configure.bat --help
+
+configure.bat ^
+--disable-all         --disable-cgi      --enable-cli   ^
+--enable-sockets      --enable-ctype     --enable-pdo    --enable-phar  ^
+--enable-filter ^
+--enable-xmlreader   --enable-xmlwriter ^
+--enable-tokenizer
+
+
+
+nmake /E php.exe
+
+#>
+
+exit
 Invoke-Expression "cmd /c $__PROJECT__\var\windows-build-deps\php-sdk-binary-tools\phpsdk-vs17-x64.bat"
 Invoke-Expression "cmd /c $__PROJECT__\sapi\quickstart\windows\native-build\native-build-php-config.bat"
 
