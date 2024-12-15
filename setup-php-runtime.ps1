98f0b800bc0e1dc3eb6f2eb9629db68c6b125977
jingjingxyk <zonghengbaihe521@qq.com>
2024-12-15 22:35:41 +0800

    merge coee

--- setup-php-runtime.ps1
+++ setup-php-runtime.ps1
@@ -88,7 +88,7 @@ Write-Output $env:PATH
 # $env:PATH = $originalPath
 
 
-cd $TMP_DOWNLOAD_RUNTIME_DIR
+cd $__PROJECT__\var\runtime\
 
 # (Test-Path -Path .\php-8.4.1-nts-Win32-vs17-x64) -and (Remove-Item -Path .\php-8.4.1-nts-Win32-vs17-x64 -Recurse)
 # (Test-Path -Path .\php-8.4.1-nts-Win32-vs17-x64.zip) -and ( 7z x o php-8.4.1-nts-Win32-vs17-x64 .\php-8.4.1-nts-Win32-vs17-x64.zip)
