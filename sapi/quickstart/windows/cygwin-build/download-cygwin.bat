8d56ff7dabf380a64c5c73a6af01d411306595d4
jingjingxyk <zonghengbaihe521@qq.com>
2024-12-15 22:08:00 +0800

    merge coee

--- sapi/quickstart/windows/cygwin-build/download-cygwin.bat
+++ sapi/quickstart/windows/cygwin-build/download-cygwin.bat
@@ -1,3 +1,24 @@
 @echo off
+
+setlocal
+
+echo %~dp0
+cd /d %~dp0
+cd /d ..\..\..\..\
+
+set "__PROJECT__=%cd%"
+echo %cd%
+
+md %__PROJECT__%\var\windows-cygwin-build-deps\
+
+cd /d %__PROJECT__%\var\windows-cygwin-build-deps\
+
 :: cygwin site: https://cygwin.com/
-start https://cygwin.com/setup-x86_64.exe
+curl.exe -fSLo setup-x86_64.exe https://cygwin.com/setup-x86_64.exe
+
+
+:: exit /b 0
+
+cd /d %__PROJECT__%\
+
+endlocal
--- sapi/quickstart/windows/cygwin-build/install-cygwin.bat
+++ sapi/quickstart/windows/cygwin-build/install-cygwin.bat
@@ -1,4 +1,19 @@
 @echo off
+
+setlocal
+
+echo %~dp0
+cd /d %~dp0
+cd /d ..\..\..\..\
+
+set "__PROJECT__=%cd%"
+echo %cd%
+
+md %__PROJECT__%\var\windows-cygwin-build-deps\
+
+cd /d %__PROJECT__%\var\windows-cygwin-build-deps\
+
+
 :: cygwin安装多个包,包之间，用逗号分隔
 
 setup-x86_64.exe  --no-desktop --no-shortcuts --no-startmenu --quiet-mode --disable-buggy-antivirus    --site  https://mirrors.ustc.edu.cn/cygwin/ --packages make,git,curl,wget,tar,libtool,bison,gcc-g++,autoconf,automake,openssl,libpcre2-devel,libssl-devel,libcurl-devel,libxml2-devel,libxslt-devel,libgmp-devel,ImageMagick,libpng-devel,libjpeg-devel,libfreetype-devel,libwebp-devel,libsqlite3-devel,zlib-devel,libbz2-devel,liblz4-devel,liblzma-devel,libzip-devel,libicu-devel,libonig-devel,libcares-devel,libsodium-devel,libyaml-devel,libMagick-devel,libzstd-devel,libbrotli-devel,libreadline-devel,libintl-devel,libpq-devel,libssh2-devel,libidn2-devel,gettext-devel,coreutils,openssl-devel
@@ -9,4 +24,6 @@ setup-x86_64.exe  --quiet-mode --disable-buggy-antivirus    --site  https://mirr
 
 setup-x86_64.exe  --quiet-mode --disable-buggy-antivirus    --site  https://mirrors.ustc.edu.cn/cygwin/ --packages libzstd-devel
 
+cd /d %__PROJECT__%\
 
+endlocal
