2b45a665154f12610f9f29c4c79f1bc2d40be90a
jingjingxyk <zonghengbaihe521@qq.com>
2024-12-25 23:38:37 +0800

    install-cygwin.bat

--- sapi/quickstart/windows/cygwin-build/install-cygwin.bat
+++ sapi/quickstart/windows/cygwin-build/install-cygwin.bat
@@ -25,12 +25,19 @@ shift
 
 if not (%1)==() goto getopt
 
-setup-x86_64.exe  --no-desktop --no-shortcuts --no-startmenu --quiet-mode --disable-buggy-antivirus    --site  %SITE% --packages make,git,curl,wget,tar,libtool,bison,gcc-g++,autoconf,automake,openssl,libpcre2-devel,libssl-devel,libcurl-devel,libxml2-devel,libxslt-devel,libgmp-devel,ImageMagick,libpng-devel,libjpeg-devel,libfreetype-devel,libwebp-devel,libsqlite3-devel,zlib-devel,libbz2-devel,liblz4-devel,liblzma-devel,libzip-devel,libicu-devel,libonig-devel,libcares-devel,libsodium-devel,libyaml-devel,libMagick-devel,libzstd-devel,libbrotli-devel,libreadline-devel,libintl-devel,libpq-devel,libssh2-devel,libidn2-devel,gettext-devel,coreutils
+if "%GITHUB_ACTIONS%"=="" (
+	setup-x86_64.exe --quiet-mode --disable-buggy-antivirus --site %SITE%
+)
 
-setup-x86_64.exe  --quiet-mode --disable-buggy-antivirus    --site %SITE% --packages zip unzip
 
-setup-x86_64.exe  --quiet-mode --disable-buggy-antivirus    --site %SITE% --packages libpq5 libpq-devel
+setup-x86_64.exe  --no-desktop --no-shortcuts --no-startmenu --quiet-mode --disable-buggy-antivirus  --site  %SITE% --packages make,git,curl,wget,tar,libtool,bison,gcc-g++,autoconf,automake,openssl,libpcre2-devel,libssl-devel,libcurl-devel,libxml2-devel,libxslt-devel,libgmp-devel,ImageMagick,libpng-devel,libjpeg-devel,libfreetype-devel,libwebp-devel,libsqlite3-devel,zlib-devel,libbz2-devel,liblz4-devel,liblzma-devel,libzip-devel,libicu-devel,libonig-devel,libcares-devel,libsodium-devel,libyaml-devel,libMagick-devel,libzstd-devel,libbrotli-devel,libreadline-devel,libintl-devel,libpq-devel,libssh2-devel,libidn2-devel,gettext-devel,coreutils
 
-setup-x86_64.exe  --quiet-mode --disable-buggy-antivirus    --site %SITE% --packages libzstd-devel
+setup-x86_64.exe  --no-desktop --no-shortcuts --no-startmenu --quiet-mode --disable-buggy-antivirus  --site %SITE% --packages zip unzip
+
+setup-x86_64.exe  --no-desktop --no-shortcuts --no-startmenu --quiet-mode --disable-buggy-antivirus  --site %SITE% --packages libpq5 libpq-devel
+
+setup-x86_64.exe  --no-desktop --no-shortcuts --no-startmenu --quiet-mode --disable-buggy-antivirus  --site %SITE% --packages libzstd-devel
+
+
 
 endlocal
