3bea2bf8e4c010c22c6a8918140be74b9c661c04
jingjingxyk <zonghengbaihe521@qq.com>
2024-12-25 22:50:54 +0800

    update install-cygwin.bat

--- sapi/quickstart/windows/cygwin-build/install-cygwin.bat
+++ sapi/quickstart/windows/cygwin-build/install-cygwin.bat
@@ -2,7 +2,7 @@
 :: cygwin site: https://cygwin.com/
 :: start https://cygwin.com/setup-x86_64.exe
 
-setlocal
+setlocal enableextensions enabledelayedexpansion
 
 
 echo %~dp0
@@ -13,12 +13,24 @@ set "__PROJECT__=%cd%"
 echo %cd%
 :: package  separate with commas
 
-setup-x86_64.exe  --no-desktop --no-shortcuts --no-startmenu --quiet-mode --disable-buggy-antivirus    --site  https://mirrors.ustc.edu.cn/cygwin/ --packages make,git,curl,wget,tar,libtool,bison,gcc-g++,autoconf,automake,openssl,libpcre2-devel,libssl-devel,libcurl-devel,libxml2-devel,libxslt-devel,libgmp-devel,ImageMagick,libpng-devel,libjpeg-devel,libfreetype-devel,libwebp-devel,libsqlite3-devel,zlib-devel,libbz2-devel,liblz4-devel,liblzma-devel,libzip-devel,libicu-devel,libonig-devel,libcares-devel,libsodium-devel,libyaml-devel,libMagick-devel,libzstd-devel,libbrotli-devel,libreadline-devel,libintl-devel,libpq-devel,libssh2-devel,libidn2-devel,gettext-devel,coreutils
+set SITE='https://mirrors.kernel.org/sourceware/cygwin/'
 
-setup-x86_64.exe  --quiet-mode --disable-buggy-antivirus    --site  https://mirrors.ustc.edu.cn/cygwin/ --packages zip unzip
+:getopt
+if /i "%1" equ "--mirror" (
+	if /i "%2" equ "china" (
+		set SITE='https://mirrors.ustc.edu.cn/cygwin/'
+	)
+)
+shift
 
-setup-x86_64.exe  --quiet-mode --disable-buggy-antivirus    --site  https://mirrors.ustc.edu.cn/cygwin/ --packages libpq5 libpq-devel
+if not (%1)==() goto getopt
 
-setup-x86_64.exe  --quiet-mode --disable-buggy-antivirus    --site  https://mirrors.ustc.edu.cn/cygwin/ --packages libzstd-devel
+setup-x86_64.exe  --no-desktop --no-shortcuts --no-startmenu --quiet-mode --disable-buggy-antivirus    --site  %SITE% --packages make,git,curl,wget,tar,libtool,bison,gcc-g++,autoconf,automake,openssl,libpcre2-devel,libssl-devel,libcurl-devel,libxml2-devel,libxslt-devel,libgmp-devel,ImageMagick,libpng-devel,libjpeg-devel,libfreetype-devel,libwebp-devel,libsqlite3-devel,zlib-devel,libbz2-devel,liblz4-devel,liblzma-devel,libzip-devel,libicu-devel,libonig-devel,libcares-devel,libsodium-devel,libyaml-devel,libMagick-devel,libzstd-devel,libbrotli-devel,libreadline-devel,libintl-devel,libpq-devel,libssh2-devel,libidn2-devel,gettext-devel,coreutils
+
+setup-x86_64.exe  --quiet-mode --disable-buggy-antivirus    --site %SITE% --packages zip unzip
+
+setup-x86_64.exe  --quiet-mode --disable-buggy-antivirus    --site %SITE% --packages libpq5 libpq-devel
+
+setup-x86_64.exe  --quiet-mode --disable-buggy-antivirus    --site %SITE% --packages libzstd-devel
 
 endlocal
