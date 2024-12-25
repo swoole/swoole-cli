2e9d50055e3d334e9318df3d86076f1acb587a6d
jingjingxyk <zonghengbaihe521@qq.com>
2024-12-25 12:21:25 +0800

    merge main source code

--- sapi/quickstart/windows/install-cygwin.bat
+++ sapi/quickstart/windows/install-cygwin.bat
@@ -11,9 +11,7 @@ cd /d ..\..\..\
 
 set "__PROJECT__=%cd%"
 echo %cd%
-
 :: package  separate with commas
-
 
 setup-x86_64.exe  --no-desktop --no-shortcuts --no-startmenu --quiet-mode --disable-buggy-antivirus    --site  https://mirrors.ustc.edu.cn/cygwin/ --packages make,git,curl,wget,tar,libtool,bison,gcc-g++,autoconf,automake,openssl,libpcre2-devel,libssl-devel,libcurl-devel,libxml2-devel,libxslt-devel,libgmp-devel,ImageMagick,libpng-devel,libjpeg-devel,libfreetype-devel,libwebp-devel,libsqlite3-devel,zlib-devel,libbz2-devel,liblz4-devel,liblzma-devel,libzip-devel,libicu-devel,libonig-devel,libcares-devel,libsodium-devel,libyaml-devel,libMagick-devel,libzstd-devel,libbrotli-devel,libreadline-devel,libintl-devel,libpq-devel,libssh2-devel,libidn2-devel,gettext-devel,coreutils,openssl-devel
 
