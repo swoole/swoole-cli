821098998fb3df7f7bc16ed53f565dfa4e4696a3
jingjingxyk <zonghengbaihe521@qq.com>
2025-01-10 16:33:32 +0800

    update cygwin-config-ext.sh

--- sapi/scripts/cygwin/cygwin-config-ext.sh
+++ sapi/scripts/cygwin/cygwin-config-ext.sh
@@ -11,49 +11,61 @@ __PROJECT__=$(
 )
 cd ${__PROJECT__}
 
-ROOT=${__PROJECT__}
-
 REDIS_VERSION=5.3.7
 MONGODB_VERSION=1.14.2
 YAML_VERSION=2.2.2
 IMAGICK_VERSION=3.7.0
 
-if [ ! -d pool/ext ]; then
-  mkdir -p pool/ext
+mkdir -p pool/ext
+mkdir -p pool/lib
+mkdir -p pool/php-tar
+
+WORK_DIR=${__PROJECT__}/var/cygwin-build/
+EXT_TEMP_CACHE_DIR=${WORK_DIR}/pool/ext/
+mkdir -p ${WORK_DIR}
+mkdir -p ${EXT_TEMP_CACHE_DIR}
+test -d ${WORK_DIR}/ext/ && rm -rf ${WORK_DIR}/ext/
+mkdir -p ${WORK_DIR}/ext/
+
+cd ${__PROJECT__}/pool/ext
+if [ ! -f redis-${REDIS_VERSION}.tgz ]; then
+  curl -fSLo ${EXT_TEMP_CACHE_DIR}/redis-${REDIS_VERSION}.tgz https://pecl.php.net/get/redis-${REDIS_VERSION}.tgz
+  mv ${EXT_TEMP_CACHE_DIR}/redis-${REDIS_VERSION}.tgz ${__PROJECT__}/pool/ext
 fi
+mkdir -p ${WORK_DIR}/ext/redis/
+tar --strip-components=1 -C ${WORK_DIR}/ext/redis/ -xf redis-${REDIS_VERSION}.tgz
 
-cd pool/ext
+: <<EOF
+# mongodb 扩展 不支持 cygwin 环境下构建
+# 详见： https://github.com/mongodb/mongo-php-driver/issues/1381
 
-if [ ! -d $ROOT/ext/redis ]; then
-  if [ ! -f redis-${REDIS_VERSION}.tgz ]; then
-    curl -fSLo redis-${REDIS_VERSION}.tgz https://pecl.php.net/get/redis-${REDIS_VERSION}.tgz
-  fi
-  tar xvf redis-${REDIS_VERSION}.tgz
-  mv redis-${REDIS_VERSION} $ROOT/ext/redis
+cd ${__PROJECT__}/pool/ext
+if [ ! -f mongodb-${MONGODB_VERSION}.tgz ]; then
+  curl -fSLo ${EXT_TEMP_CACHE_DIR}/mongodb-${MONGODB_VERSION}.tgz https://pecl.php.net/get/mongodb-${MONGODB_VERSION}.tgz
+  mv ${EXT_TEMP_CACHE_DIR}/redis-${REDIS_VERSION}.tgz ${__PROJECT__}/pool/ext
 fi
+mkdir -p ${WORK_DIR}/ext/mongodb/
+tar --strip-components=1 -C ${WORK_DIR}/ext/mongodb/ -xf redis-${REDIS_VERSION}.tgz
 
-if [ ! -d $ROOT/ext/mongodb ]; then
-  if [ ! -f mongodb-${MONGODB_VERSION}.tgz ]; then
-    curl -fSLo mongodb-${MONGODB_VERSION}.tgz https://pecl.php.net/get/mongodb-${MONGODB_VERSION}.tgz
-  fi
-  tar xvf mongodb-${MONGODB_VERSION}.tgz
-  mv mongodb-${MONGODB_VERSION} $ROOT/ext/mongodb
+EOF
+
+cd ${__PROJECT__}/pool/ext
+if [ ! -f yaml-${YAML_VERSION}.tgz ]; then
+  curl -fSLo ${EXT_TEMP_CACHE_DIR}/yaml-${YAML_VERSION}.tgz https://pecl.php.net/get/yaml-${YAML_VERSION}.tgz
+  mv ${EXT_TEMP_CACHE_DIR}/yaml-${YAML_VERSION}.tgz ${__PROJECT__}/pool/ext
 fi
+mkdir -p ${WORK_DIR}/ext/yaml/
+tar --strip-components=1 -C ${WORK_DIR}/ext/yaml/ -xf yaml-${YAML_VERSION}.tgz
 
-if [ ! -d $ROOT/ext/yaml ]; then
-  if [ ! -f yaml-${YAML_VERSION}.tgz ]; then
-    curl -fSLo yaml-${YAML_VERSION}.tgz https://pecl.php.net/get/yaml-${YAML_VERSION}.tgz
-  fi
-  tar xvf yaml-${YAML_VERSION}.tgz
-  mv yaml-${YAML_VERSION} $ROOT/ext/yaml
+cd ${__PROJECT__}/pool/ext
+if [ ! -f imagick-${IMAGICK_VERSION}.tgz ]; then
+  curl -fSLo ${EXT_TEMP_CACHE_DIR}/imagick-${IMAGICK_VERSION}.tgz https://pecl.php.net/get/imagick-${IMAGICK_VERSION}.tgz
+  mv ${EXT_TEMP_CACHE_DIR}/imagick-${IMAGICK_VERSION}.tgz ${__PROJECT__}/pool/ext
 fi
+mkdir -p ${WORK_DIR}/ext/imagick/
+tar --strip-components=1 -C ${WORK_DIR}/ext/imagick/ -xf imagick-${IMAGICK_VERSION}.tgz
 
-if [ ! -d $ROOT/ext/imagick ]; then
-  if [ ! -f imagick-${IMAGICK_VERSION}.tgz ]; then
-    curl -fSLo imagick-${IMAGICK_VERSION}.tgz https://pecl.php.net/get/imagick-${IMAGICK_VERSION}.tgz
-  fi
-  tar xvf imagick-${IMAGICK_VERSION}.tgz
-  mv imagick-${IMAGICK_VERSION} $ROOT/ext/imagick
-fi
+# cp -rf var/cygwin-build/ext/* var/
+cp -rf ${WORK_DIR}/ext/* ${__PROJECT__}/ext/
 
-cd $ROOT
+cd ${__PROJECT__}
