0ef7a18c9b10806f9bd9c108503128b6794f8cd6
jingjingxyk <zonghengbaihe521@qq.com>
2025-03-20 10:11:24 +0800

    优化 phpy 扩展配置

--- sapi/src/builder/extension/phpy.php
+++ sapi/src/builder/extension/phpy.php
@@ -6,11 +6,10 @@ use SwooleCli\Extension;
 
 return function (Preprocessor $p) {
     // anaconda 安装包
-
     // https://repo.anaconda.com/archive/
 
-    # $options .= ' --with-python-version=3.12';
-    # $options .= ' --with-python-dir=/opt/anaconda3';
+    // $options .= ' --with-python-version=3.12';
+    // $options .= ' --with-python-dir=/opt/anaconda3';
 
     $tag = 'v1.0.11';
 
@@ -33,19 +32,6 @@ return function (Preprocessor $p) {
         ->withDependentExtensions(...$dependentExtensions)
         ->withDependentLibraries(...$dependentLibraries);
     $p->addExtension($ext);
-
-    $p->withBeforeConfigureScript('phpy', function (Preprocessor $p) {
-        $php_src = $p->getWorkDir();
-        $cmd = <<<EOF
-
-        cd {$php_src}/
-        sed -i.backup "s/ -z now/  /g" ext/phpy/config.m4
-        rm -f ext/phpy/config.m4.backup
-EOF;
-
-        return $cmd;
-    });
-
 
     $libs = $p->isMacos() ? '-lc++' : ' -lstdc++ ';
     $p->withVariable('LIBS', '$LIBS ' . $libs);
