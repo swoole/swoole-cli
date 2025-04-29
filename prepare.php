301029eb49c88daf50005926c325c6a4e1f4c0fd
jingjingxyk <zonghengbaihe521@qq.com>
2025-04-29 17:20:43 +0800

    fix enable mimalloc error

--- prepare.php
+++ prepare.php
@@ -62,9 +62,7 @@ if ($p->isMacos()) {
 
 $p->setExtraCflags(' -Os');
 
-if ($p->getInputOption('with-mimalloc')) {
-    $p->loadDependentLibrary('mimalloc');
-}
+
 
 // Generate make.sh
 $p->execute();
