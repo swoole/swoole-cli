1c7acf3c7f1cbda6d5fd00c53ed55e56a4109ad9
jingjingxyk <zonghengbaihe521@qq.com>
2025-05-17 13:50:01 +0800

    update download-box-get-archive-from-server.sh

--- sapi/tools/genereate-php-extension-example.sh
+++ sapi/tools/genereate-php-extension-example.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+shopt -s expand_aliases
 
-:<<'COMMENT'
+: <<'COMMENT'
 
 创建 PHP 扩展 辅助工具
 
@@ -11,7 +12,7 @@ __DIR__=$(
   cd "$(dirname "$0")"
   pwd
 )
-if [ -f ${__DIR__}/prepare.php ] ; then
+if [ -f ${__DIR__}/prepare.php ]; then
   __PROJECT__=$(
     cd ${__DIR__}/
     pwd
@@ -25,18 +26,15 @@ fi
 
 cd ${__DIR__}
 
+export PATH="${__PROJECT__}/runtime/php-cli/:$PATH"
 
-export PATH="${__PROJECT__}/bin/runtime:$PATH"
-
-alias php="'php -c ${__PROJECT__}/bin/runtime/php.ini'"
+alias php="'php -c ${__PROJECT__}/runtime/php-cli/php.ini'"
 
 php ${__PROJECT__}/sapi/scripts/download-php-src-archive.php
-
 
 php ${__PROJECT__}/var/php-8.2.23/ext/ext_skel.php --help
 
 # 创建扩展
 # 使用 `--dir ${__PROJECT__}/../ ` # 指定生成扩展的目录
 
-php ${__PROJECT__}/var/php-8.2.23/ext/ext_skel.php  --ext example --author jingjingxyk --std
-
+php ${__PROJECT__}/var/php-8.2.23/ext/ext_skel.php --ext example --author jingjingxyk --std
