15e10ae5862962251b74baedde1026b16d1785e1
jingjingxyk <zonghengbaihe521@qq.com>
2025-08-22 22:31:53 +0800

    swow version update to v1.6.1

--- sapi/src/builder/extension/swow.php
+++ sapi/src/builder/extension/swow.php
@@ -5,6 +5,7 @@ use SwooleCli\Extension;
 use SwooleCli\Library;
 
 return function (Preprocessor $p) {
+    $swow_tag = 'v1.6.1';
     $options = ' --enable-swow ';
     $options .= ' --enable-swow-ssl ';
     $options .= ' --enable-swow-curl ';
@@ -20,14 +21,12 @@ return function (Preprocessor $p) {
             ->withHomePage('https://github.com/swow/swow')
             ->withLicense('https://github.com/swow/swow/blob/develop/LICENSE', Extension::LICENSE_APACHE2)
             ->withManual('https://docs.toast.run/swow/en/install.html')
-            ->withFile('swow-v1.5.3.tar.gz')
+            ->withFile("swow-{$swow_tag}.tar.gz")
             ->withDownloadScript(
                 "swow",
                 <<<EOF
-                git clone -b v1.5.3 https://github.com/swow/swow.git
-                mv swow swow-t
-                mv swow-t/ext  swow
-                rm -rf swow-t
+                git clone -b {$swow_tag} https://github.com/swow/swow.git swow-code
+                mv swow-code/ext  swow
 EOF
             )
             ->withBuildCached(false)
