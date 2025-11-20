fe47233fe7c06e8a676d8986f1fdec32ff4446e9
jingjingxyk <zonghengbaihe521@qq.com>
2025-11-20 08:52:23 +0800

    upgrade apcu to support php 8.4

--- sapi/src/builder/extension/apcu.php
+++ sapi/src/builder/extension/apcu.php
@@ -7,8 +7,8 @@ return function (Preprocessor $p) {
     $p->addExtension(
         (new Extension('apcu'))
             ->withOptions('--enable-apcu')
-            ->withPeclVersion('5.1.22')
-            ->withFileHash('md5', '2e1fb1f09725ada616e873c4e4012ff6')
+            ->withPeclVersion('5.1.27')
+            ->withFileHash('md5', '2f8c0244d31aabfb9cf68695764398f0')
             ->withHomePage('https://github.com/krakjoe/apcu.git')
             ->withManual('https://www.php.net/apcu')
             ->withLicense('https://github.com/krakjoe/apcu/blob/master/LICENSE', Extension::LICENSE_PHP)
