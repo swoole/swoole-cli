ddec7c6ec61c804b73c3d6b1da365c59bb48bded
tianfenghan <rango@swoole.com>
2025-11-28 14:35:04 +0800

    Add support for the dynamically linked glibc build

---
+++ sapi/scripts/install-deps-on-ubuntu.sh
@@ -0,0 +1,36 @@
+# build tools
+sudo apt install gcc g++ make autoconf libtool pkg-config re2c
+
+# libraries
+sudo apt install libyaml-dev \
+    libssl-dev \
+    libxml2-dev \
+    libmagic-dev \
+    libpng-dev \
+    libjpeg-dev \
+    libfreetype-dev \
+    libc-ares-dev \
+    libpcre2-dev \
+    libsodium-dev \
+    libcurl4-openssl-dev \
+    libmariadb-dev \
+    libaio-dev \
+    zlib1g \
+    mime-support \
+    tzdata \
+    ucf \
+    libargon2-dev \
+    libsqlite3-dev \
+    libbrotli-dev \
+    libpq-dev \
+    unixodbc-dev \
+    firebird-dev \
+    libzstd-dev \
+    libssh-dev \
+    liburing-dev \
+    libedit-dev \
+    libreadline-dev \
+    libmagic-dev \
+    libonig-dev \
+    libxslt-dev \
+    libzip-dev
