c2058c2f09b00b0ddac2babbfd06af42171733fe
jingjingxyk <zonghengbaihe521@qq.com>
2026-01-07 17:59:15 +0800

    使用 pie 下载扩展

--- setup-php-runtime.sh
+++ setup-php-runtime.sh
@@ -51,7 +51,7 @@ esac
 APP_VERSION='v5.1.3'
 APP_NAME='swoole-cli'
 VERSION='v5.1.3.0'
-PIE_VERSION="1.2.1"
+PIE_VERSION="1.3.5"
 # 查看pie最新版本 https://github.com/php/pie/releases/latest
 
 cd ${__PROJECT__}
