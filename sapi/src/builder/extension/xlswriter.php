db43402649b3144df6a866009e3242f317433279
jingjingxyk <zonghengbaihe521@qq.com>
2026-07-03 17:34:42 +0800

    update extension xlswriter hash

--- sapi/src/builder/extension/xlswriter.php
+++ sapi/src/builder/extension/xlswriter.php
@@ -10,7 +10,7 @@ return function (Preprocessor $p) {
             ->withHomePage('https://github.com/viest/php-ext-xlswriter')
             ->withLicense('https://github.com/viest/php-ext-xlswriter/blob/master/LICENSE', Extension::LICENSE_BSD)
             ->withPeclVersion('2.0.3')
-            //->withFileHash('md5', '924847f19c20a6d071e91b7d2488021d')
+            ->withFileHash('md5', '233f380e7fa2f74eb594946515fda1ad')
             ->withOptions(' --with-xlswriter --enable-reader --with-openssl=' . OPENSSL_PREFIX)
             ->withDependentLibraries('openssl')
     );
